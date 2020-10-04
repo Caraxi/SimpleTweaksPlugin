f6d982a8090e7d6fdae26964031cfcd41d20e97b
Cara <git@caraxian.com>
2020-10-04 10:43:18 +1030

    Cleanup

--- SimpleTweaksPlugin.cs
+++ SimpleTweaksPlugin.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Dalamud.Plugin;
-using SimpleTweaksPlugin.Tweaks;
 
 namespace SimpleTweaksPlugin {
     public class SimpleTweaksPlugin : IDalamudPlugin {
