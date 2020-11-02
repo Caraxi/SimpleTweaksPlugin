2956d5930487fbfddc6035fbe3232b639012a601
Cara <git@caraxian.com>
2020-11-02 12:05:07 +1030

    Update TooltipTweaks.cs

--- Tweaks/TooltipTweaks.cs
+++ Tweaks/TooltipTweaks.cs
@@ -2,10 +2,13 @@
 using Dalamud.Plugin;
 using ImGuiNET;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using Dalamud;
+using Dalamud.Game.Chat.SeStringHandling;
+using Dalamud.Game.Chat.SeStringHandling.Payloads;
 using Dalamud.Game.Internal;
 using Lumina.Excel.GeneratedSheets;
 
@@ -24,6 +27,11 @@ namespace SimpleTweaksPlugin {
             public bool TrailingZeros = true;
             public bool ShowDesynthSkill = true;
             public bool EnableCopyItemName = true;
+            public bool ShowAcquiredStatus = false;
+            #if DEBUG
+            public bool ShowItemID = false;
+            #endif
+            public bool AlwaysShowGlamourName = false;
         }
 
         public override bool DrawConfig() {
@@ -33,10 +41,14 @@ namespace SimpleTweaksPlugin {
             if (ImGui.TreeNode($"{Name}###{GetType().Name}settingsNode")) {
                 change = ImGui.Checkbox("Precise Durability", ref PluginConfig.TooltipTweaks.EnableDurability) || change;
                 change = ImGui.Checkbox("Precise Spiritbond", ref PluginConfig.TooltipTweaks.EnableSpiritbond) || change;
+                #if DEBUG
+                change = ImGui.Checkbox("Show Item ID", ref PluginConfig.TooltipTweaks.ShowItemID) || change;
+                #endif
                 ImGui.Indent(20);
                 change = ImGui.Checkbox("Trailing Zeros", ref PluginConfig.TooltipTweaks.TrailingZeros) || change;
                 ImGui.Indent(-20);
                 change = ImGui.Checkbox("Show Desynth Skill", ref PluginConfig.TooltipTweaks.ShowDesynthSkill) || change;
+                change = ImGui.Checkbox("Always show glamour name", ref PluginConfig.TooltipTweaks.AlwaysShowGlamourName) || change;
                 change = ImGui.Checkbox("CTRL-C to copy hovered item.", ref PluginConfig.TooltipTweaks.EnableCopyItemName) || change;
                 ImGui.TreePop();
             }
@@ -61,6 +73,10 @@ namespace SimpleTweaksPlugin {
         private readonly IntPtr allocDurability = Marshal.AllocHGlobal(32);
         private readonly IntPtr allocDesynthSkill = Marshal.AllocHGlobal(512);
         private readonly IntPtr allocControlDisplay = Marshal.AllocHGlobal(512);
+        private readonly IntPtr allocItemName = Marshal.AllocHGlobal(512);
+        #if DEBUG
+        private readonly IntPtr allocItemId = Marshal.AllocHGlobal(512);
+        #endif
 
         private ushort lastSpiritbond;
         private ushort lastDurability;
@@ -139,6 +155,7 @@ namespace SimpleTweaksPlugin {
             WriteString((byte*)alloc, overwrite, true);
             *startPtr = (byte*)alloc;
         }
+
         private unsafe void ReplaceText(byte** startPtr, IntPtr alloc, string find, string replace) {
             if (startPtr == null) return;
             var start = *(startPtr);
@@ -162,26 +179,69 @@ namespace SimpleTweaksPlugin {
             *startPtr = (byte*)alloc;
         }
 
+        private unsafe void AppendSeString(byte** startPtr, IntPtr alloc, SeString append) {
+            if (startPtr == null) return;
+            var start = *(startPtr);
+            if (start == null) return;
+            if (start == (byte*)alloc) return;
+            var overwrite = ReadSeString(start);
+            overwrite.Payloads.AddRange(append.Payloads);
+            WriteSeString((byte*) alloc, overwrite);
+            *startPtr = (byte*) alloc;
+        }
+
+        private unsafe void WriteSeString(byte** startPtr, IntPtr alloc, SeString seString) {
+            if (startPtr == null) return;
+            var start = *(startPtr);
+            if (start == null) return;
+            if (start == (byte*)alloc) return;
+            WriteSeString((byte*) alloc, seString);
+            *startPtr = (byte*) alloc;
+        }
+
+        private unsafe SeString ReadSeString(byte** startPtr) {
+            if (startPtr == null) return null;
+            var start = *(startPtr);
+            if (start == null) return null;
+            return ReadSeString(start);
+        }
+
+
+
         private unsafe IntPtr TooltipDetour(IntPtr a1, uint** a2, byte*** a3) {
 #if DEBUG
             PluginLog.Log("Tooltip Address: " + ((ulong) *(a3 + 4)).ToString("X"));
 #endif
             if (PluginConfig.TooltipTweaks.EnableDurability) ReplacePercentage(*(a3+4) + 28, allocDurability, lastDurability / 300.0);
             if (PluginConfig.TooltipTweaks.EnableSpiritbond) ReplacePercentage(*(a3+4) + 30, allocSpiritbond, lastSpiritbond / 100.0);
+            #if DEBUG
+            if (PluginConfig.TooltipTweaks.ShowItemID) {
+                var id = PluginInterface.Framework.Gui.HoveredItem;
+                if (id < 2000000) { 
+                    id %= 500000;
+                }
+                AppendText(*(a3 + 4) + 2, allocItemId, $"  ({id})");
+            }
+            #endif
             if (PluginConfig.TooltipTweaks.EnableCopyItemName) {
                 AppendText(*(a3 + 4) + 0x40, allocControlDisplay, "　Ctrl+C  Copy item name");
             }
+            
             if (PluginConfig.TooltipTweaks.ShowDesynthSkill) {
                 var id = PluginInterface.Framework.Gui.HoveredItem;
                 if (id < 2000000) {
                     id %= 500000;
-
+                    
                     var item = PluginInterface.Data.Excel.GetSheet<Item>().GetRow((uint) id);
                     if (item != null && item.Unknown36 > 0) {
+                        PluginLog.Log($"Desynthable: {item.Name}");
                         var classJobOffset = 2 * (int) (item.ClassJobRepair.Row - 8);
                         var desynthLevel = *(ushort*) (playerStaticAddress + (0x692 + classJobOffset)) / 100f;
 
                         var useDescription = desynthInDescription.Contains(item.ItemSearchCategory.Row);
+
+
+
 
                         switch (PluginInterface.ClientState.ClientLanguage) {
                             case ClientLanguage.Japanese:
@@ -218,6 +278,22 @@ namespace SimpleTweaksPlugin {
             return Encoding.UTF8.GetString(ptr, offset);
         }
 
+        private unsafe SeString ReadSeString(byte* ptr) {
+            var offset = 0;
+            while (true) {
+                var b = *(ptr + offset);
+                if (b == 0) {
+                    break;
+                }
+                offset += 1;
+            }
+
+            var bytes = new byte[offset];
+            Marshal.Copy(new IntPtr(ptr), bytes, 0, offset);
+
+            return PluginInterface.SeStringManager.Parse(bytes);
+        }
+
         private unsafe void WriteString(byte* dst, string s, bool finalise = false) {
             var bytes = Encoding.UTF8.GetBytes(s);
             for (var i = 0; i < bytes.Length; i++) {
@@ -226,6 +302,14 @@ namespace SimpleTweaksPlugin {
             if (finalise) {
                 *(dst + bytes.Length) = 0;
             }
+        }
+
+        private unsafe void WriteSeString(byte* dst, SeString s) {
+            var bytes = s.Encode();
+            for (var i = 0; i < bytes.Length; i++) {
+                *(dst + i) = bytes[i];
+            }
+            *(dst + bytes.Length) = 0;
         }
 
         public override void Disable() {
@@ -241,6 +325,11 @@ namespace SimpleTweaksPlugin {
             Marshal.FreeHGlobal(this.allocSpiritbond);
             Marshal.FreeHGlobal(this.allocDurability);
             Marshal.FreeHGlobal(this.allocDesynthSkill);
+            Marshal.FreeHGlobal(this.allocControlDisplay);
+            Marshal.FreeHGlobal(this.allocItemName);
+#if DEBUG
+            Marshal.FreeHGlobal(this.allocItemId);
+#endif
             Enabled = false;
             Ready = false;
         }
