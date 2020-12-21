21296791283208c2a50ea5f376d79760cb54c12d
Cara <git@caraxian.com>
2020-12-21 11:29:58 +1030

    Adjust plugin logging
    don't include useless parameters in final build

--- SimpleLog.cs
+++ SimpleLog.cs
@@ -1,75 +1,75 @@
-﻿using System.IO;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using Dalamud.Plugin;
 
 namespace SimpleTweaksPlugin {
     internal static class SimpleLog {
 
-
 #if DEBUG
-        private static int subStrIndex = 0;
+        private static int _subStrIndex;
         public static void SetupBuildPath([CallerFilePath] string callerPath = "") {
             var p = Path.GetDirectoryName(callerPath);
-            subStrIndex = p.Length + 1;
-        }
-#endif
-
-        private static string[] SplitMessage(object message) {
-            return $"{message}".Split('\n');
+            if (p != null) _subStrIndex = p.Length + 1;
         }
 
         public static void Verbose(object message, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerName = "", [CallerLineNumber] int lineNumber = -1) {
-            foreach(var m in SplitMessage(message))
-#if DEBUG
-                PluginLog.LogVerbose($"[{callerPath.Substring(subStrIndex)}::{callerName}:{lineNumber}] {m}");
-#else
-                PluginLog.LogVerbose($"{m}");
-#endif
+            foreach (var m in SplitMessage(message)) PluginLog.LogVerbose($"[{callerPath.Substring(_subStrIndex)}::{callerName}:{lineNumber}] {m}");
         }
 
         public static void Debug(object message, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerName = "", [CallerLineNumber] int lineNumber = -1) {
-            foreach (var m in SplitMessage(message))
-#if DEBUG
-                PluginLog.LogDebug($"[{callerPath.Substring(subStrIndex)}::{callerName}:{lineNumber}] {m}");
-#else
-                PluginLog.LogDebug($"{m}");
-#endif
+            foreach (var m in SplitMessage(message)) PluginLog.LogDebug($"[{callerPath.Substring(_subStrIndex)}::{callerName}:{lineNumber}] {m}");
         }
 
         public static void Information(object message, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerName = "", [CallerLineNumber] int lineNumber = -1) {
-            foreach (var m in SplitMessage(message))
-#if DEBUG
-                PluginLog.LogInformation($"[{callerPath.Substring(subStrIndex)}::{callerName}:{lineNumber}] {message}");
-#else
-                PluginLog.LogInformation($"{m}");
-#endif
+            foreach (var m in SplitMessage(message)) PluginLog.LogInformation($"[{callerPath.Substring(_subStrIndex)}::{callerName}:{lineNumber}] {message}");
         }
 
         public static void Fatal(object message, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerName = "", [CallerLineNumber] int lineNumber = -1) {
-            foreach (var m in SplitMessage(message))
-#if DEBUG
-                PluginLog.LogFatal($"[{callerPath.Substring(subStrIndex)}::{callerName}:{lineNumber}] {m}");
-#else
-                PluginLog.LogFatal($"{m}");
-#endif
+            foreach (var m in SplitMessage(message)) PluginLog.LogFatal($"[{callerPath.Substring(_subStrIndex)}::{callerName}:{lineNumber}] {m}");
         }
 
         public static void Log(object message, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerName = "", [CallerLineNumber] int lineNumber = -1) {
-            foreach (var m in SplitMessage(message))
-#if DEBUG
-                PluginLog.Log($"[{callerPath.Substring(subStrIndex)}::{callerName}:{lineNumber}] {m}");
-#else
-                PluginLog.Log($"{m}");
-#endif
+            foreach (var m in SplitMessage(message)) PluginLog.Log($"[{callerPath.Substring(_subStrIndex)}::{callerName}:{lineNumber}] {m}");
         }
 
         public static void Error(object message, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerName = "", [CallerLineNumber] int lineNumber = -1) {
-            foreach (var m in SplitMessage(message))
-#if DEBUG
-                PluginLog.Error($"[{callerPath.Substring(subStrIndex)}::{callerName}:{lineNumber}] {m}");
-#else
-                PluginLog.Error($"{m}");
-#endif
+            foreach (var m in SplitMessage(message)) PluginLog.Error($"[{callerPath.Substring(_subStrIndex)}::{callerName}:{lineNumber}] {m}");
         }
+#else
+        public static void Verbose(object message) {
+            foreach(var m in SplitMessage(message)) PluginLog.LogVerbose($"{m}");
+        }
+
+        public static void Debug(object message) {
+            foreach (var m in SplitMessage(message)) PluginLog.LogDebug($"{m}");
+        }
+
+        public static void Information(object message) {
+            foreach (var m in SplitMessage(message)) PluginLog.LogInformation($"{m}");
+        }
+
+        public static void Fatal(object message) {
+            foreach (var m in SplitMessage(message)) PluginLog.LogFatal($"{m}");
+        }
+
+        public static void Log(object message) {
+            foreach (var m in SplitMessage(message)) PluginLog.Log($"{m}");
+        }
+
+        public static void Error(object message) {
+            foreach (var m in SplitMessage(message)) PluginLog.Error($"{m}");
+        }
+#endif
+
+        private static IEnumerable<string> SplitMessage(object message) {
+            if (message is IList list) {
+                return list.Cast<object>().Select((t, i) => $"{i}: {t}");
+            }
+            return $"{message}".Split('\n');
+        }
+
     }
 }
