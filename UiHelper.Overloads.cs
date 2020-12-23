e1356e93243012e737a2d07d1e1229c263ec9f09
Cara <git@caraxian.com>
2020-12-23 21:24:43 +1030

    Add Cleaner World Select UI

---
+++ UiHelper.Overloads.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+using FFXIVClientStructs.Component.GUI;
+
+namespace SimpleTweaksPlugin {
+    public static unsafe partial class UiHelper {
+        public static void Hide(AtkTextNode* node) => Hide((AtkResNode*)node);
+        public static void Hide(AtkNineGridNode* node) => Hide((AtkResNode*)node);
+        public static void Hide(AtkImageNode* node) => Hide((AtkResNode*)node);
+        public static void Hide(AtkComponentNode* node) => Hide((AtkResNode*)node);
+
+        public static void Show(AtkTextNode* node) => Show((AtkResNode*)node);
+        public static void Show(AtkNineGridNode* node) => Show((AtkResNode*)node);
+        public static void Show(AtkImageNode* node) => Show((AtkResNode*)node);
+        public static void Show(AtkComponentNode* node) => Show((AtkResNode*)node);
+
+        public static void SetSize(AtkComponentNode* node, ushort? w, ushort? h) => SetSize((AtkResNode*) node, w, h);
+        public static void SetSize(AtkTextNode* node, ushort? w, ushort? h) => SetSize((AtkResNode*) node, w, h);
+
+        public static void SetPosition(AtkComponentNode* node, float? x, float? y) => SetPosition((AtkResNode*) node, x, y);
+        public static void SetPosition(AtkTextNode* node, float? x, float? y) => SetPosition((AtkResNode*) node, x, y);
+    }
+}
---
+++ UiHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using FFXIVClientStructs.Component.GUI;
+using FFXIVClientStructs.Component.GUI.ULD;
+
+namespace SimpleTweaksPlugin {
+    public static unsafe partial class UiHelper {
+
+        public static void Hide(AtkResNode* node) {
+            node->Flags &= ~0x10;
+            node->Flags_2 |= 0x1;
+        }
+        public static void Show(AtkResNode* node) {
+            node->Flags |= 0x10;
+            node->Flags_2 |= 0x1;
+        }
+
+        public static void SetSize(AtkResNode* node, ushort? width, ushort? height) {
+            if (width != null) node->Width = width.Value;
+            if (height != null) node->Height = height.Value;
+            node->Flags_2 |= 0x1;
+        }
+
+        public static void SetPosition(AtkResNode* node, float? x, float? y) {
+            if (x != null) node->X = x.Value;
+            if (y != null) node->Y = y.Value;
+            node->Flags_2 |= 0x1;
+        }
+
+        public static void SetWindowSize(AtkComponentNode* windowNode, ushort? width, ushort? height) {
+            if (((ULDComponentInfo*) windowNode->Component->ULDData.Objects)->ComponentType != (byte) ComponentType.Window) return;
+
+            width ??= windowNode->AtkResNode.Width;
+            height ??= windowNode->AtkResNode.Height;
+
+            if (width < 64) width = 64;
+
+            SetSize(windowNode, width, height);  // Window
+            var n = windowNode->Component->ULDData.RootNode;
+            SetSize(n, width, height);  // Collision
+            n = n->PrevSiblingNode;
+            SetSize(n, (ushort)(width - 14), null); // Header Collision
+            n = n->PrevSiblingNode;
+            SetSize(n, width, height); // Background
+            n = n->PrevSiblingNode;
+            SetSize(n, width, height); // Focused Border
+            n = n->PrevSiblingNode;
+            SetSize(n, (ushort) (width - 5), null); // Header Node
+            n = n->ChildNode;
+            SetSize(n, (ushort) (width - 20), null); // Header Seperator
+            n = n->PrevSiblingNode;
+            SetPosition(n, width - 33, 6); // Close Button
+            n = n->PrevSiblingNode;
+            SetPosition(n, width - 47, 8); // Gear Button
+            n = n->PrevSiblingNode;
+            SetPosition(n, width - 61, 8); // Help Button
+
+            windowNode->AtkResNode.Flags_2 |= 0x1;
+        }
+
+    }
+}
