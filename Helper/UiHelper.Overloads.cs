8a0ae38c7010bc651e2f7114e2e92f8efc14f27b
Cara <git@caraxian.com>
2021-04-17 23:13:59 +0930

    Simplify UiModule.OPverloads

--- Helper/UiHelper.Overloads.cs
+++ Helper/UiHelper.Overloads.cs
@@ -2,26 +2,10 @@
 
 namespace SimpleTweaksPlugin.Helper {
     public static unsafe partial class UiHelper {
-        public static void Hide(AtkTextNode* node) => Hide((AtkResNode*)node);
-        public static void Hide(AtkNineGridNode* node) => Hide((AtkResNode*)node);
-        public static void Hide(AtkImageNode* node) => Hide((AtkResNode*)node);
-        public static void Hide(AtkComponentNode* node) => Hide((AtkResNode*)node);
-        public static void Hide(AtkCollisionNode* node) => Hide((AtkResNode*)node);
-
-        public static void Show(AtkTextNode* node) => Show((AtkResNode*)node);
-        public static void Show(AtkNineGridNode* node) => Show((AtkResNode*)node);
-        public static void Show(AtkImageNode* node) => Show((AtkResNode*)node);
-        public static void Show(AtkComponentNode* node) => Show((AtkResNode*)node);
-        public static void Show(AtkCollisionNode* node) => Show((AtkResNode*)node);
-
-        public static void SetSize(AtkComponentNode* node, int? w, int? h) => SetSize((AtkResNode*) node, w, h);
-        public static void SetSize(AtkTextNode* node, int? w, int? h) => SetSize((AtkResNode*) node, w, h);
-        public static void SetSize(AtkCollisionNode* node, int? w, int? h) => SetSize((AtkResNode*)node, w, h);
-
-        public static void SetPosition(AtkComponentNode* node, float? x, float? y) => SetPosition((AtkResNode*) node, x, y);
-        public static void SetPosition(AtkTextNode* node, float? x, float? y) => SetPosition((AtkResNode*) node, x, y);
-        public static void SetPosition(AtkCollisionNode* node, float? x, float? y) => SetPosition((AtkResNode*)node, x, y);
-
-        public static AtkTextNode* CloneNode(AtkTextNode* node) => (AtkTextNode*) CloneNode((AtkResNode*) node);
+        public static void Hide<T>(T* node) where T : unmanaged => Hide((AtkResNode*)node);
+        public static void Show<T>(T* node) where T : unmanaged => Show((AtkResNode*)node);
+        public static void SetSize<T>(T* node, int? w, int? h) where T : unmanaged => SetSize((AtkResNode*) node, w, h);
+        public static void SetPosition<T>(T* node, float? x, float? y) where T : unmanaged => SetPosition((AtkResNode*) node, x, y);
+        public static T* CloneNode<T>(T* original) where T : unmanaged => (T*) CloneNode((AtkResNode*) original);
     }
 }
