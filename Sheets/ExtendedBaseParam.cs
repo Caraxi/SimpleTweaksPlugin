c8cc4ee3da7d14d22b1a271e0a85e51a02c5e879
Cara <git@caraxian.com>
2021-05-02 16:40:48 +0930

    Update ExtendedBaseParam.cs

--- Sheets/ExtendedBaseParam.cs
+++ Sheets/ExtendedBaseParam.cs
@@ -7,11 +7,9 @@ namespace SimpleTweaksPlugin.Sheets {
     [Sheet( "BaseParam")]
     public class ExtendedBaseParam : Lumina.Excel.GeneratedSheets.BaseParam {
         public readonly byte[] EquipSlotCategoryPct = new byte[22];
-        public SeString Name;
-        
+
         public override void PopulateData(RowParser parser, GameData gameData, Language language) {
             base.PopulateData(parser, gameData, language);
-            Name = parser.ReadColumn<SeString>(1);
             for (var i = 1; i < EquipSlotCategoryPct.Length; i++) {
                 EquipSlotCategoryPct[i] = parser.ReadColumn<byte>(i + 3);
             }
