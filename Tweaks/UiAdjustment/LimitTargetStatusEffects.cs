9830d3cdfadef9092de37ca9ff22ed1090958656
Aireil <33433913+Aireil@users.noreply.github.com>
2021-06-02 13:32:49 +0200

    Add only in combat option to `Limit Target Status Effects`

--- Tweaks/UiAdjustment/LimitTargetStatusEffects.cs
+++ Tweaks/UiAdjustment/LimitTargetStatusEffects.cs
@@ -18,15 +18,18 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
 
         public class Configs {
             public int NbStatusEffects = 30;
+            public bool LimitOnlyInCombat = false;
         }
 
         public Configs Config => PluginConfig.UiAdjustments.LimitTargetStatusEffects;
+        private bool isDirty = false;
 
         protected override DrawConfigDelegate DrawConfigTree => (ref bool hasChanged) => {
             ImGui.SetNextItemWidth(100 * ImGui.GetIO().FontGlobalScale);
             hasChanged |= ImGui.InputInt("Number of status effects displayed##nbStatusEffectsDisplayed", ref Config.NbStatusEffects, 1);
             if (Config.NbStatusEffects < 0) Config.NbStatusEffects = 0;
             if (Config.NbStatusEffects > 30) Config.NbStatusEffects = 30;
+            hasChanged |= ImGui.Checkbox("Only limit in combat##LimitOnlyInCombat", ref Config.LimitOnlyInCombat);
 
             UpdateTargetStatus(true);
         };
@@ -50,7 +53,7 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
             }
         }
 
-        public void UpdateTargetStatus(bool reset = false) {
+        private void UpdateTargetStatus(bool reset = false) {
             var targetInfoUnitBase = Common.GetUnitBase("_TargetInfo", 1);
             if (targetInfoUnitBase == null) return;
             if (targetInfoUnitBase->UldManager.NodeList == null || targetInfoUnitBase->UldManager.NodeListCount < 53) return;
@@ -59,23 +62,32 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
             if (targetInfoStatusUnitBase == null) return;
             if (targetInfoStatusUnitBase->UldManager.NodeList == null || targetInfoStatusUnitBase->UldManager.NodeListCount < 32) return;
 
-            if (reset) {
-                for (int i = 32; i >= 3; i--) {
+            var isInCombat =
+                this.PluginInterface.ClientState.Condition[Dalamud.Game.ClientState.ConditionFlag.InCombat];
+
+            if (reset || (Config.LimitOnlyInCombat && !isInCombat && this.isDirty)) {
+                for (var i = 32; i >= 3; i--) {
                     targetInfoUnitBase->UldManager.NodeList[i]->Color.A = 255;
                 }
 
-                for (int i = 31; i >= 2; i--) {
+                for (var i = 31; i >= 2; i--) {
                     targetInfoStatusUnitBase->UldManager.NodeList[i]->Color.A = 255;
                 }
+
+                this.isDirty = false;
 
                 return;
             }
 
-            for (int i = 32 - Config.NbStatusEffects; i >= 3; i--) {
+            if (Config.LimitOnlyInCombat && !isInCombat) return;
+
+            this.isDirty = true;
+
+            for (var i = 32 - Config.NbStatusEffects; i >= 3; i--) {
                 targetInfoUnitBase->UldManager.NodeList[i]->Color.A = 0;
             }
 
-            for (int i = 31 - Config.NbStatusEffects; i >= 2; i--) {
+            for (var i = 31 - Config.NbStatusEffects; i >= 2; i--) {
                 targetInfoStatusUnitBase->UldManager.NodeList[i]->Color.A = 0;
             }
         }
