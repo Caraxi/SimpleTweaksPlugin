3ea91c2cb1df1398a5311f5f24b67cba74eb4260
Cara <git@caraxian.com>
2021-08-03 16:50:01 +0930

    [`Party List Layout`] Fix Glitchy Chocobo and Pet position

--- Tweaks/UiAdjustment/PartyListLayout.cs
+++ Tweaks/UiAdjustment/PartyListLayout.cs
@@ -27,33 +27,38 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
 
         public override bool UseAutoConfig => true;
 
+        private delegate void* PartyListOnUpdate(AddonPartyList* @this, void* a2, void* a3);
+        private HookWrapper<PartyListOnUpdate> partyListOnUpdateHook;
+        
+        
         public override void Enable() {
             Config = LoadConfig<Configs>() ?? new Configs();
-            PluginInterface.Framework.OnUpdateEvent += FrameworkUpdate;
+            partyListOnUpdateHook ??= Common.Hook<PartyListOnUpdate>("48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 48 8B 7A 20", PartyListUpdateDetour, false);
+            partyListOnUpdateHook?.Enable();
             base.Enable();
         }
-        
-        public override void Disable() {
-            SaveConfig(Config);
-            PluginInterface.Framework.OnUpdateEvent -= FrameworkUpdate;
-            Update(true);
-            base.Disable();
-        }
-        
-        private void FrameworkUpdate(Framework framework) {
+
+        private void* PartyListUpdateDetour(AddonPartyList* @this, void* a2, void* a3) {
+            var ret = partyListOnUpdateHook.Original(@this, a2, a3);
             try {
-                Update();
+                Update(@this);
             } catch (Exception ex) {
                 SimpleLog.Error(ex);
             }
+            return ret;
+        }
+
+        public override void Disable() {
+            partyListOnUpdateHook?.Disable();
+            SaveConfig(Config);
+            Update(Common.GetUnitBase<AddonPartyList>(), true);
+            base.Disable();
         }
 
         private const int XSeparation = 260;
         private const int YSeparation = 80;
         
-        private void Update(bool reset = false) {
-
-            var partyList = Common.GetUnitBase<AddonPartyList>();
+        private void Update(AddonPartyList* partyList, bool reset = false) {
             if (partyList == null) return;
             
             if (partyList->AtkUnitBase.UldManager.NodeListSize < 17) return;
