21566fb506e758bf5fd2e239299a0bf08ffe8520
Cara <git@caraxian.com>
2021-09-12 02:05:12 +0930

    Shhh you didn't see anything

---
+++ Tweaks/Secret/NoDozeSnap.cs
@@ -0,0 +1,27 @@
+﻿using SimpleTweaksPlugin.Helper;
+
+namespace SimpleTweaksPlugin.Tweaks.Secret {
+    public class DozeSnap : SecretTweaks.SubTweak {
+        public override string Name => "No Doze Snap";
+        private delegate bool ShouldSnap();
+        private HookWrapper<ShouldSnap> shouldSnapHook;
+
+        public override void Enable() {
+            shouldSnapHook ??= Common.Hook<ShouldSnap>("E8 ?? ?? ?? ?? 84 C0 74 46 4C 8D 6D C7", ShouldSnapDetour);
+            shouldSnapHook?.Enable();
+            base.Enable();
+        }
+
+        private static bool ShouldSnapDetour() => false;
+
+        public override void Disable() {
+            shouldSnapHook?.Disable();
+            base.Disable();
+        }
+
+        public override void Dispose() {
+            shouldSnapHook?.Dispose();
+            base.Dispose();
+        }
+    }
+}
---
+++ Tweaks/SecretTweaks.cs
@@ -0,0 +1,13 @@
+﻿using SimpleTweaksPlugin.TweakSystem;
+
+namespace SimpleTweaksPlugin.Tweaks {
+    public class SecretTweaks : SubTweakManager<SecretTweaks.SubTweak> {
+        public override bool AlwaysEnabled => true;
+
+        public abstract class SubTweak : BaseTweak {
+            public override string Key => $"{nameof(SecretTweaks)}@{base.Key}";
+        }
+
+        public override string Name => "Secret";
+    }
+}
