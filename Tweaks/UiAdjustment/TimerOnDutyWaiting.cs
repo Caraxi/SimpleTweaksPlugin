f341fca89bbd23e2d7136def508149928d04891e
Cara <git@caraxian.com>
2021-10-30 17:12:32 +1030

    [`Timer on Duty Waiting`] Add some more null checks
    Maybe prevent a crash?

--- Tweaks/UiAdjustment/TimerOnDutyWaiting.cs
+++ Tweaks/UiAdjustment/TimerOnDutyWaiting.cs
@@ -12,7 +12,7 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
 
         public override void Enable() {
             Service.Framework.Update += UpdateFramework;
-            prefix = Service.Data.Excel.GetSheet<Addon>().GetRow(2780).Text.RawString;
+            prefix = Service.Data.Excel.GetSheet<Addon>()?.GetRow(2780)?.Text?.RawString ?? "Checking member status...";
             base.Enable();
         }
 
@@ -25,7 +25,7 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
         private void UpdateFramework(Framework framework) {
             try {
                 var confirmWindow = Common.GetUnitBase("ContentsFinderConfirm");
-                if (confirmWindow != null) {
+                if (confirmWindow != null && confirmWindow->UldManager.NodeList != null) {
                     var timerTextNode = (AtkTextNode*) confirmWindow->UldManager.NodeList[10];
                     if (timerTextNode == null) return;
                     var text = Plugin.Common.ReadSeString(timerTextNode->NodeText.StringPtr).TextValue;
@@ -47,7 +47,7 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
                 }
                 
                 var readyWindow = Common.GetUnitBase("ContentsFinderReady");
-                if (readyWindow == null) return;
+                if (readyWindow == null || readyWindow->UldManager.NodeList == null) return;
 
                 var checkingTextNode = (AtkTextNode*) readyWindow->UldManager.NodeList[7];
                 if (checkingTextNode == null) return;
