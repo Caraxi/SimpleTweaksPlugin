82e858d76c3c833ed2af6ebbef369653907cf3d0
Cara <git@caraxian.com>
2021-12-10 12:24:50 +1030

    [`Equipment Positions`] Adjust bag widget layout
    Need to work out how to draw a background on it

--- Tweaks/UiAdjustment/GearPositions.cs
+++ Tweaks/UiAdjustment/GearPositions.cs
@@ -10,13 +10,54 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
 
         private HookWrapper<AddonOnSetup> characterOnSetup;
         private HookWrapper<AddonOnSetup> inspectOnSetup;
+        private HookWrapper<Common.AddonOnUpdate> bagWidgetUpdate;
 
         public override void Enable() {
             characterOnSetup ??= Common.Hook<AddonOnSetup>("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 41 54 41 55 41 56 41 57 48 83 EC 60 4D 8B F0", CharacterOnSetup);
             inspectOnSetup ??= Common.Hook<AddonOnSetup>("48 89 5C 24 ?? 48 89 6C 24 ?? 56 57 41 56 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 84 24 ?? ?? ?? ?? 48 8B F9 48 8B D1", InspectOnSetup);
+            bagWidgetUpdate ??= Common.HookAfterAddonUpdate("48 89 5C 24 ?? 55 56 57 41 54 41 55 41 56 41 57 48 83 EC 20 4C 8B 62 38", BagWidgetUpdate);
             characterOnSetup?.Enable();
             inspectOnSetup?.Enable();
+            bagWidgetUpdate?.Enable();
+
+            var bagWidget = Common.GetUnitBase("_BagWidget");
+            if (bagWidget != null) BagWidgetUpdate(bagWidget, null, null);
+
             base.Enable();
+        }
+
+        private void BagWidgetUpdate(AtkUnitBase* atkUnitBase, NumberArrayData** numberArrayData, StringArrayData** stringArrayData) {
+            var equipmentComponentNode = atkUnitBase->GetNodeById(6);
+            if (equipmentComponentNode == null) return;
+            if ((ushort)equipmentComponentNode->Type < 1000) return;
+            var equipmentComponent = ((AtkComponentNode*)equipmentComponentNode)->Component;
+            if (equipmentComponent == null) return;
+            MoveNode(equipmentComponent, 3, 5, 10);
+            MoveNode(equipmentComponent, 4, 23, 10);
+            for (var i = 5U; i < 10; i++) MoveNode(equipmentComponent, i, 5, 10 + (i - 4) * 6);
+            for (var i = 10U; i < 15; i++) MoveNode(equipmentComponent, i, 23, 10 + (i - 9) * 6);
+
+            var backgroundImage = (AtkImageNode*) equipmentComponent->UldManager.SearchNodeById(15);
+            if (backgroundImage != null) {
+                backgroundImage->AtkResNode.ToggleVisibility(false);
+            }
+        }
+
+        private void ResetBagWidget(AtkUnitBase* atkUnitBase) {
+            var equipmentComponentNode = atkUnitBase->GetNodeById(6);
+            if (equipmentComponentNode == null) return;
+            if ((ushort)equipmentComponentNode->Type < 1000) return;
+            var equipmentComponent = ((AtkComponentNode*)equipmentComponentNode)->Component;
+            if (equipmentComponent == null) return;
+            MoveNode(equipmentComponent, 3, 5, 5);
+            MoveNode(equipmentComponent, 4, 23, 13);
+            for (var i = 5U; i < 10; i++) MoveNode(equipmentComponent, i, 5, 13 + (i - 5) * 6);
+            for (var i = 10U; i < 15; i++) MoveNode(equipmentComponent, i, 23, 19 + (i - 10) * 6);
+
+            var backgroundImage = (AtkImageNode*) equipmentComponent->UldManager.SearchNodeById(15);
+            if (backgroundImage != null) {
+                backgroundImage->AtkResNode.ToggleVisibility(true);
+            }
         }
 
         private void* InspectOnSetup(AtkUnitBase* atkUnitBase, int a2, void* a3) {
@@ -66,6 +107,13 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
             return retVal;
         }
 
+        private void MoveNode(AtkComponentBase* componentBase, uint nodeId, float x, float y) {
+            if (componentBase == null) return;
+            var node = componentBase->UldManager.SearchNodeById(nodeId);
+            if (node == null) return;
+            node->SetPositionFloat(x, y);
+        }
+
         private void MoveNode(AtkUnitBase* atkUnitBase, uint nodeId, float x, float y) {
             if (atkUnitBase == null) return;
             var node = atkUnitBase->GetNodeById(nodeId);
@@ -74,14 +122,18 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
         }
 
         public override void Disable() {
+            var bagWidget = Common.GetUnitBase("_BagWidget");
+            if (bagWidget != null) ResetBagWidget(bagWidget);
             characterOnSetup?.Disable();
             inspectOnSetup?.Disable();
+            bagWidgetUpdate?.Disable();
             base.Disable();
         }
 
         public override void Dispose() {
             characterOnSetup?.Dispose();
             inspectOnSetup?.Dispose();
+            bagWidgetUpdate?.Dispose();
             base.Dispose();
         }
     }
