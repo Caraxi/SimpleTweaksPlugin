3b97b9eb932c15451ab42697df659e8c199022a0
Cara <git@caraxian.com>
2021-12-11 12:25:07 +1030

    Add Agent Viewer

---
+++ Helper/Colour.cs
@@ -0,0 +1,8 @@
+﻿using System.Numerics;
+
+namespace SimpleTweaksPlugin.Helper {
+    public static class Colour {
+        public static readonly Vector4 Red = new(1, 0, 0, 1);
+        public static readonly Vector4 Green = new(0, 1, 0, 1);
+    }
+}
