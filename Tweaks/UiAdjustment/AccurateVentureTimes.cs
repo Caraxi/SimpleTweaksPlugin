f47300f783edb5a2a76652dd12e835a1af4be737
Cara <git@caraxian.com>
2021-12-14 21:25:50 +1030

    [`Accurate Venture Times`] Rework entry point for less glitchy display

--- Tweaks/UiAdjustment/AccurateVentureTimes.cs
+++ Tweaks/UiAdjustment/AccurateVentureTimes.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using Dalamud;
 using Dalamud.Game;
 using FFXIVClientStructs.FFXIV.Client.Game;
@@ -22,29 +24,57 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
 
         public override bool UseAutoConfig => true;
 
+        private delegate void UpdateRetainerDelegate(void* a1, int index, void* a3, void* a4);
+
+        private UpdateRetainerDelegate updateRetainer;
+
+        private void* updateRetainerPointer;
+        private delegate void* AddonSetupDelegate(void* a1, AtkUnitBase* a2, void* a3);
+        private HookWrapper<AddonSetupDelegate> addonSetupHook;
+
+        private UpdateRetainerDelegate replacementUpdateRetainerDelegate;
+
         public override void Enable() {
             Config = LoadConfig<Configs>() ?? new Configs();
+            replacementUpdateRetainerDelegate = UpdateRetainerLineDetour;
+            updateRetainerPointer = (void*) Common.Scanner.ScanText("40 53 56 41 56 48 83 EC 30 48 8B B1");
+            updateRetainer = Marshal.GetDelegateForFunctionPointer<UpdateRetainerDelegate>(new IntPtr(updateRetainerPointer));
+            addonSetupHook ??= Common.Hook("E8 ?? ?? ?? ?? 41 B1 1E", new AddonSetupDelegate(SetupDetour));
+            addonSetupHook?.Enable();
             Service.Framework.Update += FrameworkOnUpdate;
             base.Enable();
         }
 
-        private int updateThrottle;
+        private void* SetupDetour(void* a1, AtkUnitBase* a2, void* a3) {
+            if (a3 == updateRetainerPointer) {
+                var ptr = Marshal.GetFunctionPointerForDelegate(replacementUpdateRetainerDelegate);
+                a3 = (void*) ptr;
+            }
+            return addonSetupHook.Original(a1, a2, a3);
+        }
 
-        private void FrameworkOnUpdate(Framework framework) {
-            updateThrottle++;
-            if (updateThrottle > 10) {
-                updateThrottle = 0;
+        private void UpdateRetainerLineDetour(void* a1, int index, void* a3, void* a4) {
+            updateRetainer(a1, index, a3, a4);
+            try {
+                UpdateRetainerList();
+            } catch {
+                //
+            }
+        }
+
+        private bool UpdateRetainerList() {
+            try {
                 var addon = Common.GetUnitBase("RetainerList");
-                if (addon == null) return;
+                if (addon == null) return false;
                 var listNode = (AtkComponentNode*)addon->GetNodeById(24);
-                if (listNode == null || (ushort)listNode->AtkResNode.Type < 1000) return;
+                if (listNode == null || (ushort)listNode->AtkResNode.Type < 1000) return false;
                 var retainerManager = RetainerManager.Instance();
                 for (uint i = 0; i < 10; i++) {
                     var retainer = retainerManager->GetRetainerBySortedIndex(i);
 
                     if (retainer->VentureComplete != 0) {
                         var renderer = Common.GetNodeByID<AtkComponentNode>(listNode->Component->UldManager, i == 0 ? 4U : 41000U + i, (NodeType) 1011);
-                        if (renderer == null || !renderer->AtkResNode.IsVisible) return;
+                        if (renderer == null || !renderer->AtkResNode.IsVisible) continue;
                         var ventureText = (AtkTextNode*) renderer->Component->UldManager.SearchNodeById(12);
                         var cTime = FFXIVClientStructs.FFXIV.Client.System.Framework.Framework.GetServerTime();
                         var rTime = retainer->VentureComplete - cTime;
@@ -95,13 +125,41 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
                         }
                     }
                 }
+
+                return true;
+            } catch (Exception ex) {
+                SimpleLog.Error(ex);
+                return false;
             }
+        }
+
+        private readonly Stopwatch sw = new();
+
+        private void FrameworkOnUpdate(Framework framework) {
+            if (!sw.IsRunning) sw.Restart();
+            if (sw.ElapsedMilliseconds >= 1000) {
+                if (!UpdateRetainerList()) {
+                    sw.Restart();
+                }
+            }
+        }
+
+        private void CloseRetainerList() {
+            var rl = Common.GetUnitBase("RetainerList");
+            if (rl != null) UiHelper.Close(rl, true);
         }
 
         public override void Disable() {
             Service.Framework.Update -= FrameworkOnUpdate;
+            addonSetupHook?.Disable();
+            CloseRetainerList();
             SaveConfig(Config);
             base.Disable();
+        }
+
+        public override void Dispose() {
+            addonSetupHook?.Dispose();
+            base.Dispose();
         }
     }
 }
