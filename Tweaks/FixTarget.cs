4230a99fb190f8f7cd08dd77d36ab89cd08444f0
Aireil <33433913+Aireil@users.noreply.github.com>
2021-12-19 22:49:45 +0100

    [`FixTarget`] Ignore invisible objects

--- Tweaks/FixTarget.cs
+++ Tweaks/FixTarget.cs
@@ -7,6 +7,7 @@ using Dalamud.Game.Text.SeStringHandling;
 using Dalamud.Game.ClientState.Objects.Types;
 using SimpleTweaksPlugin.Helper;
 using SimpleTweaksPlugin.TweakSystem;
+using GameObjectStruct = FFXIVClientStructs.FFXIV.Client.Game.Object.GameObject;
 
 namespace SimpleTweaksPlugin.Tweaks {
     public class FixTarget : Tweak {
@@ -14,19 +15,19 @@ namespace SimpleTweaksPlugin.Tweaks {
         public override string Description => "Allows using the default '/target' command for targeting players or NPCs by their names.";
 
         private Regex regex;
-        
+
         public override void Enable() {
-            
+
             regex = Service.ClientState.ClientLanguage switch {
                 ClientLanguage.Japanese => new Regex(@"^\d+?番目のターゲット名の指定が正しくありません。： (.+)$"),
                 ClientLanguage.German => new Regex(@"^Der Unterbefehl \[Name des Ziels\] an der \d+\. Stelle des Textkommandos \((.+)\) ist fehlerhaft\.$"),
-                ClientLanguage.French => new Regex(@"^Le \d+er? argument “nom de la cible” est incorrect (.*?)\.$"), 
+                ClientLanguage.French => new Regex(@"^Le \d+er? argument “nom de la cible” est incorrect (.*?)\.$"),
                 ClientLanguage.English => new Regex(@"^“(.+)” is not a valid target name\.$"),
                 _ => null
             };
-            
+
             Service.Chat.ChatMessage += OnChatMessage;
-            
+
             base.Enable();
         }
 
@@ -34,7 +35,7 @@ namespace SimpleTweaksPlugin.Tweaks {
             Service.Chat.ChatMessage -= OnChatMessage;
             base.Disable();
         }
-        
+
         private unsafe void OnChatMessage(XivChatType type, uint senderid, ref SeString sender, ref SeString message, ref bool isHandled) {
             if (type != XivChatType.ErrorMessage) return;
             if (Common.LastCommand == null || Common.LastCommand->StringPtr == null) return;
@@ -51,9 +52,10 @@ namespace SimpleTweaksPlugin.Tweaks {
             var closestDistance = float.MaxValue;
             var player = Service.ClientState.LocalPlayer;
             foreach (var actor in Service.Objects) {
-                
+
                 if (actor == null) continue;
-                if (actor.Name.TextValue.ToLowerInvariant().Contains(searchName)) {
+                if (actor.Name.TextValue.ToLowerInvariant().Contains(searchName) &&
+                    ((GameObjectStruct*)actor.Address)->GetIsTargetable()) {
                     var distance = Vector3.Distance(player.Position, actor.Position);
                     if (closestMatch == null) {
                         closestMatch = actor;
