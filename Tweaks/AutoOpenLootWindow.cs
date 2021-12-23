fde1144f0c427a24c8de6a6164967ff6714b08eb
Chivalrik <77893938+fitzchivalrik@users.noreply.github.com>
2021-12-23 08:03:40 +0100

    fix: Do not show loot window if in cutscene (#170)
    * fix: Do not not show loot window if in cutscene

--- Tweaks/AutoOpenLootWindow.cs
+++ Tweaks/AutoOpenLootWindow.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Dalamud;
+using Dalamud.Game;
+using Dalamud.Game.ClientState.Conditions;
 using Dalamud.Game.Text;
 using Dalamud.Game.Text.SeStringHandling;
 using FFXIVClientStructs.FFXIV.Component.GUI;
@@ -17,18 +19,48 @@ namespace SimpleTweaksPlugin.Tweaks {
             base.Enable();
         }
 
-        private static void HandleChat(XivChatType type, uint senderId, ref SeString sender, ref SeString message, ref bool isHandled) {
+        private void HandleChat(XivChatType type, uint senderId, ref SeString sender, ref SeString message, ref bool isHandled) {
             try {
                 if ((ushort)type != 2105) return;
-                if (message.TextValue == Service.ClientState.ClientLanguage switch {
+                if (message.TextValue == Service.ClientState.ClientLanguage switch
+                {
                     ClientLanguage.German => "Bitte um das Beutegut würfeln.",
                     ClientLanguage.French => "Veuillez lancer les dés pour le butin.",
                     ClientLanguage.Japanese => "ロットを行ってください。",
                     _ => "Cast your lot."
-                }) TryOpenWindow();
+                })
+                {
+                    if (Service.Condition[ConditionFlag.WatchingCutscene]
+                        || Service.Condition[ConditionFlag.WatchingCutscene78]
+                        || Service.Condition[ConditionFlag.OccupiedInCutSceneEvent])
+                    {
+                        Service.Framework.Update -= TryOpenAfterCutsceneFrameworkUpdate;
+                        Service.Framework.Update += TryOpenAfterCutsceneFrameworkUpdate;
+                    }
+                    else
+                    {
+                        TryOpenWindow();
+                    }
+                }
             } catch (Exception ex) {
                 SimpleLog.Error(ex);
             }
+        }
+
+        private byte throttle;
+        private void TryOpenAfterCutsceneFrameworkUpdate(Framework framework)
+        {
+            throttle++;
+            if (throttle <= 10) return;
+            throttle = 0;
+            if (Service.Condition[ConditionFlag.WatchingCutscene]
+                || Service.Condition[ConditionFlag.WatchingCutscene78]
+                || Service.Condition[ConditionFlag.OccupiedInCutSceneEvent])
+            {
+                return;
+            }
+            Service.Framework.Update -= TryOpenAfterCutsceneFrameworkUpdate;
+            TryOpenWindow();
         }
 
         private static void TryOpenWindow() {
@@ -51,6 +83,7 @@ namespace SimpleTweaksPlugin.Tweaks {
 
         public override void Disable() {
             Service.Chat.CheckMessageHandled -= HandleChat;
+            Service.Framework.Update -= TryOpenAfterCutsceneFrameworkUpdate;
             base.Disable();
         }
     }
