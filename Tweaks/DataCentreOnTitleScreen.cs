3dd1c46d230d30d78647aa7c99d3efea1347ed5b
Cara <git@caraxian.com>
2022-02-04 19:33:39 +1030

    Add Tweak: Data Centre on Title Screen
    An actually simple tweak

---
+++ Tweaks/DataCentreOnTitleScreen.cs
@@ -0,0 +1,37 @@
+﻿using Dalamud.Game;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using Lumina.Excel.GeneratedSheets;
+using SimpleTweaksPlugin.Helper;
+using SimpleTweaksPlugin.TweakSystem;
+
+namespace SimpleTweaksPlugin.Tweaks {
+    public unsafe class DataCentreOnTitleScreen : Tweak {
+        public override string Name => "Data Centre on Title Screen";
+        public override string Description => "Shows the current Data Centre on the Title Screen";
+
+        public override void Enable() {
+            Service.Framework.Update += FrameworkOnUpdate;
+            base.Enable();
+        }
+
+        private void FrameworkOnUpdate(Framework framework) {
+            if (Service.Condition.Any()) return;
+            var addon = Common.GetUnitBase("_TitleMenu");
+            if (addon == null) return;
+            var dc = Service.Data.Excel.GetSheet<WorldDCGroupType>()?.GetRow(AgentLobby.Instance()->DataCenter);
+            if (dc == null) return;
+            var button = (AtkComponentNode*) addon->GetNodeById(5);
+            if (button == null || (ushort) button->AtkResNode.Type < 1000) return;
+            var text = (AtkTextNode*) button->Component->UldManager.SearchNodeById(3);
+            if (text == null || text->AtkResNode.Type != NodeType.Text) return;
+            text->SetText(dc.Name.RawData.ToArray());
+        }
+
+        public override void Disable() {
+            Service.Framework.Update -= FrameworkOnUpdate;
+            base.Disable();
+        }
+    }
+}
+
