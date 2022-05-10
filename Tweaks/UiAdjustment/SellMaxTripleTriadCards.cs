5f0d233577a5f0f5ed1f45f1cd39720d470f6c06
Cara <git@caraxian.com>
2022-05-10 22:24:55 +0930

    [`Sell Max Triad Cards`] Use Common.AddonSetup

--- Tweaks/UiAdjustment/SellMaxTripleTriadCards.cs
+++ Tweaks/UiAdjustment/SellMaxTripleTriadCards.cs
@@ -6,31 +6,23 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment;
 public unsafe class SellMaxTripleTriadCards : UiAdjustments.SubTweak {
     public override string Name => "Default to max when selling Triple Triad Cards";
     public override string Description => "Set the default number of cards to sell at the Triple Triad Trader to the number of cards you have.";
-
-    private delegate void* AddonCardShopDialogOnSetup(AddonShopCardDialog* addonShopCardDialog, uint a2, void* a3);
-    private HookWrapper<AddonCardShopDialogOnSetup> setupHook;
-
+    
     public override void Enable() {
-        setupHook ??= Common.Hook<AddonCardShopDialogOnSetup>("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 48 89 7C 24 ?? 41 54 41 56 41 57 48 83 EC 50 48 8B F9", SetupDetour);
-        setupHook?.Enable();
+        Common.AddonSetup += SetupDetour;
         base.Enable();
     }
 
-    private void* SetupDetour(AddonShopCardDialog* addonShopCardDialog, uint a2, void* a3) {
-        var ret = setupHook.Original(addonShopCardDialog, a2, a3);
-        if (addonShopCardDialog == null) return ret;
-        if (addonShopCardDialog->CardQuantityInput == null) return ret;
+    private void SetupDetour(SetupAddonArgs args) {
+        if (args.AddonName != "ShopCardDialog") return;
+        var addonShopCardDialog = (AddonShopCardDialog*)args.Addon;
+        if (addonShopCardDialog == null) return;
+        if (addonShopCardDialog->CardQuantityInput == null) return;
         addonShopCardDialog->CardQuantityInput->SetValue(addonShopCardDialog->CardQuantityInput->Data.Max);
-        return ret;
+        return;
     }
 
     public override void Disable() {
-        setupHook?.Disable();
+        Common.AddonSetup -= SetupDetour;
         base.Disable();
-    }
-
-    public override void Dispose() {
-        setupHook?.Dispose();
-        base.Dispose();
     }
 }
