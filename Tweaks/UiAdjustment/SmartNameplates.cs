fb6a37e2173bcd274dd394ba222ce87614884b5b
Aireil <33433913+Aireil@users.noreply.github.com>
2022-05-20 22:55:50 +0200

    Fix configs not saving on window close (#308)

--- Tweaks/UiAdjustment/SmartNameplates.cs
+++ Tweaks/UiAdjustment/SmartNameplates.cs
@@ -26,7 +26,7 @@ public unsafe class SmartNameplates : UiAdjustments.SubTweak {
         public bool IgnoreTargets = false;
     }
 
-    private Configs config;
+    public Configs Config { get; private set; }
 
     private const int statusFlagsOffset = 0x19A0;
     private IntPtr targetManager = IntPtr.Zero;
@@ -36,18 +36,18 @@ public unsafe class SmartNameplates : UiAdjustments.SubTweak {
     private GetTargetTypeDelegate GetTargetType;
 
     protected override DrawConfigDelegate DrawConfigTree => (ref bool _) => {
-        ImGui.Checkbox(LocString("Show HP") + "##SmartNameplatesShowHP", ref config.ShowHP);
+        ImGui.Checkbox(LocString("Show HP") + "##SmartNameplatesShowHP", ref Config.ShowHP);
         if (ImGui.IsItemHovered())
             ImGui.SetTooltip(LocString("ShopHpTooltip", "Will not hide HP bars for affected players."));
 
         ImGui.Spacing();
         ImGui.Spacing();
         ImGui.TextUnformatted(LocString("IgnorePlayersNote", "The following options will disable the tweak for certain players."));
-        ImGui.Checkbox(LocString("Ignore Party Members") + "##SmartNameplatesIgnoreParty", ref config.IgnoreParty);
-        ImGui.Checkbox(LocString("Ignore Alliance Members") + "##SmartNameplatesIgnoreAlliance", ref config.IgnoreAlliance);
-        ImGui.Checkbox(LocString("Ignore Friends") + "##SmartNameplatesIgnoreFriends", ref config.IgnoreFriends);
-        ImGui.Checkbox(LocString("Ignore Dead Players") + "##SmartNameplatesIgnoreDead", ref config.IgnoreDead);
-        ImGui.Checkbox(LocString("Ignore Targeted Players") + "##SmartNameplatesIgnoreTargets", ref config.IgnoreTargets);
+        ImGui.Checkbox(LocString("Ignore Party Members") + "##SmartNameplatesIgnoreParty", ref Config.IgnoreParty);
+        ImGui.Checkbox(LocString("Ignore Alliance Members") + "##SmartNameplatesIgnoreAlliance", ref Config.IgnoreAlliance);
+        ImGui.Checkbox(LocString("Ignore Friends") + "##SmartNameplatesIgnoreFriends", ref Config.IgnoreFriends);
+        ImGui.Checkbox(LocString("Ignore Dead Players") + "##SmartNameplatesIgnoreDead", ref Config.IgnoreDead);
+        ImGui.Checkbox(LocString("Ignore Targeted Players") + "##SmartNameplatesIgnoreTargets", ref Config.IgnoreTargets);
     };
 
     // returns 2 bits (b01 == display name, b10 == display hp)
@@ -61,17 +61,17 @@ public unsafe class SmartNameplates : UiAdjustments.SubTweak {
             || (pc->Character.StatusFlags & (byte) StatusFlags.InCombat) == 0 // Alternate in combat flag
             || GetTargetType(actor) == 3
 
-            || (config.IgnoreParty && (pc->Character.StatusFlags & (byte) StatusFlags.PartyMember) != 0) // Ignore party members
-            || (config.IgnoreAlliance && (pc->Character.StatusFlags & (byte) StatusFlags.AllianceMember) != 0) // Ignore alliance members
-            || (config.IgnoreFriends && (pc->Character.StatusFlags & (byte) StatusFlags.Friend) != 0) // Ignore friends
-            || (config.IgnoreDead && pc->Character.Health == 0) // Ignore dead players
+            || (Config.IgnoreParty && (pc->Character.StatusFlags & (byte) StatusFlags.PartyMember) != 0) // Ignore party members
+            || (Config.IgnoreAlliance && (pc->Character.StatusFlags & (byte) StatusFlags.AllianceMember) != 0) // Ignore alliance members
+            || (Config.IgnoreFriends && (pc->Character.StatusFlags & (byte) StatusFlags.Friend) != 0) // Ignore friends
+            || (Config.IgnoreDead && pc->Character.Health == 0) // Ignore dead players
 
             // Ignore targets
-            || (config.IgnoreTargets && targets->Target == actor
+            || (Config.IgnoreTargets && targets->Target == actor
                 || targets->SoftTarget == actor
                 || targets->FocusTarget == actor)) goto ReturnOriginal;
 
-        return (byte)(config.ShowHP ? (shouldDisplayNameplateHook.Original(raptureAtkModule, actor, localPlayer, distance) & ~1) : 0); // Ignore HP
+        return (byte)(Config.ShowHP ? (shouldDisplayNameplateHook.Original(raptureAtkModule, actor, localPlayer, distance) & ~1) : 0); // Ignore HP
 
 
         ReturnOriginal:
@@ -79,7 +79,7 @@ public unsafe class SmartNameplates : UiAdjustments.SubTweak {
     }
 
     public override void Enable() {
-        config = LoadConfig<Configs>() ?? new Configs();
+        Config = LoadConfig<Configs>() ?? new Configs();
         targetManager = targetManager != IntPtr.Zero ? targetManager : Common.Scanner.GetStaticAddressFromSig("48 8B 05 ?? ?? ?? ?? 48 8D 0D ?? ?? ?? ?? FF 50 ?? 48 85 DB", 3); // Taken from Dalamud
         GetTargetType ??= Marshal.GetDelegateForFunctionPointer<GetTargetTypeDelegate>(Common.Scanner.ScanText("E8 ?? ?? ?? ?? 83 F8 06 0F 87 ?? ?? ?? ?? 48 8D 15 ?? ?? ?? ?? 8B C0"));
         shouldDisplayNameplateHook ??= new Hook<ShouldDisplayNameplateDelegate>(Common.Scanner.ScanText("E8 ?? ?? ?? ?? 89 44 24 40 48 C7 85"), ShouldDisplayNameplateDetour);
@@ -88,7 +88,7 @@ public unsafe class SmartNameplates : UiAdjustments.SubTweak {
     }
 
     public override void Disable() {
-        SaveConfig(config);
+        SaveConfig(Config);
         shouldDisplayNameplateHook?.Disable();
         base.Disable();
     }
