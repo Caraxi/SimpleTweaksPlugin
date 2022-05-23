6d0e82463afc8973f08712d2d7bc975c2d56fdd7
Cara <git@caraxian.com>
2022-05-23 22:06:09 +0930

    [`Target HP`] Make hiding Auto Attack icon optional

--- Tweaks/UiAdjustment/TargetHP.cs
+++ Tweaks/UiAdjustment/TargetHP.cs
@@ -27,6 +27,7 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
             public bool UseCustomColor = false;
             public Vector4 CustomColor = new Vector4(1);
             public byte FontSize = 14;
+            public bool HideAutoAttack = false;
             
             public bool NoFocus;
             public Vector2 FocusPosition = new Vector2(0);
@@ -70,6 +71,7 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
                 ImGui.SameLine();
                 hasChanged |= ImGui.ColorEdit4("##TargetHPCustomColor", ref Config.CustomColor);
             }
+            hasChanged |= ImGui.Checkbox("Hide Auto Attack Icon", ref Config.HideAutoAttack);
             
             ImGui.Dummy(new Vector2(5) * ImGui.GetIO().FontGlobalScale);
             hasChanged |= ImGui.Checkbox("Disable Focus Target HP", ref Config.NoFocus);
@@ -139,7 +141,7 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
             if (unitBase == null || unitBase->UldManager.NodeList == null || unitBase->UldManager.NodeListCount < 40) return;
             var gauge = (AtkComponentNode*) unitBase->UldManager.NodeList[36];
             var textNode = (AtkTextNode*) unitBase->UldManager.NodeList[39];
-            UiHelper.SetSize(unitBase->UldManager.NodeList[37], reset ? 44 : 0, reset ? 20 : 0);
+            UiHelper.SetSize(unitBase->UldManager.NodeList[37], reset || !Config.HideAutoAttack ? 44 : 0, reset || !Config.HideAutoAttack ? 20 : 0);
             UpdateGaugeBar(gauge, textNode, target, Config.Position, Config.UseCustomColor ? Config.CustomColor : null, Config.FontSize, reset);
         }
         private void UpdateFocusTarget(AtkUnitBase* unitBase, GameObject target, bool reset = false) {
@@ -153,7 +155,7 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
             if (unitBase == null || unitBase->UldManager.NodeList == null || unitBase->UldManager.NodeListCount < 9) return;
             var gauge = (AtkComponentNode*) unitBase->UldManager.NodeList[5];
             var textNode = (AtkTextNode*) unitBase->UldManager.NodeList[8];
-            UiHelper.SetSize(unitBase->UldManager.NodeList[6], reset ? 44 : 0, reset ? 20 : 0);
+            UiHelper.SetSize(unitBase->UldManager.NodeList[6], reset || !Config.HideAutoAttack ? 44 : 0, reset || !Config.HideAutoAttack ? 20 : 0);
             UpdateGaugeBar(gauge, textNode, target, Config.Position, Config.UseCustomColor ? Config.CustomColor : null, Config.FontSize, reset);
         }
         
