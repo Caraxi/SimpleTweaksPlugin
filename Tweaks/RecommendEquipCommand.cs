3357f985fab4613760b093682fc826f8017c93f3
Cara <git@caraxian.com>
2022-06-10 17:38:52 +0930

    Add Tweak: `Equip Recommended Command`

---
+++ Tweaks/RecommendEquipCommand.cs
@@ -0,0 +1,33 @@
+﻿using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using SimpleTweaksPlugin.Tweaks.AbstractTweaks;
+using SimpleTweaksPlugin.Utility;
+
+namespace SimpleTweaksPlugin.Tweaks;
+
+public unsafe class RecommendEquipCommand : CommandTweak {
+    public override string Name => "Equip Recommended Command";
+    public override string Description => $"Adds /{Command} to equip recommended gear.";
+    protected override string HelpMessage => "Equips recommended gear.";
+    protected override string Command => "equiprecommended";
+    private static RecommendEquipModule* Module => Framework.Instance()->GetUiModule()->GetRecommendEquipModule();
+    
+    protected override void OnCommand(string args) {
+        if (Module == null) return;
+        Module->SetupRecommendedGear();
+        Common.FrameworkUpdate += DoEquip;
+    }
+
+    private static void DoEquip() {
+        if (Module == null || Module->EquippedMainHand == null) {
+            Common.FrameworkUpdate -= DoEquip;
+            return;
+        }
+        Module->EquipRecommendedGear();
+    }
+
+    public override void Disable() {
+        Common.FrameworkUpdate -= DoEquip;
+        base.Disable();
+    }
+}
