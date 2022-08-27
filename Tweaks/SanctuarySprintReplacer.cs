8a0be1844101ff01a5883790d7516df44cfaab39
KazWolfe <KazWolfe@users.noreply.github.com>
2022-08-26 17:17:48 -0700

    New Tweak: Island Sanctuary Sprint Replacer (#369)
    * New Tweak: Island Sanctuary Sprint Replacer
    
    This tweak aims to redirect uses of the "Sprint" action to "Isle Sprint" while in the Island Sanctuary. Note that at this time it does not edit the hotbar or copy cooldown statistics.
    
    * [`Island Sanctuary Sprint Replacer`] Fix bug with teleport exploding

---
+++ Tweaks/SanctuarySprintReplacer.cs
@@ -0,0 +1,69 @@
+﻿using System.Runtime.InteropServices;
+using Dalamud.Logging;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using SimpleTweaksPlugin.TweakSystem;
+using SimpleTweaksPlugin.Utility;
+
+namespace SimpleTweaksPlugin.Tweaks; 
+
+internal unsafe class SanctuarySprintReplacer : Tweak {
+    // A huge thank you to Pohky who provided the framework and the base code for this tweak.
+
+    public override string Name => "Island Sanctuary Sprint Replacer";
+    public override string Description => "Replaces the normal Sprint action with Isle Sprint while in the Island Sanctuary.";
+    protected override string Author => "KazWolfe";
+    public override bool Experimental => true;
+
+    private const string GetDutyActionIdSignature = "E8 ?? ?? ?? ?? EB 17 33 C9";
+
+    private delegate uint GetDutyActionId(ushort dutyActionSlot);
+    private GetDutyActionId? _getDutyActionId;
+    
+    private delegate byte UseActionDelegate(ActionManager* mgr, ActionType actionType, uint actionID, long targetID, int a4, int a5, int a6, void* a7);
+    private HookWrapper<UseActionDelegate>? _useActionHook;
+
+    public override void Enable() {
+        this._useActionHook ??= Common.Hook<UseActionDelegate>(ActionManager.fpUseAction, this.UseActionDetour);
+        this._useActionHook.Enable();
+
+        if (this._getDutyActionId == null &&
+            Service.SigScanner.TryScanText(GetDutyActionIdSignature, out var ptr)) {
+            this._getDutyActionId = Marshal.GetDelegateForFunctionPointer<GetDutyActionId>(ptr);
+        }
+
+        base.Enable();
+    }
+        
+    public override void Disable() {
+        this._useActionHook?.Disable();
+        
+        base.Disable();
+    }
+
+    public override void Dispose() {
+        this._useActionHook?.Dispose();
+        
+        base.Dispose();
+    }
+
+    private byte UseActionDetour(ActionManager* mgr, ActionType type, uint id, long targetid, int a4, int a5, int a6, void* a7) {
+        if (this._getDutyActionId == null)
+            return this._useActionHook!.Original(mgr, type, id, targetid, a4, a5, a6, a7);
+
+        if (AgentMap.Instance()->CurrentTerritoryId != 1055)
+            return this._useActionHook!.Original(mgr, type, id, targetid, a4, a5, a6, a7);
+
+        // Override sprint
+        if (type == ActionType.General && id == 4) {
+            if (this._getDutyActionId(0) == 31314) {
+                id = 31314;
+                type = ActionType.Spell;
+            } else {
+                PluginLog.Debug("Got a sprint in Island Sanctuary, but the Sprint DutyAction is not ready.");
+            }
+        }
+
+        return this._useActionHook!.Original(mgr, type, id, targetid, a4, a5, a6, a7);
+    }
+}
