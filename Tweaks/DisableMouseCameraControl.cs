68088417c5e3400d53c26eb0e20fadfbaee3125f
Caraxi <git@caraxian.com>
2022-12-16 21:44:45 +1030

    Add Tweak: Disable Mouse Camera Control

---
+++ Tweaks/DisableMouseCameraControl.cs
@@ -0,0 +1,46 @@
+﻿using Dalamud.Game.ClientState.Keys;
+using SimpleTweaksPlugin.TweakSystem;
+using SimpleTweaksPlugin.Utility;
+
+namespace SimpleTweaksPlugin.Tweaks;
+
+public class DisableMouseCameraControl : Tweak {
+    public override string Name => "Disable Mouse Camera Control";
+    public override string Description => "Disable all control of the camera using the mouse.";
+
+    public enum CameraControlType : ulong { None, Keyboard, Gamepad, Mouse }
+    private delegate CameraControlType GetCameraControlType();
+    private HookWrapper<GetCameraControlType> getCameraControlTypeHook;
+
+    public class Configs : TweakConfig {
+        [TweakConfigOption("Allow control while holding Shift")]
+        public bool AllowWithShift = true;
+    }
+
+    public Configs Config { get; private set; }
+    public override bool UseAutoConfig => true;
+
+    public override void Enable() {
+        Config = LoadConfig<Configs>() ?? new Configs();
+        getCameraControlTypeHook ??= Common.Hook<GetCameraControlType>("E8 ?? ?? ?? ?? 83 F8 01 74 55", GetCameraControlTypeDetour);
+        getCameraControlTypeHook?.Enable();
+        base.Enable();
+    }
+
+    private CameraControlType GetCameraControlTypeDetour() {
+        var cameraControlType = getCameraControlTypeHook.Original();
+        if (Config.AllowWithShift && Service.KeyState[VirtualKey.SHIFT]) return cameraControlType ;
+        return cameraControlType == CameraControlType.Mouse ? CameraControlType.None : cameraControlType;
+    }
+
+    public override void Disable() {
+        getCameraControlTypeHook?.Disable();
+        SaveConfig(Config);
+        base.Disable();
+    }
+
+    public override void Dispose() {
+        getCameraControlTypeHook?.Dispose();
+        base.Dispose();
+    }
+}
