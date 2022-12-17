11973d4c876dfe47902281978653fa502f5451d3
Caraxi <git@caraxian.com>
2022-12-17 23:46:58 +1030

    New Tweak: `Combat Movement Type Control`

---
+++ Tweaks/CombatMovementControl.cs
@@ -0,0 +1,83 @@
+﻿using Dalamud.Game.ClientState.Conditions;
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
+using ImGuiNET;
+using SimpleTweaksPlugin.TweakSystem;
+using SimpleTweaksPlugin.Utility;
+
+namespace SimpleTweaksPlugin.Tweaks;
+
+public unsafe class CombatMovementControl : Tweak {
+    public override string Name => "Combat Movement Type Control";
+    public override string Description => "Set movement type between Standard and Legacy when in/out of combat or when weapon is drawn/sheathed.";
+    
+    public enum MoveModeType {
+        Ignore = -1,
+        Standard,
+        Legacy,
+    }
+    
+    public class Configs : TweakConfig {
+        public MoveModeType InCombat = MoveModeType.Ignore;
+        public MoveModeType OutOfCombat = MoveModeType.Ignore;
+        public MoveModeType WeaponDrawn = MoveModeType.Ignore;
+        public MoveModeType WeaponSheathed = MoveModeType.Ignore;
+    }
+
+    public Configs Config { get; private set; }
+
+    protected override DrawConfigDelegate DrawConfigTree => (ref bool _) => {
+
+        void ShowOption(string label, ref MoveModeType c) {
+            if (ImGui.BeginCombo(label, $"{c}")) {
+                if (ImGui.Selectable(nameof(MoveModeType.Ignore), c == MoveModeType.Ignore)) c = MoveModeType.Ignore;
+                if (ImGui.Selectable(nameof(MoveModeType.Standard), c == MoveModeType.Standard)) c = MoveModeType.Standard;
+                if (ImGui.Selectable(nameof(MoveModeType.Legacy), c == MoveModeType.Standard)) c = MoveModeType.Legacy;
+                ImGui.EndCombo();
+            }
+        }
+        
+        ShowOption("Out of Combat", ref Config.OutOfCombat);
+        ShowOption("In Combat", ref Config.InCombat);
+        ShowOption("Weapon Drawn", ref Config.WeaponDrawn);
+        ShowOption("Weapon Sheathed", ref Config.WeaponSheathed);
+    };
+    
+    public override void Enable() {
+        Config = LoadConfig<Configs>() ?? new Configs();
+        Service.Condition.ConditionChange += OnConditionChange;
+        Common.FrameworkUpdate += OnFrameworkUpdate;
+        base.Enable();
+    }
+
+
+    private byte? previousUnsheathedState;
+    private void OnFrameworkUpdate() {
+        var unsheathedState = UIState.Instance()->WeaponState.WeaponUnsheathed;
+        if (previousUnsheathedState == null) {
+            previousUnsheathedState = unsheathedState;
+            return;
+        }
+        
+        if (unsheathedState != previousUnsheathedState) {
+            previousUnsheathedState = unsheathedState;
+            var v = unsheathedState == 1 ? Config.WeaponDrawn : Config.WeaponSheathed;
+            if (v == MoveModeType.Ignore) return;
+            GameConfig.UiControl.Set("MoveMode", (uint) v);
+        }
+    }
+
+    private void OnConditionChange(ConditionFlag flag, bool value) {
+        if (flag != ConditionFlag.InCombat) {
+            var v = value ? Config.InCombat : Config.OutOfCombat;
+            if (v == MoveModeType.Ignore) return;
+            GameConfig.UiControl.Set("MoveMode", (uint) v);
+        }
+    }
+
+    public override void Disable() {
+        Service.Condition.ConditionChange -= OnConditionChange;
+        Common.FrameworkUpdate -= OnFrameworkUpdate;
+        SaveConfig(Config);
+        base.Disable();
+    }
+}
