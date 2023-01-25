9d0632d8a084a7531fbed7a3360926113259235c
Ziggy <dook@stardust.red>
2023-01-25 11:59:40 -0800

    New Tweak: Hide Chat (#412)
    * New Tweak: Hide Chat
    
    * Cleanup
    
    * Rework - Provides 3 commands to control visibility of chat logs. Showing onDemand is now provided as a config option.
    
    * Update HideChat.cs
    
    * Add null checks
    
    * Remove SetOnDemand
    
    * Reduce scope of tweak to just providing chat commands

---
+++ Tweaks/Chat/HideChat.cs
@@ -0,0 +1,115 @@
+﻿using Dalamud.Game;
+using Dalamud.Game.Command;
+using Dalamud.Game.ClientState.Conditions;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using ImGuiNET;
+using SimpleTweaksPlugin.TweakSystem;
+using SimpleTweaksPlugin.Utility;
+using System;
+
+namespace SimpleTweaksPlugin.Tweaks.Chat;
+
+public unsafe class HideChat : ChatTweaks.SubTweak
+{
+    public override string Name => "Hide Chat";
+    public override string Description => $"Provides commands to hide the chat. ({Command} show|hide|toggle)";
+
+    protected override string Author => "@dookssh @VariableVixen";
+
+    // Note that since this is a chat SubTweak, we cannot inherit from CommandTweak
+    private string Command = "/chatvis";
+    private string HelpMessage => $"[{Plugin.Name} {Name}";
+    private bool ShowInHelp = true;
+
+    // If the plugin is failing, most likely you will need to update these IDs
+    private readonly uint TextInputNodeID = 5;
+    private readonly uint TextInputCursorID = 2;
+    private readonly string[] ChatLogNodeNames = { "ChatLog", "ChatLogPanel_0", "ChatLogPanel_1", "ChatLogPanel_2", "ChatLogPanel_3" };
+
+    public override void Enable()
+    {
+        Service.Commands.AddHandler(Command, new CommandInfo(OnCommand)
+        {
+            HelpMessage = HelpMessage,
+            ShowInHelp = ShowInHelp
+        });
+
+        Service.Framework.Update += FrameworkUpdate;
+        base.Enable();
+    }
+
+    public override void Disable()
+    {
+        Service.Commands.RemoveHandler(Command);
+        Service.Framework.Update -= FrameworkUpdate;
+        SetVisibility(true);
+        base.Disable();
+    }
+
+    private void FrameworkUpdate(Framework framework)
+    {
+        try
+        {
+            // Always show chat when actively typing
+            var inputCursorNode = GetChatInputCursorNode();
+            if (inputCursorNode == null) return;
+            if (inputCursorNode->IsVisible) SetVisibility(true);
+        }
+        catch (Exception ex)
+        {
+            SimpleLog.Error(ex);
+        }
+    }
+
+    private void OnCommand(string command, string args)
+    {
+        var argList = args.ToLower().Split(" ");
+
+        switch (argList[0])
+        {
+            case "show":
+                SetVisibility(true);
+                break;
+
+            case "hide":
+                SetVisibility(false);
+                break;
+
+            case "toggle":
+                var baseNode = Common.GetUnitBase("ChatLog");
+                var isVisible = baseNode->RootNode->IsVisible;
+
+                SetVisibility(!isVisible);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private AtkResNode* GetChatInputCursorNode()
+    {
+        var baseNode = Common.GetUnitBase("ChatLog");
+        if (baseNode == null) return null;
+
+        var textInputComponentNode = (AtkComponentNode*)baseNode->GetNodeById(TextInputNodeID);
+        if (textInputComponentNode == null) return null;
+
+        var textInputBaseNode = textInputComponentNode->Component;
+        if (textInputBaseNode == null) return null;
+
+        AtkUldManager uldManager = textInputBaseNode->UldManager;
+
+        return Common.GetNodeByID(&uldManager, TextInputCursorID);
+    }
+
+    private void SetVisibility(bool visibility)
+    {
+        foreach (string name in ChatLogNodeNames)
+        {
+            AtkUnitBase* node = Common.GetUnitBase(name);
+            if (node == null) continue;
+            node->RootNode->ToggleVisibility(visibility);
+        };
+    }
+}
