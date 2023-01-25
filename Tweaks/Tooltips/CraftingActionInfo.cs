548d75a0df354ce055f81375c3af53d926960b1c
Caraxi <git@caraxian.com>
2023-01-26 09:59:12 +1030

    [`Crafting Action Tooltips`] Show ghost values on Synthesis window.
    maybe actually commit the file?

--- Tweaks/Tooltips/CraftingActionInfo.cs
+++ Tweaks/Tooltips/CraftingActionInfo.cs
@@ -1,9 +1,14 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Dalamud.Game.Text.SeStringHandling.Payloads;
+using FFXIVClientStructs.FFXIV.Client.System.Memory;
+using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using Lumina.Excel.GeneratedSheets;
 using SimpleTweaksPlugin.Enums;
+using SimpleTweaksPlugin.TweakSystem;
+using SimpleTweaksPlugin.Utility;
 
 namespace SimpleTweaksPlugin.Tweaks.Tooltips;
 
@@ -11,30 +16,211 @@ public unsafe class CraftingActionInfo : TooltipTweaks.SubTweak {
     public override string Name => "Improved Crafting Action Tooltips";
     public override string Description => "Adds calculated efficiency of crafting actions to tooltips.";
 
+    public class Configs : TweakConfig {
+        [TweakConfigOption("Show Results Preview")]
+        public bool ShowResultsPreview = false;
+    }
+
+    public Configs Config { get; private set; }
+
+    public override bool UseAutoConfig => true;
+
     private DalamudLinkPayload? identifier;
     private string progressString;
     private string qualityString;
     
     public override void Enable() {
+        Config = LoadConfig<Configs>() ?? new Configs();
         progressString ??= Service.Data.Excel.GetSheet<Addon>()?.GetRow(213)?.Text?.RawString ?? "Progress";
         qualityString ??= Service.Data.Excel.GetSheet<Addon>()?.GetRow(216)?.Text?.RawString ?? "Quality";
         
-        
         identifier = PluginInterface.AddChatLinkHandler((uint) LinkHandlerId.CraftingActionInfoIdentifier, (_, _) => { });
+
+        if (Config.ShowResultsPreview) {
+            Common.FrameworkUpdate += FrameworkUpdate;
+        }
         base.Enable();
     }
-    
+
+    protected override void ConfigChanged() {
+        Common.FrameworkUpdate -= FrameworkUpdate;
+        if (Config.ShowResultsPreview) {
+            Common.FrameworkUpdate += FrameworkUpdate;
+        }
+    }
+
     public override void Disable() {
+        SaveConfig(Config);
+        Common.FrameworkUpdate -= FrameworkUpdate;
         PluginInterface.RemoveChatLinkHandler((uint) LinkHandlerId.CraftingActionInfoIdentifier);
         base.Dispose();
     }
+
+    private void SetGhost(AtkTextNode* textNode, AtkTextNode* maxTextNode, AtkComponentNode* gauge, uint addValue) {
+        var mainBar = Common.GetNodeByID<AtkNineGridNode>(&gauge->Component->UldManager, 2);
+        if (mainBar == null) return;
+
+        if (!(uint.TryParse(textNode->NodeText.ToString(), out var current) && uint.TryParse(maxTextNode->NodeText.ToString(), out var max))) {
+            return;
+        }
+
+        var newValue = Math.Min(max, current + addValue);
+        var newPercentage = newValue / (float)max;
+        
+        var ghostBar = Common.GetNodeByID<AtkNineGridNode>(&gauge->Component->UldManager, CustomNodes.CraftingGhostBar);
+        var ghostText = Common.GetNodeByID<AtkTextNode>(&gauge->Component->UldManager, CustomNodes.CraftingGhostText);
+
+        if (ghostBar == null && addValue > 0) {
+            
+            var newGhostBar = (AtkNineGridNode*)IMemorySpace.GetUISpace()->Malloc((ulong)sizeof(AtkNineGridNode), 8);
+            
+            if (newGhostBar != null) {
+                IMemorySpace.Memset(newGhostBar, 0, (ulong)sizeof(AtkNineGridNode));
+                newGhostBar->Ctor();
+
+                newGhostBar->AtkResNode.Type = NodeType.NineGrid;
+                newGhostBar->PartsList = mainBar->PartsList;
+                newGhostBar->PartID = mainBar->PartID;
+                newGhostBar->TopOffset = mainBar->TopOffset;
+                newGhostBar->BottomOffset = mainBar->BottomOffset;
+                newGhostBar->LeftOffset = mainBar->LeftOffset;
+                newGhostBar->RightOffset = mainBar->RightOffset;
+                newGhostBar->BlendMode = mainBar->BlendMode;
+                newGhostBar->PartsTypeRenderType = mainBar->PartsTypeRenderType;
+
+                newGhostBar->AtkResNode.NodeID = CustomNodes.CraftingGhostBar;
+                newGhostBar->AtkResNode.SetPositionShort(0, 2);
+                newGhostBar->AtkResNode.ScaleX = 1;
+                newGhostBar->AtkResNode.ScaleY = 1;
+                newGhostBar->AtkResNode.Alpha_2 = 128;
+                
+                
+                newGhostBar->AtkResNode.SetWidth((ushort) (240 * newPercentage)); // todo: calculate
+                newGhostBar->AtkResNode.SetHeight(12);
+
+                
+                newGhostBar->AtkResNode.ToggleVisibility(true);
+                
+                newGhostBar->AtkResNode.ParentNode = mainBar->AtkResNode.ParentNode;
+                newGhostBar->AtkResNode.NextSiblingNode = &mainBar->AtkResNode;
+                newGhostBar->AtkResNode.PrevSiblingNode = mainBar->AtkResNode.PrevSiblingNode;
+                if (mainBar->AtkResNode.PrevSiblingNode != null) {
+                    mainBar->AtkResNode.PrevSiblingNode->NextSiblingNode = &newGhostBar->AtkResNode;
+                }
+               
+                mainBar->AtkResNode.PrevSiblingNode = &newGhostBar->AtkResNode;
+                gauge->Component->UldManager.UpdateDrawNodeList();
+                
+                ghostBar = newGhostBar;
+            }
+        }
+
+        if (ghostText == null && addValue > 0) {
+            var newGhostText = (AtkTextNode*)IMemorySpace.GetUISpace()->Malloc((ulong)sizeof(AtkTextNode), 8);
+            if (newGhostText != null) {
+
+                IMemorySpace.Memset(newGhostText, 0, (ulong)sizeof(AtkTextNode));
+                newGhostText->Ctor();
+                
+                newGhostText->AtkResNode.Type = NodeType.Text;
+                newGhostText->AtkResNode.Flags = textNode->AtkResNode.Flags;
+                newGhostText->AtkResNode.DrawFlags = 0;
+                newGhostText->AtkResNode.SetPositionFloat(textNode->AtkResNode.X, -4);
+                newGhostText->AtkResNode.SetWidth(textNode->AtkResNode.Width);
+                newGhostText->AtkResNode.SetHeight(textNode->AtkResNode.Height);
+                
+                newGhostText->SetText("9999");
+
+                newGhostText->LineSpacing = textNode->LineSpacing;
+                newGhostText->AlignmentFontType = textNode->AlignmentFontType;
+                newGhostText->FontSize = textNode->FontSize;
+                newGhostText->TextFlags = (byte) ((TextFlags)textNode->TextFlags | TextFlags.Edge);
+                newGhostText->TextFlags2 = 0;
+
+                newGhostText->AtkResNode.NodeID = CustomNodes.CraftingGhostText;
+
+                newGhostText->AtkResNode.Color.A = 0xFF;
+                newGhostText->AtkResNode.Color.R = 0xFF;
+                newGhostText->AtkResNode.Color.G = 0xFF;
+                newGhostText->AtkResNode.Color.B = 0xFF;
+                
+                newGhostText->TextColor.A = 0xFF;
+                newGhostText->TextColor.R = 0xFF;
+                newGhostText->TextColor.G = 0xFF;
+                newGhostText->TextColor.B = 0xFF;
+
+                newGhostText->EdgeColor.A = 0x55;
+                newGhostText->EdgeColor.R = 0xF0;
+                newGhostText->EdgeColor.G = 0xF0;
+                newGhostText->EdgeColor.B = 0x00;
+
+                newGhostText->AtkResNode.ParentNode = mainBar->AtkResNode.ParentNode;
+                newGhostText->AtkResNode.NextSiblingNode = &mainBar->AtkResNode;
+                newGhostText->AtkResNode.PrevSiblingNode = mainBar->AtkResNode.PrevSiblingNode;
+                if (mainBar->AtkResNode.PrevSiblingNode != null) {
+                    mainBar->AtkResNode.PrevSiblingNode->NextSiblingNode = &newGhostText->AtkResNode;
+                }
+               
+                mainBar->AtkResNode.PrevSiblingNode = &newGhostText->AtkResNode;
+                gauge->Component->UldManager.UpdateDrawNodeList();
+
+                ghostText = newGhostText;
+            }
+        }
+        
+
+        if (ghostBar == null || ghostText == null) return;
+
+        if (addValue > 0) {
+            ghostBar->AtkResNode.ToggleVisibility(true);
+            textNode->AtkResNode.ToggleVisibility(false);
+            ghostBar->AtkResNode.SetWidth((ushort) (240 * newPercentage));
+            
+            ghostText->AtkResNode.ToggleVisibility(true);
+            ghostText->SetText($"{newValue}");
+        } else {
+            ghostBar->AtkResNode.ToggleVisibility(false);
+            textNode->AtkResNode.ToggleVisibility(true);
+            ghostText->AtkResNode.ToggleVisibility(false);
+        }
+        
+    }
     
-    public override void OnGenerateActionTooltip(NumberArrayData* numberArrayData, StringArrayData* stringArrayData) {
-        if (identifier == null) return;
+    public void FrameworkUpdate() {
+        var addon = Common.GetUnitBase<AddonSynthesis>("Synthesis");
+        if (addon == null) return;
 
+        var progressGaugeNode = (AtkComponentNode*) addon->AtkUnitBase.GetNodeById(54);
+        var progressText = addon->CurrentProgress;
+        var progressMaxText = addon->MaxProgress;
+        
+        var qualityGaugeNode = (AtkComponentNode*) addon->AtkUnitBase.GetNodeById(60);
+        var qualityText = addon->CurrentQuality;
+        var qualityMaxText = addon->MaxQuality;
+        
+        // Confirm Existence
+        if (progressGaugeNode == null || progressText == null || progressMaxText == null || qualityGaugeNode == null || qualityText == null || qualityMaxText == null) return;
+        
+        // Confirm types
+        progressGaugeNode = progressGaugeNode->AtkResNode.GetAsAtkComponentNode();
+        qualityGaugeNode = qualityGaugeNode->AtkResNode.GetAsAtkComponentNode();
+        if (progressGaugeNode == null || qualityGaugeNode == null) return;
+        
+        if (Service.GameGui.HoveredAction.ActionID != 0) {
+            var result = GetActionResult(Service.GameGui.HoveredAction.ActionID);
+            SetGhost(progressText, progressMaxText, progressGaugeNode, result.progress);
+            SetGhost(qualityText, qualityMaxText, qualityGaugeNode, result.quality);
+        } else {
+            SetGhost(progressText, progressMaxText, progressGaugeNode, 0);
+            SetGhost(qualityText, qualityMaxText, qualityGaugeNode, 0);
+        }
+    }
+
+    private (uint progress, uint quality) GetActionResult(uint id) {
+        
         var agent = AgentCraftActionSimulator.Instance();
-        if (agent == null) return;
-
+        if (agent == null) return (0, 0);
+        
         var progress = 0U;
         var quality = 0U;
         
@@ -42,7 +228,7 @@ public unsafe class CraftingActionInfo : TooltipTweaks.SubTweak {
         var p = (ProgressEfficiencyCalculation*) agent->Progress;
         for (var i = 0; i < sizeof(ProgressEfficiencyCalculations) / sizeof(ProgressEfficiencyCalculation); i++) {
             if (p == null) break;
-            if (p->ActionId == Action.Id) {
+            if (p->ActionId == id) {
                 progress = p->ProgressIncrease;
                 break;
             }
@@ -52,12 +238,20 @@ public unsafe class CraftingActionInfo : TooltipTweaks.SubTweak {
         var q = (QualityEfficiencyCalculation*) agent->Quality;
         for (var i = 0; i < sizeof(QualityEfficiencyCalculations) / sizeof(QualityEfficiencyCalculation); i++) {
             if (q == null) break;
-            if (q->ActionId == Action.Id) {
+            if (q->ActionId == id) {
                 quality = q->QualityIncrease;
                 break;
             }
             q++;
         }
+
+        return (progress, quality);
+    }
+    
+    public override void OnGenerateActionTooltip(NumberArrayData* numberArrayData, StringArrayData* stringArrayData) {
+        if (identifier == null) return;
+        
+        var (progress, quality) = GetActionResult(Action.Id);
 
         if (progress == 0 && quality == 0) return;
         
