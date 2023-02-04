53fbffb607150ddbf5b57088390a5e3670e1f250
Julian <hi@notnite.com>
2023-02-04 08:42:32 -0800

    Add High Resolution Screenshots (#447)
    * Add High Resolution Screenshots
    
    * HighResScreenshots: wrap text
    
    * Rework High Res Screenshots delay
    
    * Experimental

---
+++ Tweaks/HighResScreenshots.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Dalamud.Interface;
+using FFXIVClientStructs.FFXIV.Client.Graphics.Kernel;
+using ImGuiNET;
+using SimpleTweaksPlugin.TweakSystem;
+using SimpleTweaksPlugin.Utility;
+
+namespace SimpleTweaksPlugin.Tweaks;
+
+public unsafe class HighResScreenshots : Tweak {
+    public override string Name => "High Resolution Screenshots";
+    public override string Description => "Increases the resolution in game screenshots are taken at.";
+    protected override string Author => "NotNite";
+    public override bool Experimental => true;
+
+    public class Configs : TweakConfig {
+        public int Scale = 2;
+        public float Delay = 1.0f;
+    }
+
+    public Configs Config { get; private set; }
+
+    private delegate byte IsInputIDClickedDelegate(nint a1, int a2);
+
+    private HookWrapper<IsInputIDClickedDelegate> isInputIDClickedHook;
+
+    protected override DrawConfigDelegate DrawConfigTree => (ref bool hasChanged) => {
+        ImGui.TextWrapped(
+            "This tweak will increase the resolution of screenshots taken in game. It will NOT increase the scale of your HUD/plugin windows.");
+        ImGui.TextWrapped("Your HUD will appear smaller while the screenshot is processing.");
+
+        ImGui.NewLine();
+
+        ImGui.TextWrapped("Higher scale will take longer and use more resources.");
+        ImGui.TextWrapped(
+            "The higher the scale is, the longer the delay lasts. Experiment with these settings to find the best options for your system.");
+
+        ImGui.SetNextItemWidth(ImGuiHelpers.GlobalScale * 100);
+        hasChanged |= ImGui.InputInt("Scale", ref Config.Scale);
+
+        ImGui.SetNextItemWidth(ImGuiHelpers.GlobalScale * 100);
+        hasChanged |= ImGui.InputFloat("Delay", ref Config.Delay);
+
+        if (Config.Scale < 2) Config.Scale = 2;
+        if (Config.Delay < 0) Config.Delay = 0;
+    };
+
+    public override void Enable() {
+        Config = LoadConfig<Configs>() ?? new Configs();
+
+        isInputIDClickedHook ??=
+            Common.Hook<IsInputIDClickedDelegate>("E9 ?? ?? ?? ?? 83 7F 44 02", IsInputIDClickedDetour);
+        isInputIDClickedHook?.Enable();
+
+        base.Enable();
+    }
+
+    private bool shouldPress;
+    private uint oldWidth;
+    private uint oldHeight;
+
+    const int ScreenshotButton = 543;
+
+    // IsInputIDClicked is called from Client::UI::UIInputModule.CheckScreenshotState, which is polled
+    // We change the res when the button is pressed and tell it to take a screenshot the next time it is polled
+    private byte IsInputIDClickedDetour(nint a1, int a2) {
+        var orig = isInputIDClickedHook.Original(a1, a2);
+
+        if (orig == 1 && a2 == ScreenshotButton && !shouldPress) {
+            var device = Device.Instance();
+            oldWidth = device->Width;
+            oldHeight = device->Height;
+
+            device->NewWidth = oldWidth * (uint)Config.Scale;
+            device->NewHeight = oldHeight * (uint)Config.Scale;
+            device->RequestResolutionChange = 1;
+
+            Service.Framework.RunOnTick(() => { shouldPress = true; }, delay: TimeSpan.FromSeconds(Config.Delay));
+
+            return 0;
+        }
+
+        if (a2 == ScreenshotButton && shouldPress) {
+            shouldPress = false;
+            
+            // Reset the res back to normal after the screenshot is taken
+            Service.Framework.RunOnTick(() => {
+                var device = Device.Instance();
+                device->NewWidth = oldWidth;
+                device->NewHeight = oldHeight;
+                device->RequestResolutionChange = 1;
+            }, delayTicks: 1);
+
+            return 1;
+        }
+
+        return orig;
+    }
+
+    public override void Disable() {
+        SaveConfig(Config);
+        isInputIDClickedHook?.Disable();
+        base.Disable();
+    }
+}
