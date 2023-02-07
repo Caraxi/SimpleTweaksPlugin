2e22e95391149607fb55d3db38004d366acdff32
Caraxi <git@caraxian.com>
2023-02-07 17:32:22 +1030

    [`Keep Windows Open`] Add support for Character window

--- Tweaks/UiAdjustment/KeepOpen.cs
+++ Tweaks/UiAdjustment/KeepOpen.cs
@@ -21,8 +21,6 @@ public unsafe class KeepOpen : Tweak {
     public Configs Config { get; private set; }
 
     protected override DrawConfigDelegate DrawConfigTree => (ref bool hasChanged) => {
-
-        
         ImGui.TextWrapped("Windows to keep open when accessing retainers, shops or market board:");
         foreach (var (internalName, displayName) in Windows) {
             var enabled = Config.EnabledWindows.Contains(internalName);
@@ -46,29 +44,51 @@ public unsafe class KeepOpen : Tweak {
         base.Enable();
     }
 
-    private Dictionary<string, string> Windows = new() {
+
+    private record WindowInfo(string DisplayName, bool UseReopenMethod = false) {
+        public static implicit operator WindowInfo(string displayName) => new(displayName);
+        public override string ToString() => DisplayName;
+    };
+    
+    private Dictionary<string, WindowInfo> Windows = new() {
         ["ItemFinder"] = "Item Search List",
         ["RecipeNote"] = "Crafting Log",
         ["RecipeTree"] = "Recipe Tree",
         ["ContentsInfoDetail"] = "Timer Details",
         ["ContentsInfo"] = "Timers",
+        ["Character"] = new("Character Window", true),
     };
     
     private void* HideDetour(AtkUnitBase* atkUnitBase, byte a2, byte a3, int a4) {
-        if (atkUnitBase != null && a2 == 1 && a3 == 0 && a4 == 2) {
-            var name = Common.ReadString(atkUnitBase->Name, 0x20);
-            
-            if (Windows.ContainsKey(name)) {
-                if (Config.EnabledWindows.Contains(name)) {
-                    SimpleLog.Log($"Suppress Hide: {name}");
-                    return null;
+        var doReopen = false;
+        try {
+            if (atkUnitBase != null && a2 == 1 && a3 == 0 && a4 == 2) {
+                var name = Common.ReadString(atkUnitBase->Name, 0x20);
+
+                if (Windows.TryGetValue(name, out var windowInfo)) {
+                    if (Config.EnabledWindows.Contains(name)) {
+                        if (windowInfo.UseReopenMethod) {
+                            doReopen = true;
+                            SimpleLog.Log($"Attempting Reopen: {name}");
+                            return hideHook.Original(atkUnitBase, a2, a3, a4);
+                        }
+                        SimpleLog.Log($"Suppress Hide: {name}");
+                        return null;
+                    }
+                } else {
+                    SimpleLog.Verbose($"Allowed Hiding: {name}");
                 }
-            } else {
-                SimpleLog.Verbose($"Allowed Hiding: {name}");
+            }
+
+            return hideHook.Original(atkUnitBase, a2, a3, a4);
+        } catch {
+            doReopen = false;
+            return hideHook.Original(atkUnitBase, a2, a3, a4);
+        } finally {
+            if (doReopen) {
+                atkUnitBase->Show(0);
             }
         }
-
-        return hideHook.Original(atkUnitBase, a2, a3, a4);
     }
 
     public override void Disable() {
