94ad8339a24c6d93ece6d16b081101d51b060767
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-02-18 17:39:57 -0800

    Prototype

---
+++ Utility/UiHelper.Nodes.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using FFXIVClientStructs.FFXIV.Client.System.Memory;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace SimpleTweaksPlugin.Utility;
+
+public static unsafe partial class UiHelper
+{
+    public record PartInfo(ushort U, ushort V, ushort Width, ushort Height);
+    
+    public static AtkImageNode* MakeImageNode(uint id) => MakeImageNode(id, new PartInfo(0, 0, 0, 0));
+    public static AtkImageNode* MakeImageNode(uint id, PartInfo partInfo)
+    {
+        if (!TryMakeImageNode(id, 0, 0, 0, 0, out var imageNode))
+        {
+            SimpleLog.Error("Failed to alloc memory for AtkImageNode.");
+            return null;
+        }
+
+        if (!TryMakePartsList(0, out var partsList))
+        {
+            SimpleLog.Error("Failed to alloc memory for AtkUldPartsList.");
+            FreeImageNode(imageNode);
+            return null;
+        }
+
+        if (!TryMakePart(partInfo.U, partInfo.V, partInfo.Width, partInfo.Height, out var part))
+        {
+            SimpleLog.Error("Failed to alloc memory for AtkUldPart.");
+            FreePartsList(partsList);
+            FreeImageNode(imageNode);
+            return null;
+        }
+
+        if (!TryMakeAsset(0, out var asset))
+        {
+            SimpleLog.Error("Failed to alloc memory for AtkUldAsset.");
+            FreePart(part);
+            FreePartsList(partsList);
+            FreeImageNode(imageNode);
+        }
+
+        AddAsset(part, asset);
+        AddPart(partsList, part);
+        AddPartsList(imageNode, partsList);
+
+        return imageNode;
+    }
+
+    public static void LinkNodeAtEnd(AtkImageNode* imageNode, AtkUnitBase* parent)
+    {
+        var node = parent->RootNode->ChildNode;
+        while (node->PrevSiblingNode != null) node = node->PrevSiblingNode;
+
+        node->PrevSiblingNode = (AtkResNode*) imageNode;
+        imageNode->AtkResNode.NextSiblingNode = node;
+        imageNode->AtkResNode.ParentNode = node->ParentNode;
+        
+        parent->UldManager.UpdateDrawNodeList();
+    }
+    
+    public static void UnlinkAndFreeImageNode(AtkImageNode* node, AtkUnitBase* parent)
+    {
+        if (node != null)
+        {
+            if (node->AtkResNode.PrevSiblingNode != null)
+                node->AtkResNode.PrevSiblingNode->NextSiblingNode = node->AtkResNode.NextSiblingNode;
+            
+            if (node->AtkResNode.NextSiblingNode != null)
+                node->AtkResNode.NextSiblingNode->PrevSiblingNode = node->AtkResNode.PrevSiblingNode;
+            
+            parent->UldManager.UpdateDrawNodeList();
+
+            FreePartsList(node->PartsList);
+            FreeImageNode(node);
+        }
+    }
+
+    private static void FreeImageNode(AtkImageNode* node)
+    {
+        node->AtkResNode.Destroy(false);
+        IMemorySpace.Free(node, (ulong)sizeof(AtkImageNode));
+    }
+    
+    private static bool TryMakeImageNode(uint id, short resNodeFlags, uint resNodeDrawFlags, byte wrapMode, byte imageNodeFlags, [NotNullWhen(true)] out AtkImageNode* imageNode)
+    {
+        imageNode = IMemorySpace.GetUISpace()->Create<AtkImageNode>();
+
+        if (imageNode is not null)
+        {
+            imageNode->AtkResNode.Type = NodeType.Image;
+            imageNode->AtkResNode.NodeID = id;
+            imageNode->AtkResNode.Flags = resNodeFlags;
+            imageNode->AtkResNode.DrawFlags = resNodeDrawFlags;
+            imageNode->WrapMode = wrapMode;
+            imageNode->Flags = imageNodeFlags;
+            return true;
+        }
+
+        return false;
+    }
+
+    private static void FreePartsList(AtkUldPartsList* partsList)
+    {
+        foreach (var index in Enumerable.Range(0, (int)partsList->PartCount))
+        {
+            var part = &partsList->Parts[index];
+            
+            FreeAsset(part->UldAsset);
+            FreePart(part);
+        }
+        
+        IMemorySpace.Free(partsList, (ulong)sizeof(AtkUldPartsList));
+    }
+    
+    private static bool TryMakePartsList(uint id, [NotNullWhen(true)] out AtkUldPartsList* partsList)
+    {
+        partsList = (AtkUldPartsList*) IMemorySpace.GetUISpace()->Malloc((ulong) sizeof(AtkUldPartsList), 8);
+
+        if (partsList is not null)
+        {
+            partsList->Id = id;
+            partsList->PartCount = 0;
+            partsList->Parts = null;
+            return true;
+        }
+
+        return false;
+    }
+
+    private static void AddPartsList(AtkImageNode* imageNode, AtkUldPartsList* partsList)
+    {
+        imageNode->PartsList = partsList;
+    }
+    
+    private static void AddPartsList(AtkCounterNode* counterNode, AtkUldPartsList* partsList)
+    {
+        counterNode->PartsList = partsList;
+    }
+
+    private static void FreePart(AtkUldPart* part)
+    {
+        IMemorySpace.Free(part, (ulong)sizeof(AtkUldPart));
+    }
+    
+    private static bool TryMakePart(ushort u, ushort v, ushort width, ushort height, [NotNullWhen(true)] out AtkUldPart* part)
+    {
+        part = (AtkUldPart*)IMemorySpace.GetUISpace()->Malloc((ulong)sizeof(AtkUldPart), 8);
+
+        if (part is not null)
+        {
+            part->U = u;
+            part->V = v;
+            part->Width = width;
+            part->Height = height;
+            return true;
+        }
+
+        return false;
+    }
+
+    private static void AddPart(AtkUldPartsList* partsList, AtkUldPart* part)
+    {
+        // copy pointer to old array
+        var oldPartArray = partsList->Parts;
+        
+        // allocate space for new array
+        var newSize = partsList->PartCount + 1;
+        var newArray = (AtkUldPart*) IMemorySpace.GetUISpace()->Malloc((ulong)sizeof(AtkUldPart) * newSize, 8);
+
+        if (oldPartArray is not null)
+        {
+            // copy each member of old array2
+            foreach (var index in Enumerable.Range(0, (int)partsList->PartCount))
+            {
+                Buffer.MemoryCopy(oldPartArray + index, newArray + index, sizeof(AtkUldPart), sizeof(AtkUldPart));
+            }
+        
+            // free old array
+            IMemorySpace.Free(oldPartArray, (ulong)sizeof(AtkUldPart) * partsList->PartCount);
+        }
+        
+        // add new part
+        Buffer.MemoryCopy(part, newArray + (newSize - 1), sizeof(AtkUldPart), sizeof(AtkUldPart));
+        partsList->Parts = newArray;
+        partsList->PartCount = newSize;
+    }
+
+    private static void FreeAsset(AtkUldAsset* asset)
+    {
+        IMemorySpace.Free(asset, (ulong) sizeof(AtkUldPart));
+    }
+    
+    private static bool TryMakeAsset(uint id, [NotNullWhen(true)] out AtkUldAsset* asset)
+    {
+        asset = (AtkUldAsset*)IMemorySpace.GetUISpace()->Malloc((ulong)sizeof(AtkUldAsset), 8);
+
+        if (asset is not null)
+        {
+            asset->Id = id;
+            asset->AtkTexture.Ctor();
+            return true;
+        }
+
+        return false;
+    }
+
+    private static void AddAsset(AtkUldPart* part, AtkUldAsset* asset)
+    {
+        part->UldAsset = asset;
+    }
+}
