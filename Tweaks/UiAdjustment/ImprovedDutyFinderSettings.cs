f0acc7e6aaab79b6fe0730acbfba38e77f8a0bde
Aireil <33433913+Aireil@users.noreply.github.com>
2023-03-04 23:04:59 +0100

    [`Improved Duty Finder Settings`] Fix for high-end duty UI part II

--- Tweaks/UiAdjustment/ImprovedDutyFinderSettings.cs
+++ Tweaks/UiAdjustment/ImprovedDutyFinderSettings.cs
@@ -216,6 +216,10 @@ public unsafe class ImprovedDutyFinderSettings : UiAdjustments.SubTweak {
 
     private void OnDraw() {
         var addon = (AtkUnitBase*)Service.GameGui.GetAddonByName("ContentsFinder");
+        if (addon == null) {
+            addon = (AtkUnitBase*)Service.GameGui.GetAddonByName("RaidFinder");
+        }
+
         if (addon == null || !this.iconsReady || !this.Enabled) {
             return;
         }
