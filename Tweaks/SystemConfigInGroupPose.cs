b58599b91e70932a97d739bb2c012f7307dab80e
Caraxi <git@caraxian.com>
2023-03-05 20:27:12 +1030

    New Tweak: `System Config in Group Pose`

---
+++ Tweaks/SystemConfigInGroupPose.cs
@@ -0,0 +1,60 @@
+using System.Linq;
+using Dalamud.Game.Text;
+using Dalamud.Game.Text.SeStringHandling;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using SimpleTweaksPlugin.TweakSystem;
+
+namespace SimpleTweaksPlugin.Tweaks;
+
+public unsafe class SystemConfigInGroupPose : Tweak {
+    public override string Name => "SystemConfig in Group Pose";
+    public override string Description => "Allows the use of the /systemconfig command while in gpose.";
+    
+    private readonly string[] commands = {
+        // EN
+        "The command “/systemconfig” is unavailable at this time.",
+        "The command “/sconfig” is unavailable at this time.",
+        
+        // DE
+        "„/systemconfig“ wurde als Textkommando nicht richtig verwendet.",
+        "„/sconfig“ wurde als Textkommando nicht richtig verwendet.",
+        "„/systemkonfig“ wurde als Textkommando nicht richtig verwendet.",
+        "„/skon“ wurde als Textkommando nicht richtig verwendet.",
+        
+        // FR
+        "La commande texte “/systemconfig” ne peut pas être utilisée de cette façon.",
+        "La commande texte “/sconfig” ne peut pas être utilisée de cette façon.",
+        "La commande texte “/confs” ne peut pas être utilisée de cette façon.",
+        "La commande texte “/configsys” ne peut pas être utilisée de cette façon.",
+        
+        // JA
+        "そのコマンドは現在使用できません。： /systemconfig",
+        "そのコマンドは現在使用できません。： /sconfig",
+    };
+    
+    
+    public override void Setup() {
+        AddChangelogNewTweak(Changelog.UnreleasedVersion);
+        base.Setup();
+    }
+
+    public override void Enable() {
+        Service.Chat.CheckMessageHandled += OnChatMessage;
+        base.Enable();
+    }
+
+    private void OnChatMessage(XivChatType type, uint senderId, ref SeString sender, ref SeString message, ref bool isHandled) {
+        if (type != XivChatType.ErrorMessage) return;
+        if (!Service.PluginInterface.UiBuilder.GposeActive) return;
+        if (commands.Contains(message.TextValue)) {
+            var agent = AgentModule.Instance()->GetAgentByInternalId(AgentId.Config);
+            agent->Show();
+            isHandled = true;
+        }
+    }
+
+    public override void Disable() {
+        Service.Chat.CheckMessageHandled -= OnChatMessage;
+        base.Disable();
+    }
+}
