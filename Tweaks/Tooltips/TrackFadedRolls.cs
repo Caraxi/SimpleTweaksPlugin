203a9211eef4a9d09f7c896eb8eca7d49887c5c7
Caraxi <git@caraxian.com>
2023-04-20 13:34:38 +0930

    [`Track Faded Orchestrion Rolls`] Fix

--- Tweaks/Tooltips/TrackFadedRolls.cs
+++ Tweaks/Tooltips/TrackFadedRolls.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using Dalamud.Memory;
+using Dalamud.Utility;
 using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using FFXIVClientStructs.FFXIV.Client.System.Framework;
 using Lumina.Excel.GeneratedSheets;
@@ -48,10 +50,10 @@ public unsafe class TrackFadedRolls : TooltipTweaks.SubTweak {
 
         // Why is this such a mess? Because the Item row from EXD does not contain the item ID of the item.
         // So, what are we to do? Abuse Lumina. This is suboptimal. I do not care. I just want this done.
-        var itemName = Marshal.PtrToStringUTF8(item + *(ushort*) item);
+        var itemName = MemoryHelper.ReadSeStringNullTerminated(item + *(ushort*) item);
         if (itemName == null) return 4;
 
-        var luminaItem = GetRollBySingularName(itemName);
+        var luminaItem = GetRollBySingularName(itemName.TextValue);
         if (luminaItem == null) return 4;
         
         var areAllUnlocked = GetRollsCraftedWithItem(luminaItem)
@@ -63,7 +65,7 @@ public unsafe class TrackFadedRolls : TooltipTweaks.SubTweak {
     private static Item? GetRollBySingularName(string singular) {
         return Service.Data.Excel.GetSheet<Item>()!
             .Where(item => (item.FilterGroup == 12 && item.ItemUICategory.Row == 94))
-            .FirstOrDefault(item => item.Singular.RawString.Equals(singular));
+            .FirstOrDefault(item => item.Singular.ToDalamudString().TextValue.Equals(singular));
     }
 
     private static List<Item> GetRollsCraftedWithItem(Item item) {
