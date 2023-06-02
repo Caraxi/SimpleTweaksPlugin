9c94658776c0e5a5244735b0637cb83c285e984a
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-06-02 00:04:46 -0700

    [`SellMaxTripleTriadCards`] Disable Tweak (#585)

--- Tweaks/UiAdjustment/SellMaxTripleTriadCards.cs
+++ Tweaks/UiAdjustment/SellMaxTripleTriadCards.cs
@@ -1,28 +1,9 @@
-﻿using FFXIVClientStructs.FFXIV.Client.UI;
-using SimpleTweaksPlugin.Utility;
+﻿using SimpleTweaksPlugin.TweakSystem;
 
 namespace SimpleTweaksPlugin.Tweaks.UiAdjustment; 
 
-public unsafe class SellMaxTripleTriadCards : UiAdjustments.SubTweak {
+public class SellMaxTripleTriadCards : UiAdjustments.SubTweak, IDisabledTweak {
     public override string Name => "Default to max when selling Triple Triad Cards";
     public override string Description => "Set the default number of cards to sell at the Triple Triad Trader to the number of cards you have.";
-    
-    public override void Enable() {
-        Common.AddonSetup += SetupDetour;
-        base.Enable();
-    }
-
-    private void SetupDetour(SetupAddonArgs args) {
-        if (args.AddonName != "ShopCardDialog") return;
-        var addonShopCardDialog = (AddonShopCardDialog*)args.Addon;
-        if (addonShopCardDialog == null) return;
-        if (addonShopCardDialog->CardQuantityInput == null) return;
-        addonShopCardDialog->CardQuantityInput->SetValue(addonShopCardDialog->CardQuantityInput->Data.Max);
-        return;
-    }
-
-    public override void Disable() {
-        Common.AddonSetup -= SetupDetour;
-        base.Disable();
-    }
+    public string DisabledMessage => "This tweak was implemented into the base game as of 6.4";
 }
