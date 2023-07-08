10c39de623e7bde16bcf6d067ddcbcf72500f396
Caraxi <git@caraxian.com>
2023-07-08 10:39:18 +0930

    [`Custom FC Tags`] Full RGB, Icon Viewer

--- Tweaks/UiAdjustment/CustomFreeCompanyTags.cs
+++ Tweaks/UiAdjustment/CustomFreeCompanyTags.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Numerics;
 using System.Text;
-using Dalamud.Game.ClientState.Conditions;
 using Dalamud.Game.Text;
 using Dalamud.Game.Text.SeStringHandling;
 using Dalamud.Game.Text.SeStringHandling.Payloads;
@@ -15,6 +15,7 @@ using Lumina.Excel.GeneratedSheets;
 using SimpleTweaksPlugin.TweakSystem;
 using static FFXIVClientStructs.FFXIV.Client.UI.RaptureAtkModule;
 using FFXIVClientStructs.FFXIV.Client.UI;
+using SimpleTweaksPlugin.ExtraPayloads;
 using SimpleTweaksPlugin.Utility;
 
 namespace SimpleTweaksPlugin.Tweaks.UiAdjustment; 
@@ -45,6 +46,8 @@ public unsafe class CustomFreeCompanyTags : UiAdjustments.SubTweak {
     public override void Setup() {
         AddChangelog("1.8.7.0", "Added option to display FC tags on a separate line to character name.");
         AddChangelog("1.8.7.2", "Removed 'Hide in Duty' option from Wanderer. This is now a vanilla game option.");
+        AddChangelog(Changelog.UnreleasedVersion, "Added support for full RGB colours.");
+        AddChangelog(Changelog.UnreleasedVersion, "Added an icon viewer for supported icons.");
         base.Setup();
     }
 
@@ -91,9 +94,6 @@ public unsafe class CustomFreeCompanyTags : UiAdjustments.SubTweak {
                     _ => customization
                 };
             }
-                
-                
-                
 
             if (customization != null && customization.Enabled) {
                 if (customization.Replacement.Trim().Length == 0) {
@@ -110,7 +110,9 @@ public unsafe class CustomFreeCompanyTags : UiAdjustments.SubTweak {
                     var resetForeground = false;
                     var resetGlow = false;
                     var resetItalic = false;
-                        
+
+                    var resetHexForegrond = false;
+                    var resetHexGlow = false;
 
                     foreach (var t in customization.Replacement) {
                         switch (t) {
@@ -153,33 +155,85 @@ public unsafe class CustomFreeCompanyTags : UiAdjustments.SubTweak {
                                         resetItalic = false;
                                         break;
                                     }
+                                    case "</color>":
+                                    case "</colour>": {
+                                        if (resetHexForegrond) {
+                                            payloads.Add(new ColorEndPayload().AsRaw());
+                                            resetHexForegrond = false;
+                                        }
+
+                                        if (resetForeground) {
+                                            payloads.Add(new UIForegroundPayload(0));
+                                            resetForeground = false;
+                                        }
+                                        break;
+                                    }
+                                    case "</glow>": {
+                                        if (resetHexGlow) {
+                                            payloads.Add(new GlowEndPayload().AsRaw());
+                                            resetHexGlow = false;
+                                        }
+
+                                        if (resetGlow) {
+                                            payloads.Add(new UIGlowPayload(0));
+                                            resetGlow = false;
+                                        }
+                                        break;
+                                    }
                                     case { } s when s.StartsWith("<color:"): {
                                         var k = s.Substring(7, s.Length - 8);
-                                        if (ushort.TryParse(k, out var colorKey)) {
-                                            payloads.Add(new UIForegroundPayload(colorKey));
-                                            resetForeground = colorKey != 0;
+
+                                        if (TryGetColorFromHex(k, out var hexColor)) {
+                                            if (resetHexForegrond) {
+                                                payloads.Add(new ColorEndPayload().AsRaw());
+                                            }
+                                            payloads.Add(new ColorPayload(hexColor).AsRaw());
+                                            resetHexForegrond = true;
                                         } else {
-                                            payloads.Add(new TextPayload(cText));
+                                            if (ushort.TryParse(k, out var colorKey)) {
+                                                payloads.Add(new UIForegroundPayload(colorKey));
+                                                resetForeground = colorKey != 0;
+                                            } else {
+                                                payloads.Add(new TextPayload(cText));
+                                            }
                                         }
+                                        
                                         break;
                                     }
                                     case { } s when s.StartsWith("<colour:"): {
                                         var k = s.Substring(8, s.Length - 9);
-                                        if (ushort.TryParse(k, out var colorKey)) {
-                                            payloads.Add(new UIForegroundPayload(colorKey));
-                                            resetForeground = colorKey != 0;
+                                        if (TryGetColorFromHex(k, out var hexColor)) {
+                                            if (resetHexForegrond) {
+                                                payloads.Add(new ColorEndPayload().AsRaw());
+                                            }
+                                            payloads.Add(new ColorPayload(hexColor).AsRaw());
+                                            resetHexForegrond = true;
                                         } else {
-                                            payloads.Add(new TextPayload(cText));
+                                            if (ushort.TryParse(k, out var colorKey)) {
+                                                payloads.Add(new UIForegroundPayload(colorKey));
+                                                resetForeground = colorKey != 0;
+                                            } else {
+                                                payloads.Add(new TextPayload(cText));
+                                            }
                                         }
+
                                         break;
                                     }
                                     case { } s when s.StartsWith("<glow:"): {
                                         var k = s.Substring(6, s.Length - 7);
-                                        if (ushort.TryParse(k, out var colorKey)) {
-                                            payloads.Add(new UIGlowPayload(colorKey));
-                                            resetGlow = colorKey != 0;
+                                        if (TryGetColorFromHex(k, out var hexColor)) {
+                                            if (resetHexGlow) {
+                                                payloads.Add(new ColorEndPayload().AsRaw());
+                                            }
+                                            payloads.Add(new ColorPayload(hexColor).AsRaw());
+                                            resetHexGlow = true;
                                         } else {
-                                            payloads.Add(new TextPayload(cText));
+                                            if (ushort.TryParse(k, out var colorKey)) {
+                                                payloads.Add(new UIGlowPayload(colorKey));
+                                                resetGlow = colorKey != 0;
+                                            } else {
+                                                payloads.Add(new TextPayload(cText));
+                                            }
                                         }
                                         break;
                                     }
@@ -208,8 +262,6 @@ public unsafe class CustomFreeCompanyTags : UiAdjustments.SubTweak {
                         }
                     }
 
-
-
                     if (!string.IsNullOrWhiteSpace(cText)) {
                         payloads.Add(new TextPayload(cText));
                     }
@@ -217,6 +269,8 @@ public unsafe class CustomFreeCompanyTags : UiAdjustments.SubTweak {
                     if (resetForeground) payloads.Add(new UIForegroundPayload(0));
                     if (resetGlow) payloads.Add(new UIGlowPayload(0));
                     if (resetItalic) payloads.Add(new EmphasisItalicPayload(false));
+                    if (resetHexForegrond) payloads.Add(new ColorEndPayload());
+                    if (resetHexGlow) payloads.Add(new GlowEndPayload());
                         
                     if (!customization.HideQuoteMarks) 
                         payloads.Add(new TextPayload("»"));
@@ -235,6 +289,20 @@ public unsafe class CustomFreeCompanyTags : UiAdjustments.SubTweak {
         return original;
     }
 
+    private bool TryGetColorFromHex(string str, out Vector3 hexColor) {
+        hexColor = Vector3.One;
+        if (str.Length != 7) return false;
+        if (str[0] != '#') return false;
+        if (str.Contains(' ')) return false;
+
+        if (!byte.TryParse(str.AsSpan(1, 2), NumberStyles.HexNumber, NumberFormatInfo.InvariantInfo, out var r) 
+            || !byte.TryParse(str.AsSpan(3, 2), NumberStyles.HexNumber, NumberFormatInfo.InvariantInfo, out var g) 
+            || !byte.TryParse(str.AsSpan(5, 2), NumberStyles.HexNumber, NumberFormatInfo.InvariantInfo, out var b)) return false;
+        
+        hexColor = new Vector3(r / 255f, g / 255f, b / 255f);
+        return true;
+    }
+
     private bool errored;
         
     private string defaultString = "Default (FC)";
@@ -249,8 +317,7 @@ public unsafe class CustomFreeCompanyTags : UiAdjustments.SubTweak {
             ImGui.TableSetupColumn(LocString("FC Tag"), ImGuiTableColumnFlags.WidthFixed | ImGuiTableColumnFlags.NoClip, 75* ImGui.GetIO().FontGlobalScale);
             ImGui.TableSetupColumn(LocString("Replacement"), ImGuiTableColumnFlags.NoClip);
             ImGui.TableHeadersRow();
-
-
+            
             string deleteKey = null;
             string renameKey = null;
                 
@@ -302,21 +369,36 @@ public unsafe class CustomFreeCompanyTags : UiAdjustments.SubTweak {
                 ImGui.TextColored(new Vector4(1, 0, 0, 1), LocString("FCAlreadyAddedError", "FC is already on list."));
             }
 
-            ImGui.TableNextColumn();
-            ImGui.TableNextColumn();
-            ImGui.TableNextColumn();
-            ImGui.TableNextColumn();
-            ImGui.TextColored(new Vector4(0, colourLinkHovered ? 1f : 0.5f, 0.5f, 1), LocString("ColourHelpLink", "Click here for a list of supported icons, colours, and glows."));
-            if (ImGui.IsItemClicked()) Common.OpenBrowser("https://raw.githubusercontent.com/Caraxi/SimpleTweaksPlugin/main/images/placeholderHelp.png");
-            if (colourLinkHovered = ImGui.IsItemHovered()) {
-                ImGui.SetMouseCursor(ImGuiMouseCursor.Hand);
+            ImGui.EndTable();
+            
+            if (ImGui.CollapsingHeader("Supported Icons")) {
+                if (ImGui.BeginTable("iconViewer", 1 + (int)(ImGui.GetContentRegionAvail().X / 100))) {
+
+                    foreach (var i in Plugin.IconManager.FontIcons.Icons) {
+                        if (i.IsValid()) {
+                            ImGui.TableNextColumn();
+                            i.Draw();
+                            if (ImGui.IsItemHovered()) {
+                                ImGui.BeginTooltip();
+                                ImGui.Text($"<icon:{i.ID}>");
+                                ImGui.Separator();
+                                i.DrawScaled(new Vector2(2));
+                                ImGui.EndTooltip();
+                                
+                            }
+
+                            if (ImGui.IsItemClicked()) {
+                                ImGui.SetClipboardText($"<icon:{i.ID}>");
+                            }
+                        }
+                    }
+                    
+                    ImGui.EndTable();
+                }
             }
 
-            ImGui.EndTable();
         }
     };
-
-    private bool colourLinkHovered = false;
 
     private enum ChangeType {
         None,
@@ -373,7 +455,7 @@ public unsafe class CustomFreeCompanyTags : UiAdjustments.SubTweak {
                     ImGui.SetNextWindowPos(new Vector2(ImGui.GetCursorScreenPos().X + ImGui.GetItemRectSize().X - placeholderTooltipSize.X, ImGui.GetCursorScreenPos().Y));
                     ImGui.BeginTooltip();
                     if (ImGui.BeginTable("placeholdersTable", 2, ImGuiTableFlags.Borders)) {
-                        ImGui.TableSetupColumn("Placeholders", ImGuiTableColumnFlags.WidthFixed, 100 * ImGui.GetIO().FontGlobalScale);
+                        ImGui.TableSetupColumn("Placeholders", ImGuiTableColumnFlags.WidthFixed, 190 * ImGui.GetIO().FontGlobalScale);
                         ImGui.TableSetupColumn("Description");
                             
                         ImGui.TableHeadersRow();
@@ -402,9 +484,9 @@ public unsafe class CustomFreeCompanyTags : UiAdjustments.SubTweak {
                         ImGui.TableNextColumn();
                         ImGui.Text($"The {(char)SeIconChar.CrossWorld} icon.");
                         ImGui.TableNextColumn();
-                        ImGui.Text("<colour:#>\n<glow:#>");
+                        ImGui.Text("<colour:#abc123> & </colour>\n<glow:#abc123> & </glow>");
                         ImGui.TableNextColumn();
-                        ImGui.Text($"Change the colour of the tag.\nReplace # with a colour number.");
+                        ImGui.Text($"Change the colour of the tag with full RGB support.\nReplace 'abc123' with any hex colour code.");
                         ImGui.TableNextColumn();
                         ImGui.Text("<icon:#>");
                         ImGui.TableNextColumn();
