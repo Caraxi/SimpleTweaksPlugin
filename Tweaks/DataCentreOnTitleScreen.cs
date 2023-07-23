e7811a6a08aaf4f98fb298de2398d9fc78fe1dae
Caraxi <git@caraxian.com>
2023-07-23 22:05:41 +0930

    [`Data Center on Title Screen`] Added option to include service account

--- Tweaks/DataCentreOnTitleScreen.cs
+++ Tweaks/DataCentreOnTitleScreen.cs
@@ -1,4 +1,6 @@
 ﻿using Dalamud.Game;
+using Dalamud.Game.Config;
+using Dalamud.Utility;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using Lumina.Excel.GeneratedSheets;
@@ -7,11 +9,22 @@ using SimpleTweaksPlugin.Utility;
 
 namespace SimpleTweaksPlugin.Tweaks; 
 
+[Changelog(UnreleasedVersion, "Added option to show the selected service account.")]
 public unsafe class DataCentreOnTitleScreen : Tweak {
     public override string Name => "Data Centre on Title Screen";
     public override string Description => "Shows the current Data Centre on the Title Screen";
 
+    public class Configs : TweakConfig {
+        [TweakConfigOption("Show Service Account Number")]
+        public bool ShowServiceAccountIndex;
+    }
+    
+    public Configs Config { get; private set; }
+
+    public override bool UseAutoConfig => true;
+
     public override void Enable() {
+        Config = LoadConfig<Configs>() ?? new Configs();
         Service.Framework.Update += FrameworkOnUpdate;
         base.Enable();
     }
@@ -30,10 +43,35 @@ public unsafe class DataCentreOnTitleScreen : Tweak {
         if (button == null || (ushort) button->AtkResNode.Type < 1000) return;
         var text = (AtkTextNode*) button->Component->UldManager.SearchNodeById(3);
         if (text == null || text->AtkResNode.Type != NodeType.Text) return;
-        text->SetText(dc.Name.RawData.ToArray());
+        
+        var displayText = $"{dc.Name.ToDalamudString().TextValue}";
+        if (Config.ShowServiceAccountIndex) {
+            var selectedServiceIndex = AgentLobby.Instance()->ServiceAccountIndex;
+            if (selectedServiceIndex < 0) {
+                if (Service.GameConfig.TryGet(SystemConfigOption.ServiceIndex, out uint lastServiceIndex)) {
+                    selectedServiceIndex = (sbyte)lastServiceIndex;
+                }
+            }
+            if (selectedServiceIndex >= 0) {
+                displayText += $"\nService Account {selectedServiceIndex + 1}";
+                text->TextFlags |= (byte)TextFlags.MultiLine;
+                text->FontSize = 15;
+                text->LineSpacing = 13;
+                text->SetAlignment(AlignmentType.Top);
+                text->SetText(displayText);
+                return;
+            }
+        }
+            
+        text->FontSize = 18;
+        text->LineSpacing = 18;
+        unchecked { text->TextFlags &= (byte)~TextFlags.MultiLine; }
+        text->SetAlignment(AlignmentType.Center);
+        text->SetText(displayText);
     }
 
     public override void Disable() {
+        SaveConfig(Config);
         Service.Framework.Update -= FrameworkOnUpdate;
         base.Disable();
     }
