d62a3ec419460cabdd091a35931ba1cac4e7afc9
Caraxi <git@caraxian.com>
2023-09-09 10:25:06 +0930

    Update TweakNameAttribute.cs

--- TweakSystem/Attributes/TweakNameAttribute.cs
+++ TweakSystem/Attributes/TweakNameAttribute.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace SimpleTweaksPlugin.TweakSystem; 
 
+[AttributeUsage(AttributeTargets.Class)]
 public class TweakNameAttribute : Attribute {
     public string Name { get; }
     public TweakNameAttribute(string name) {
