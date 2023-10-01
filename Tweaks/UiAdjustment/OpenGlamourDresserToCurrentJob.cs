2de22198a067e6f1d7b63a1c9f3a0c9bb2b7cb15
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-10-01 13:21:17 -0700

    [`OpenGlamourDresserToCurrentJob`] Add New Tweak

---
+++ Tweaks/UiAdjustment/OpenGlamourDresserToCurrentJob.cs
@@ -0,0 +1,18 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+using SimpleTweaksPlugin.Events;
+using SimpleTweaksPlugin.TweakSystem;
+
+namespace SimpleTweaksPlugin.Tweaks.UiAdjustment;
+
+[TweakName("Open Glamour Dresser to Current Job")]
+[TweakAuthor("MidoriKami")]
+[TweakDescription("Sets the job selection dropdown to your current job when opening the Glamour Dresser.")]
+[TweakReleaseVersion(UnreleasedVersion)]
+public unsafe class OpenGlamourDresserToCurrentJob : UiAdjustments.SubTweak {
+    [AddonSetup("MiragePrismPrismBox")]
+    private void OnMiragePrismBoxOpen(AtkUnitBase* atkUnitBase) {
+        if (Service.ClientState is { LocalPlayer.ClassJob.Id: var playerJob }) {
+            *((byte*) atkUnitBase + 400) = (byte)playerJob;
+        }
+    }
+}
