5d3b1625e7e4e55bd0bb665c745f2ab51f1798ee
Caraxi <git@caraxian.com>
2023-10-05 22:20:36 +1030

    [`Duty List Background`] Apply scaling

--- Tweaks/UiAdjustment/DutyListBackground.cs
+++ Tweaks/UiAdjustment/DutyListBackground.cs
@@ -64,8 +64,8 @@ public unsafe class DutyListBackground : UiAdjustments.SubTweak {
                 }
 
                 imageNode->AtkResNode.ToggleVisibility(unitBase->IsVisible && unitBase->RootNode->IsVisible && (unitBase->VisibilityFlags & 1) == 0);
-                imageNode->AtkResNode.SetWidth(unitBase->RootNode->GetWidth());
-                imageNode->AtkResNode.SetHeight(unitBase->RootNode->GetHeight());
+                imageNode->AtkResNode.SetWidth((ushort)unitBase->GetScaledWidth(true));
+                imageNode->AtkResNode.SetHeight((ushort)unitBase->GetScaledHeight(true));
                 imageNode->AtkResNode.SetPositionFloat(unitBase->X, unitBase->Y);
                 imageNode->AtkResNode.Color.A = (byte)(TweakConfig.BackgroundColor.W * 255);
                 imageNode->AtkResNode.AddRed = (byte)(TweakConfig.BackgroundColor.X * 255);
