64ddffadb747c172f6d6294897d7209ca3b39ef6
Caraxi <git@caraxian.com>
2023-10-13 01:30:45 +1030

    Update NoCharacterNotFoundWarning.cs

--- Tweaks/NoCharacterNotFoundWarning.cs
+++ Tweaks/NoCharacterNotFoundWarning.cs
@@ -3,7 +3,7 @@ using SimpleTweaksPlugin.TweakSystem;
 
 namespace SimpleTweaksPlugin.Tweaks; 
 
-[TweakName("Hide 'Character not found' Message.")]
+[TweakName("Hide 'Character not found' Message")]
 [TweakDescription("Prevent the game from displaying the \"The character you last logged out with could not be found on the current data center.\" message.")]
 [TweakReleaseVersion(UnreleasedVersion)]
 public unsafe class NoCharacterNotFoundWarning : Tweak {
