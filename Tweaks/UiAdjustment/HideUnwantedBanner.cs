9096b742966bec05b75ce42f46369bfcc772a41a
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-10-13 22:27:56 -0700

    [`HideUnwantedBanners`] Modernize Tweak

--- Tweaks/UiAdjustment/HideUnwantedBanner.cs
+++ Tweaks/UiAdjustment/HideUnwantedBanner.cs
@@ -6,24 +6,24 @@ using Dalamud.Utility.Signatures;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using ImGuiNET;
 using SimpleTweaksPlugin.TweakSystem;
+using SimpleTweaksPlugin.Utility;
 
 namespace SimpleTweaksPlugin.Tweaks.UiAdjustment;
 
-public unsafe class HideUnwantedBanner : UiAdjustments.SubTweak
-{
-    public override string Name => "Hide Unwanted Banners";
-    public override string Description => "Hide information banners such as 'Venture Complete', or 'Levequest Accepted'";
-    protected override string Author => "MidoriKami";
-
+[TweakName("Hide Unwanted Banners")]
+[TweakDescription("Hide information banners such as 'Venture Complete', or 'Levequest Accepted'.")]
+[TweakAuthor("MidoriKami")]
+[TweakAutoConfig]
+[TweakReleaseVersion("1.8.3.0")]
+public unsafe class HideUnwantedBanner : UiAdjustments.SubTweak {
     private delegate void ImageSetImageTextureDelegate(AtkUnitBase* addon, int bannerId, int a3, int sfxId);
 
-    [Signature("48 89 5C 24 ?? 57 48 83 EC 30 48 8B D9 89 91", DetourName = nameof(OnSetImageTexture))]
-    private readonly Hook<ImageSetImageTextureDelegate>? setImageTextureHook = null!;
+    [TweakHook, Signature("48 89 5C 24 ?? 57 48 83 EC 30 48 8B D9 89 91", DetourName = nameof(OnSetImageTexture))]
+    private readonly HookWrapper<ImageSetImageTextureDelegate>? setImageTextureHook = null!;
 
     private record BannerSetting(int Id, string Label);
 
-    private readonly List<BannerSetting> banners = new()
-    {
+    private readonly List<BannerSetting> banners = new() {
         new BannerSetting(120031, "Levequest Accepted"),
         new BannerSetting(120032, "Levequest Complete"),
         new BannerSetting(120055, "Delivery Complete"),
@@ -43,71 +43,36 @@ public unsafe class HideUnwantedBanner : UiAdjustments.SubTweak
         new BannerSetting(121082, "Tribal Quest Complete"),
     };
     
-    private class Config : TweakConfig
-    {
+    public class Config : TweakConfig {
         public readonly List<int> HiddenBanners = new();
     }
 
-    private Config TweakConfig { get; set; } = null!;
+    public Config TweakConfig { get; set; } = null!;
     
-    protected override DrawConfigDelegate DrawConfigTree => (ref bool _) =>
-    {
-        foreach (var banner in banners)
-        {
+    protected void DrawConfig() {
+        foreach (var banner in banners) {
             var enabled = TweakConfig.HiddenBanners.Contains(banner.Id);
-            if (ImGui.Checkbox(banner.Label, ref enabled))
-            {
-                if (TweakConfig.HiddenBanners.Contains(banner.Id) && !enabled)
-                {
+            if (ImGui.Checkbox(banner.Label, ref enabled)) {
+                if (TweakConfig.HiddenBanners.Contains(banner.Id) && !enabled) {
                     TweakConfig.HiddenBanners.Remove(banner.Id);
                     SaveConfig(TweakConfig);
+
                 }
-                else if(!TweakConfig.HiddenBanners.Contains(banner.Id) && enabled)
-                {
+                else if(!TweakConfig.HiddenBanners.Contains(banner.Id) && enabled) {
                     TweakConfig.HiddenBanners.Add(banner.Id);
                     SaveConfig(TweakConfig);
                 }
             }
         }
-    };
+    }
     
-    public override void Setup()
-    {
-        if (Ready) return;
-        AddChangelogNewTweak("1.8.3.0");
-        base.Setup();
-    }
-
-    protected override void Enable()
-    {
-        TweakConfig = LoadConfig<Config>() ?? new Config();
-        setImageTextureHook?.Enable();
-        base.Enable();
-    }
-
-    protected override void Disable()
-    {
-        SaveConfig(TweakConfig);
-        setImageTextureHook?.Disable();
-        base.Disable();
-    }
-
-    public override void Dispose()
-    {
-        setImageTextureHook?.Dispose();
-        base.Dispose();
-    }
-
-    private void OnSetImageTexture(AtkUnitBase* addon, int bannerId, int a3, int soundEffectId)
-    {
+    private void OnSetImageTexture(AtkUnitBase* addon, int bannerId, int a3, int soundEffectId) {
         var skipOriginal = false;
         
-        try
-        {
+        try {
             skipOriginal = TweakConfig.HiddenBanners.Contains(bannerId);
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             SimpleLog.Error(e, "Something went wrong in HideUnwantedBanners, let MidoriKami know!");
         }
 
