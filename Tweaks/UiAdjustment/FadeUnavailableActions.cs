e235dc84ae0600e32fcb03a75a5bb32f13886df4
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-10-17 23:42:25 -0700

    [`FadeUnavailableActions`] Modernize Tweak (#640)

--- Tweaks/UiAdjustment/FadeUnavailableActions.cs
+++ Tweaks/UiAdjustment/FadeUnavailableActions.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
-using Dalamud.Hooking;
 using Dalamud.Utility.Signatures;
 using FFXIVClientStructs.FFXIV.Client.Game;
 using FFXIVClientStructs.FFXIV.Client.UI;
@@ -13,23 +12,27 @@ using Action = Lumina.Excel.GeneratedSheets.Action;
 
 namespace SimpleTweaksPlugin.Tweaks.UiAdjustment;
 
-public unsafe class FadeUnavailableActions : UiAdjustments.SubTweak
-{
-    public override string Name => "Fade Unavailable Actions";
-    public override string Description => "Instead of darkening icons, makes them transparent when unavailable";
-    protected override string Author => "MidoriKami";
-
+[TweakName("Fade Unavailable Actions")]
+[TweakDescription("Instead of darkening icons, makes them transparent when unavailable.")]
+[TweakAuthor("MidoriKami")]
+[TweakAutoConfig]
+[TweakReleaseVersion("1.8.3.1")]
+[Changelog("1.8.3.2", "Tweak now only applies to the icon image itself and not the entire button")]
+[Changelog("1.8.3.2", "Add option to apply transparency to the slot frame of the icon")]
+[Changelog("1.8.3.2", "Add option to apply to sync'd skills only")]
+[Changelog("1.8.4.0", "Tweak now only applies to combat actions")]
+[Changelog("1.8.4.0", "Properly resets hotbar state on unload/disable")]
+public unsafe class FadeUnavailableActions : UiAdjustments.SubTweak {
     private delegate void UpdateHotBarSlotDelegate(AddonActionBarBase* addon, SlotData* uiData, NumberArrayData* numberArray, StringArrayData* stringArray, int numberArrayIndex, int stringArrayIndex);
     
-    [Signature("E8 ?? ?? ?? ?? 49 81 C6 ?? ?? ?? ?? 83 C7 10", DetourName = nameof(OnHotBarSlotUpdate))]
-    private readonly Hook<UpdateHotBarSlotDelegate>? onHotBarSlotUpdateHook = null!;
+    [TweakHook, Signature("E8 ?? ?? ?? ?? 49 81 C6 ?? ?? ?? ?? 83 C7 10", DetourName = nameof(OnHotBarSlotUpdate))]
+    private readonly HookWrapper<UpdateHotBarSlotDelegate>? onHotBarSlotUpdateHook = null!;
 
     private readonly Dictionary<uint, Action> actionCache = new();
 
     private readonly List<string> addonActionBarNames = new() { "_ActionBar", "_ActionBar01", "_ActionBar02", "_ActionBar03", "_ActionBar04", "_ActionBar05", "_ActionBar06", "_ActionBar07", "_ActionBar08", "_ActionBar09","_ActionCross", "_ActionDoubleCrossR", "_ActionDoubleCrossL" };
     
-    public class Config : TweakConfig
-    {
+    public class Config : TweakConfig {
         [TweakConfigOption("Fade Percentage", IntMax = 90, IntMin = 0, IntType = TweakConfigOptionAttribute.IntEditType.Slider, EditorSize = 150)]
         public int FadePercentage = 70;
 
@@ -42,55 +45,19 @@ public unsafe class FadeUnavailableActions : UiAdjustments.SubTweak
     
     public Config TweakConfig { get; private set; } = null!;
 
-    public override bool UseAutoConfig => true;
-    
-    public override void Setup()
-    {
-        if (Ready) return;
-        AddChangelogNewTweak("1.8.3.1");
-        AddChangelog("1.8.3.2", "Tweak now only applies to the icon image itself and not the entire button");
-        AddChangelog("1.8.3.2", "Add option to apply transparency to the slot frame of the icon");
-        AddChangelog("1.8.3.2", "Add option to apply to sync'd skills only");
-        AddChangelog("1.8.4.0", "Tweak now only applies to combat actions");
-        AddChangelog("1.8.4.0", "Properly resets hotbar state on unload/disable");
-        base.Setup();
-    }
-
-    protected override void Enable()
-    {
-        TweakConfig = LoadConfig<Config>() ?? new Config();
-        onHotBarSlotUpdateHook?.Enable();
-        base.Enable();
-    }
-
-    protected override void Disable()
-    {
-        SaveConfig(TweakConfig);
-        onHotBarSlotUpdateHook?.Disable();
+    protected override void Disable() {
         ResetAllHotBars();
-        base.Disable();
     }
-
-    public override void Dispose()
-    {
-        onHotBarSlotUpdateHook?.Dispose();
-        base.Dispose();
-    }
-
-    private void ResetAllHotBars()
-    {
-        foreach (var addonName in addonActionBarNames)
-        {
+    
+    private void ResetAllHotBars() {
+        foreach (var addonName in addonActionBarNames) {
             var addon = (AddonActionBarBase*) Common.GetUnitBase(addonName);
             if (addon is null) continue;
 
-            foreach (var slot in addon->Slot)
-            {
-                if (slot.Icon is not null)
-                {
+            foreach (var slot in addon->Slot) {
+                if (slot.Icon is not null) {
                     var iconComponent = (AtkComponentIcon*) slot.Icon->Component;
-                    if (iconComponent is not null)
-                    {
+                    if (iconComponent is not null) {
                         iconComponent->IconImage->AtkResNode.Color.A = 0xFF;
                         iconComponent->Frame->Color.A = 0xFF;
                     }
@@ -99,41 +66,35 @@ public unsafe class FadeUnavailableActions : UiAdjustments.SubTweak
         }
     }
     
-    private void OnHotBarSlotUpdate(AddonActionBarBase* addon, SlotData* hotBarSlotData, NumberArrayData* numberArray, StringArrayData* stringArray, int numberArrayIndex, int stringArrayIndex)
-    {
+    private void OnHotBarSlotUpdate(AddonActionBarBase* addon, SlotData* hotBarSlotData, NumberArrayData* numberArray, StringArrayData* stringArray, int numberArrayIndex, int stringArrayIndex) {
         try {
             ProcessHotBarSlot(hotBarSlotData, numberArray, numberArrayIndex);
-        }
-        catch (Exception e)
-        {
+        } 
+        catch (Exception e) {
             SimpleLog.Error(e, "Something went wrong in FadeUnavailableActions, let MidoriKami know!");
         }
         
         onHotBarSlotUpdateHook!.Original(addon, hotBarSlotData, numberArray, stringArray, numberArrayIndex, stringArrayIndex);
     }
 
-    private void ProcessHotBarSlot(SlotData* hotBarSlotData, NumberArrayData* numberArray, int numberArrayIndex)
-    {
+    private void ProcessHotBarSlot(SlotData* hotBarSlotData, NumberArrayData* numberArray, int numberArrayIndex) {
         if (hotBarSlotData->ActionId > ushort.MaxValue) return;
         if (Service.ClientState.LocalPlayer is { IsCasting: true } ) return;
 
         var numberArrayData = (NumberArrayStruct*) (&numberArray->IntArray[numberArrayIndex]);
         
         // If action type is not combat action, remove transparency and return
-        if (numberArrayData->ActionType != 45)
-        {
+        if (numberArrayData->ActionType != 45) {
             ApplyTransparency(hotBarSlotData, false);
             return;
         }
 
-        if (TweakConfig.ApplyToSyncActions)
-        {
+        if (TweakConfig.ApplyToSyncActions) {
             var action = GetAction(hotBarSlotData->ActionId);
             var actionLevel = action.ClassJobLevel;
             var playerLevel = Service.ClientState.LocalPlayer?.Level ?? 0;
 
-            switch (action)
-            {
+            switch (action) {
                 case { IsRoleAction: false } when actionLevel > playerLevel:
                     ApplyTransparency(hotBarSlotData, true);
                     break;
@@ -143,14 +104,12 @@ public unsafe class FadeUnavailableActions : UiAdjustments.SubTweak
                     break;
             }
         }
-        else
-        {
+        else {
             ApplyTransparency(hotBarSlotData, ShouldFadeAction(numberArrayData));
         }
     }
 
-    private Action GetAction(uint actionId)
-    {
+    private Action GetAction(uint actionId) {
         var adjustedActionId = ActionManager.Instance()->GetAdjustedActionId(actionId);
 
         if (actionCache.TryGetValue(adjustedActionId, out var action)) return action;
@@ -162,8 +121,7 @@ public unsafe class FadeUnavailableActions : UiAdjustments.SubTweak
 
     private bool ShouldFadeAction(NumberArrayStruct* numberArrayData) => !(numberArrayData->ActionAvailable_1 && numberArrayData->ActionAvailable_2);
 
-    private void ApplyTransparency(SlotData* hotBarSlotData, bool fade)
-    {
+    private void ApplyTransparency(SlotData* hotBarSlotData, bool fade) {
         if (hotBarSlotData is null) return;
         var iconComponent = (AtkComponentIcon*) hotBarSlotData->IconComponentNode->Component;
 
@@ -171,21 +129,18 @@ public unsafe class FadeUnavailableActions : UiAdjustments.SubTweak
         if (iconComponent->IconImage is null) return;
         if (iconComponent->Frame is null) return;
 
-        if (fade)
-        {
+        if (fade) {
             iconComponent->IconImage->AtkResNode.Color.A = (byte)(0xFF * ((100 - TweakConfig.FadePercentage) / 100.0f));
             if(TweakConfig.ApplyToFrame) iconComponent->Frame->Color.A = (byte)(0xFF * ((100 - TweakConfig.FadePercentage) / 100.0f));
         }
-        else
-        {
+        else {
             iconComponent->IconImage->AtkResNode.Color.A = 0xFF;
             iconComponent->Frame->Color.A = 0xFF;
         }
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x40)]
-    private struct NumberArrayStruct
-    {
+    private struct NumberArrayStruct {
         [FieldOffset(0x00)] public uint ActionType;
         [FieldOffset(0x0C)] public uint ActionId;
         [FieldOffset(0x14)] public bool ActionAvailable_1;
@@ -195,8 +150,7 @@ public unsafe class FadeUnavailableActions : UiAdjustments.SubTweak
     }
     
     [StructLayout(LayoutKind.Explicit, Size = 0xC8)]
-    private struct SlotData
-    {
+    private struct SlotData {
         [FieldOffset(0x04)] public uint ActionId;
         [FieldOffset(0x88)] public AtkComponentDragDrop* DragDropNode;
         [FieldOffset(0x90)] public AtkComponentNode* IconComponentNode;
