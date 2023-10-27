c2f9b0270c26c058de0bfc5d25d007903251b7b0
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-10-27 15:43:24 -0700

    [`ExitGame`] Add new Tweak (#650)
    * [`ExitGame`] Add new Tweak
    
    * [`ExitGame`] Remove imgui alt-check
    
    * [`ExitGame`] Use new UIInputData helpers
    
    * [`ExitGame`] Disallow on title screen
    
    * [`ExitGame`] Use new SeVirtualKey enum
    
    * Add version info

---
+++ Tweaks/ExitGame.cs
@@ -0,0 +1,20 @@
+﻿using FFXIVClientStructs.FFXIV.Client.UI;
+using SimpleTweaksPlugin.Events;
+using SimpleTweaksPlugin.TweakSystem;
+
+namespace SimpleTweaksPlugin.Tweaks.Chat;
+
+[TweakName("Alt + F4 Exit Game")]
+[TweakDescription("Pressing Alt + F4 will cause the game to close safely.")]
+[TweakAuthor("MidoriKami")]
+[TweakReleaseVersion(UnreleasedVersion)]
+public unsafe class ExitGame : Tweak {
+    [FrameworkUpdate]
+    private void FrameworkUpdate() {
+        if (!Service.ClientState.IsLoggedIn) return;
+        
+        if (UIInputData.Instance()->IsKeyDown(SeVirtualKey.MENU) && UIInputData.Instance()->IsKeyPressed(SeVirtualKey.F4)) {
+            UIModule.Instance()->ExecuteMainCommand(24);
+        }
+    }
+}
