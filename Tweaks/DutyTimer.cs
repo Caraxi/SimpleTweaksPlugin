0f0b54c3c3c31f8f4d8c7de7a533e372bf630fcd
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-11-22 00:31:04 -0800

    [`DutyTimer`] Add new tweak (#660)
    * [`DutyTimer`] Add new tweak
    
    * [`DutyTimer`] Fix timestamp formatting

---
+++ Tweaks/DutyTimer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SimpleTweaksPlugin.TweakSystem;
+
+namespace SimpleTweaksPlugin.Tweaks; 
+
+[TweakName("Duty Timer")]
+[TweakDescription("When completing a duty, tells you how much time the duty took.")]
+[TweakAuthor("MidoriKami")]
+[TweakReleaseVersion(UnreleasedVersion)]
+public class DutyTimer : Tweak {
+    private DateTime startTimestamp;
+
+    protected override void Enable() {
+        Service.DutyState.DutyStarted += OnDutyStarted;
+        Service.DutyState.DutyCompleted += OnDutyCompleted;
+    }
+
+    protected override void Disable() {
+        Service.DutyState.DutyStarted -= OnDutyStarted;
+        Service.DutyState.DutyCompleted -= OnDutyCompleted;
+    }
+    
+    private void OnDutyStarted(object sender, ushort e) 
+        => startTimestamp = DateTime.UtcNow;
+
+    private void OnDutyCompleted(object sender, ushort e) 
+        => Service.Chat.Print($@"Duty Completed in: {DateTime.UtcNow - startTimestamp:hh\:mm\:ss\.ffff}");
+}
