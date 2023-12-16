98c118ae2e5488ca12a37ccdeefc19db6ca83c1d
Sami Laine <sami.v.laine@gmail.com>
2023-12-16 09:35:01 +0200

    [`Set Option Command`] Add "small" and "smallest" to `DisplayNameSize` (#679)

--- Tweaks/SetOptionCommand.cs
+++ Tweaks/SetOptionCommand.cs
@@ -146,7 +146,7 @@ public unsafe class SetOptionCommand : CommandTweak {
         new OptionDefinition<uint>("CutsceneAudioLanguage", "CutsceneMovieVoice", OptionGroup.System, ValueType.AudioLanguage, "cl"),
         new OptionDefinition<uint>("DisplayNameSize", "NamePlateDispSize", OptionGroup.UiConfig, () => {
             return (
-                new() { ["maximum"] = 2, ["large"] = 1, ["standard"] = 0 },
+                new() { ["maximum"] = 2, ["large"] = 1, ["standard"] = 0, ["small"] = 3, ["smallest"] = 4 },
                 new() { ["m"] = 2, ["max"] = 2, ["l"] = 1, ["s"] = 0, }
             );
         }, "dns") { AllowToggle = true },
