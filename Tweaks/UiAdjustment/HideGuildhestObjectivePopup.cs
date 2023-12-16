7ec4c8165caf3caca96d40f9d0eb53e1f06e9453
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-12-15 23:45:32 -0800

    [`HideGuildhestObjectivePopup`] Add New Tweak (#678)

---
+++ Tweaks/UiAdjustment/HideGuildhestObjectivePopup.cs
@@ -0,0 +1,18 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+using Lumina.Excel.GeneratedSheets2;
+using SimpleTweaksPlugin.Events;
+using SimpleTweaksPlugin.TweakSystem;
+
+namespace SimpleTweaksPlugin.Tweaks.UiAdjustment; 
+
+[TweakName("Hide Guildhest Objective Popup")]
+[TweakAuthor("MidoriKami")]
+[TweakDescription("Hides the objective popup when starting a guildhest.")]
+[TweakReleaseVersion(UnreleasedVersion)]
+public unsafe class HideGuildhestObjectivePopup : UiAdjustments.SubTweak {
+    [AddonPreSetup("JournalAccept")]
+    private void JournalAcceptPreSetup(AtkUnitBase* addon) {
+        if (Service.Data.GetExcelSheet<TerritoryType>()!.GetRow(Service.ClientState.TerritoryType) is not { TerritoryIntendedUse: 3 }) return;
+        addon->Hide(false, false, 1);
+    }
+}
