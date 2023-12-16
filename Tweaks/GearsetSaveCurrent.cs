42d24ef28d4216ce130661fed33df150c5830e44
Caraxi <git@caraxian.com>
2023-12-16 20:30:15 +1030

    New Tweak: `Gearset Update Command`
    #635

---
+++ Tweaks/GearsetSaveCurrent.cs
@@ -0,0 +1,32 @@
+﻿using FFXIVClientStructs.FFXIV.Client.Game.UI;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using SimpleTweaksPlugin.Tweaks.AbstractTweaks;
+using SimpleTweaksPlugin.TweakSystem;
+
+namespace SimpleTweaksPlugin.Tweaks;
+
+[TweakName("Gearset Update Command")]
+[TweakDescription("Updates the active gearset with your current equipment.")]
+[TweakReleaseVersion(UnreleasedVersion)]
+public unsafe class GearsetSaveCurrent : CommandTweak {
+    protected override string Command => "/updategearset";
+
+    protected override void OnCommand(string args) {
+        var module = RaptureGearsetModule.Instance();
+        if (module->CurrentGearsetIndex < 0 || !module->IsValidGearset(module->CurrentGearsetIndex)) {
+            Service.Chat.PrintError("No gearset is active.");
+            return;
+        }
+
+        var activeGearset = module->GetGearset(module->CurrentGearsetIndex);
+        if (activeGearset == null) return;
+
+        if (activeGearset->ClassJob != UIState.Instance()->PlayerState.CurrentClassJobId) {
+            Service.Chat.PrintError("Active gearset class does not match current class.");
+            return;
+        }
+        
+        module->UpdateGearset(module->CurrentGearsetIndex);
+    }
+}
+
