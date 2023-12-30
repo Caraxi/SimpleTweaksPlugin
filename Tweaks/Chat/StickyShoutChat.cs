a993366976da678760779c1a4a7d23c2a7e858d0
Kaz Wolfe <root@kazwolfe.io>
2023-12-30 14:27:06 -0800

    [`StickyShoutChat`] Add New Tweak
    Nice small tweak to bypass the usual method that causes the chat window to reset from Shout chat.

---
+++ Tweaks/Chat/StickyShoutChat.cs
@@ -0,0 +1,19 @@
+﻿using Dalamud.Utility.Signatures;
+using FFXIVClientStructs.FFXIV.Client.UI.Shell;
+using SimpleTweaksPlugin.TweakSystem;
+using SimpleTweaksPlugin.Utility;
+
+namespace SimpleTweaksPlugin.Tweaks.Chat; 
+
+[TweakReleaseVersion(UnreleasedVersion)]
+public unsafe class StickyShoutChat : ChatTweaks.SubTweak {
+    public override string Name => "Sticky Shout Chat";
+    public override string Description => "Prevents the game from automatically switching out of shout chat.";
+    
+    private delegate void FocusPreviousChatChannelDelegate(RaptureShellModule* self);
+
+    [TweakHook, Signature("E8 ?? ?? ?? ?? 33 C0 49 8B CE", DetourName = nameof(SkipFocusChannel))]
+    private readonly HookWrapper<FocusPreviousChatChannelDelegate> focusPreviousChannelHook;
+
+    private void SkipFocusChannel(RaptureShellModule* self) { }
+}
