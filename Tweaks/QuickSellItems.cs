67e62aa87af6b2330a5dd7c0f8db23939c01b902
Caraxi <git@caraxian.com>
2024-01-31 16:31:42 +1030

    [`Quick Sell`] Add option to allow quick selling to retainer

--- Tweaks/QuickSellItems.cs
+++ Tweaks/QuickSellItems.cs
@@ -12,17 +12,18 @@ using ValueType = FFXIVClientStructs.FFXIV.Component.GUI.ValueType;
 
 namespace SimpleTweaksPlugin.Tweaks; 
 
+[TweakName("Quick Sell Items at Vendors")]
+[TweakDescription("Hold a modifier key to sell items from your inventory in one click.")]
+[Changelog(UnreleasedVersion, "Added option to allow quick selling items from player inventory while using a retainer.")]
 public unsafe class QuickSellItems : Tweak {
-
-    public override string Name => "Quick Sell Items at Vendors";
-    public override string Description => "Hold a modifier key to sell items from your inventory in one click.";
-
+    
     public class Configs : TweakConfig {
         public bool Shift = true;
         public bool Ctrl = false;
         public bool Alt = false;
 
         public bool RetainerSell = false;
+        public bool SellAtRetainer = false;
     }
 
     private delegate void* OpenInventoryContext(AgentInventoryContext* agent, InventoryType inventory, ushort slot, int a4, ushort a5, byte a6);
@@ -30,14 +31,14 @@ public unsafe class QuickSellItems : Tweak {
     
     public Configs Config { get; private set; }
 
-    protected override DrawConfigDelegate DrawConfigTree => (ref bool hasChanged) => {
+    private void DrawConfig() {
         ImGui.Text("Modifier Keys to Auto Sell:");
         ImGui.Dummy(Vector2.Zero);
         ImGui.Indent();
         ImGui.BeginGroup();
-        hasChanged |= ImGui.Checkbox("Shift", ref Config.Shift);
-        hasChanged |= ImGui.Checkbox("Ctrl", ref Config.Ctrl);
-        hasChanged |= ImGui.Checkbox("Alt", ref Config.Alt);
+        ImGui.Checkbox("Shift", ref Config.Shift);
+        ImGui.Checkbox("Ctrl", ref Config.Ctrl);
+        ImGui.Checkbox("Alt", ref Config.Alt);
         ImGui.EndGroup();
         var s = ImGui.GetItemRectSize();
         var min = ImGui.GetItemRectMin();
@@ -60,12 +61,13 @@ public unsafe class QuickSellItems : Tweak {
         ImGui.EndGroup();
         ImGui.Unindent();
         ImGui.Dummy(Vector2.Zero);
-        ImGui.Checkbox("Enable tweak in retainer inventory", ref Config.RetainerSell);
+        ImGui.Checkbox("Enable Quick Sell from retainer inventory", ref Config.RetainerSell);
         ImGui.Dummy(Vector2.Zero);
-
+        ImGui.Checkbox("Enable Quick Sell from player inventory at retainer", ref Config.SellAtRetainer);
+        ImGui.Dummy(Vector2.Zero);
         
         ImGui.Text("You may still receive a prompt to confirm for some items.\nThis can be disabled in the Character Configuration.");
-    };
+    }
 
     public InventoryType[] CanSellFrom = {
         InventoryType.Inventory1,
@@ -118,7 +120,7 @@ public unsafe class QuickSellItems : Tweak {
         var retVal = openInventoryContextHook.Original(agent, inventoryType, slot, a4, a5, a6);
 
         try {
-            void TrySell(string sellText) {
+            bool TrySell(string sellText) {
                 var inventory = InventoryManager.Instance()->GetInventoryContainer(inventoryType);
                 if (inventory != null) {
                     var itemSlot = inventory->GetInventorySlot(slot);
@@ -127,9 +129,9 @@ public unsafe class QuickSellItems : Tweak {
                         var item = Service.Data.Excel.GetSheet<Item>()?.GetRow(itemId);
                         if (item != null) {
                             var addonId = agent->AgentInterface.GetAddonID();
-                            if (addonId == 0) return;
+                            if (addonId == 0) return false;
                             var addon = Common.GetAddonByID(addonId);
-                            if (addon == null) return;
+                            if (addon == null) return false;
 
                             for (var i = 0; i < agent->ContextItemCount; i++) {
                                 var contextItemParam = agent->EventParamsSpan[agent->ContexItemStartIndex + i];
@@ -140,20 +142,26 @@ public unsafe class QuickSellItems : Tweak {
                                     Common.GenerateCallback(addon, 0, i, 0U, 0, 0);
                                     agent->AgentInterface.Hide();
                                     UiHelper.Close(addon);
-                                    return;
+                                    return true;
                                 }
                             }
                         }
                     }
                 }
+
+                return false;
             }
 
             if (CanSellFrom.Contains(inventoryType) && HotkeyIsHeld && Common.GetUnitBase("Shop") != null) {
-                TrySell(sellText);
+                if (TrySell(sellText)) return retVal;
             }
 
             if (Config.RetainerSell && RetainerInventories.Contains(inventoryType) && HotkeyIsHeld && Common.GetUnitBase("RetainerGrid0") != null) {
-                TrySell(retainerSellText);
+                if (TrySell(retainerSellText)) return retVal;
+            }
+
+            if (Config.SellAtRetainer && CanSellFrom.Contains(inventoryType) && HotkeyIsHeld && Common.GetUnitBase("RetainerGrid0") != null) {
+                if (TrySell(retainerSellText)) return retVal;
             }
         } catch (Exception ex) {
             SimpleLog.Error(ex);
