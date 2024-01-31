423f699568f0a3fc86421e0083f19b9a34e43a6a
KazWolfe <KazWolfe@users.noreply.github.com>
2024-01-30 23:07:59 -0800

    [`StickyShoutChat`] Add New Tweak (#692)
    * [`StickyShoutChat`] Add New Tweak
    
    Nice small tweak to bypass the usual method that causes the chat window to reset from Shout chat.
    
    * Avoid breaking temporary channels set with hotkeys
    
    ---------
    
    Co-authored-by: Caraxi <git@caraxian.com>

---
+++ Tweaks/Chat/StickyShoutChat.cs
@@ -0,0 +1,23 @@
+﻿using Dalamud;
+using SimpleTweaksPlugin.TweakSystem;
+
+namespace SimpleTweaksPlugin.Tweaks.Chat; 
+
+[TweakName("Sticky Shout Chat")]
+[TweakDescription("Prevents the game from automatically switching out of shout chat.")]
+[TweakReleaseVersion(UnreleasedVersion)]
+public class StickyShoutChat : ChatTweaks.SubTweak {
+    private nint editAddress;
+    
+    protected override void Enable() {
+        // Skip the 
+        if (!Service.SigScanner.TryScanText("05 75 0C 8B D7 E8 ?? ?? ?? ?? E9", out editAddress)) return;
+        SafeMemory.Write(editAddress, (sbyte)-2);
+    }
+
+    protected override void Disable() {
+        if (editAddress == nint.Zero) return;
+        if (SafeMemory.Write(editAddress, (sbyte) 5))
+            editAddress = nint.Zero;
+    }
+}
