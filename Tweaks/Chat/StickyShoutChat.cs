d433b0b916c17dd2ba81b2a201230234eec30d94
Caraxi <git@caraxian.com>
2024-07-07 20:36:26 +0930

    Sticky Shout Chat

--- Tweaks/Chat/StickyShoutChat.cs
+++ Tweaks/Chat/StickyShoutChat.cs
@@ -1,23 +1,22 @@
 using Dalamud;
 using SimpleTweaksPlugin.TweakSystem;
 
-namespace SimpleTweaksPlugin.Tweaks.Chat; 
+namespace SimpleTweaksPlugin.Tweaks.Chat;
 
 [TweakName("Sticky Shout Chat")]
 [TweakDescription("Prevents the game from automatically switching out of shout chat.")]
 [TweakReleaseVersion("1.9.6.0")]
 public class StickyShoutChat : ChatTweaks.SubTweak {
     private nint editAddress;
-    
+
     protected override void Enable() {
-        // Skip the 
-        if (!Service.SigScanner.TryScanText("05 75 0C 8B D7 E8 ?? ?? ?? ?? E9", out editAddress)) return;
+        if (!Service.SigScanner.TryScanText("05 75 0C 8B D3 E8 ?? ?? ?? ?? E9", out editAddress)) return;
         SafeMemory.Write(editAddress, (sbyte)-2);
     }
 
     protected override void Disable() {
         if (editAddress == nint.Zero) return;
-        if (SafeMemory.Write(editAddress, (sbyte) 5))
+        if (SafeMemory.Write(editAddress, (sbyte)5))
             editAddress = nint.Zero;
     }
 }
