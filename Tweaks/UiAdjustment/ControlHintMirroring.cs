aeeb2463b3b40c747e411121e6e8575d42871614
Caraxi <git@caraxian.com>
2024-07-07 13:36:35 +0930

    Duplicate Keybinds

--- Tweaks/UiAdjustment/ControlHintMirroring.cs
+++ Tweaks/UiAdjustment/ControlHintMirroring.cs
@@ -7,7 +7,7 @@ using SimpleTweaksPlugin.Events;
 using SimpleTweaksPlugin.TweakSystem;
 using SimpleTweaksPlugin.Utility;
 
-namespace SimpleTweaksPlugin.Tweaks.UiAdjustment; 
+namespace SimpleTweaksPlugin.Tweaks.UiAdjustment;
 
 [TweakName("Duplicate Keybind Hints Between Hotbars")]
 [TweakDescription("Will display the keybind hint for any hotbar slot onto unbound slots with the same action.")]
@@ -16,10 +16,11 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment;
 [Changelog("1.9.1.0", "Rewritten & re-enabled")]
 [Changelog("1.9.1.1", "Fixed flickering while cooldown is active.")]
 public unsafe class ControlHintMirroring : UiAdjustments.SubTweak {
-    private readonly Dictionary<(HotbarSlotType Type, uint Id), string> hints = new();
+    private readonly Dictionary<(RaptureHotbarModule.HotbarSlotType Type, uint Id), string> hints = new();
     private readonly Dictionary<(byte BarID, byte SlotIndex), bool> barSlotHasNoHint = new();
     private readonly Dictionary<(byte BarID, byte SlotIndex), bool> barSlotIsSet = new();
-    private readonly string[] actionBars = { "_ActionBar09", "_ActionBar", "_ActionBar01", "_ActionBar02", "_ActionBar03", "_ActionBar04", "_ActionBar05", "_ActionBar06", "_ActionBar07", "_ActionBar08" };
+    private readonly string[] actionBars = ["_ActionBar09", "_ActionBar", "_ActionBar01", "_ActionBar02", "_ActionBar03", "_ActionBar04", "_ActionBar05", "_ActionBar06", "_ActionBar07", "_ActionBar08"];
+
     // const arrays please?
     [AddonPreRequestedUpdate("_ActionBar09", "_ActionBar", "_ActionBar01", "_ActionBar02", "_ActionBar03", "_ActionBar04", "_ActionBar05", "_ActionBar06", "_ActionBar07", "_ActionBar08")]
     private void ActionBarUpdateRequested(AddonActionBarX* addon) {
@@ -27,27 +28,27 @@ public unsafe class ControlHintMirroring : UiAdjustments.SubTweak {
         if (barId == 9) hints.Clear(); // Relies on the game updating action bars in reverse order
         for (var slotIndex = (byte)(addon->AddonActionBarBase.SlotCount - 1); slotIndex < addon->AddonActionBarBase.SlotCount; slotIndex--) {
             if (barSlotIsSet.TryGetValue((barId, slotIndex), out var isSet) && isSet) {
-                addon->AddonActionBarBase.ActionBarSlotVector.Get(slotIndex).ControlHintTextNode->SetText(string.Empty);
+                addon->AddonActionBarBase.ActionBarSlotVector[slotIndex].ControlHintTextNode->SetText(string.Empty);
                 barSlotIsSet[(barId, slotIndex)] = false;
             }
 
             var slot = RaptureHotbarModule.Instance()->GetSlotById(barId, slotIndex);
-            if (slot->CommandType == HotbarSlotType.Empty) continue;
+            if (slot->CommandType == RaptureHotbarModule.HotbarSlotType.Empty) continue;
 
-            var str = Common.ReadString(slot->KeybindHint, 0x10);
+            var str = slot->KeybindHintString;
             if (string.IsNullOrWhiteSpace(str)) {
                 barSlotHasNoHint[(barId, slotIndex)] = true;
                 continue;
             }
 
-            var commandId = slot->CommandType == HotbarSlotType.Action ? ActionManager.Instance()->GetAdjustedActionId(slot->CommandId) : slot->CommandId;
+            var commandId = slot->CommandType == RaptureHotbarModule.HotbarSlotType.Action ? ActionManager.Instance()->GetAdjustedActionId(slot->CommandId) : slot->CommandId;
             hints[(slot->CommandType, commandId)] = str;
             barSlotHasNoHint[(barId, slotIndex)] = false;
         }
-        
+
         if (barId == 0 && !Unloading) ApplyHints();
     }
-    
+
     private void ApplyHints() {
         using var _ = PerformanceMonitor.Run();
         foreach (var bar in actionBars) {
@@ -55,14 +56,14 @@ public unsafe class ControlHintMirroring : UiAdjustments.SubTweak {
             var barId = addon->AddonActionBarBase.RaptureHotbarId;
             for (byte slotIndex = 0; slotIndex < addon->AddonActionBarBase.SlotCount; slotIndex++) {
                 var slot = RaptureHotbarModule.Instance()->GetSlotById(barId, slotIndex);
-                if (slot->CommandType == HotbarSlotType.Empty) continue;
+                if (slot->CommandType == RaptureHotbarModule.HotbarSlotType.Empty) continue;
                 if (barSlotHasNoHint.TryGetValue((barId, slotIndex), out var noHint) && noHint) {
-                    var commandId = slot->CommandType == HotbarSlotType.Action ? ActionManager.Instance()->GetAdjustedActionId(slot->CommandId) : slot->CommandId;
+                    var commandId = slot->CommandType == RaptureHotbarModule.HotbarSlotType.Action ? ActionManager.Instance()->GetAdjustedActionId(slot->CommandId) : slot->CommandId;
                     if (hints.TryGetValue((slot->CommandType, commandId), out var hint)) {
-                        addon->AddonActionBarBase.ActionBarSlotVector.Get(slotIndex).ControlHintTextNode->SetText(hint);
+                        addon->AddonActionBarBase.ActionBarSlotVector[slotIndex].ControlHintTextNode->SetText(hint);
                         barSlotIsSet[(barId, slotIndex)] = true;
                     } else if (barSlotIsSet.TryGetValue((barId, slotIndex), out var isSet) && isSet) {
-                        addon->AddonActionBarBase.ActionBarSlotVector.Get(slotIndex).ControlHintTextNode->SetText(string.Empty);
+                        addon->AddonActionBarBase.ActionBarSlotVector[slotIndex].ControlHintTextNode->SetText(string.Empty);
                         barSlotIsSet[(barId, slotIndex)] = false;
                     }
                 }
@@ -71,7 +72,7 @@ public unsafe class ControlHintMirroring : UiAdjustments.SubTweak {
     }
 
     private void UpdateAll() {
-        foreach(var addonName in actionBars) {
+        foreach (var addonName in actionBars) {
             if (Common.GetUnitBase<AddonActionBarX>(out var addon, addonName)) ActionBarUpdateRequested(addon);
         }
     }
