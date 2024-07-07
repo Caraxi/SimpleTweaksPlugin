1e476182f3781cc0e23f56a22a7d1be65434fdd1
Caraxi <git@caraxian.com>
2024-07-07 19:30:20 +0930

    Disable Chat Autoscroll

--- Tweaks/Chat/DisableChatAutoscroll.cs
+++ Tweaks/Chat/DisableChatAutoscroll.cs
@@ -1,89 +1,101 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using Dalamud.Utility;
+﻿using System.Runtime.InteropServices;
+using Dalamud.Interface.Utility;
+using Dalamud.Interface.Utility.Raii;
+using Dalamud.Utility.Signatures;
 using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 using ImGuiNET;
-using SimpleTweaksPlugin.Tweaks.Chat;
 using SimpleTweaksPlugin.TweakSystem;
 using SimpleTweaksPlugin.Utility;
 
-namespace SimpleTweaksPlugin {
-    public partial class ChatTweaksConfig {
-        public bool ShouldSerializeDisableChatAutoscroll() => DisableChatAutoscroll != null;
-        public DisableChatAutoscroll.Configs DisableChatAutoscroll = null;
+namespace SimpleTweaksPlugin.Tweaks.Chat;
+
+[TweakName("Smart AutoScroll")]
+[TweakDescription("Attempts to prevent autoscrolling when receiving new chat messages while scrolled up.")]
+[TweakAutoConfig]
+public unsafe class DisableChatAutoscroll : ChatTweaks.SubTweak {
+    public class Configs : TweakConfig {
+        public PanelConfig Panel0 = new();
+        public PanelConfig Panel1 = new();
+        public PanelConfig Panel2 = new();
+        public PanelConfig Panel3 = new();
     }
-}
 
-namespace SimpleTweaksPlugin.Tweaks.Chat {
-    public unsafe class DisableChatAutoscroll : ChatTweaks.SubTweak {
-        public override string Name => "Smart AutoScroll";
-        public override string Description => "Attempts to prevent autoscrolling when receiving new chat messages while scrolled up.";
+    public class PanelConfig {
+        public bool Enabled = true;
+        public int Tolerance;
+    }
 
-        public class Configs : TweakConfig {
-            public bool DisablePanel0;
-            public bool DisablePanel1;
-            public bool DisablePanel2;
-            public bool DisablePanel3;
-        }
+    public Configs Config { get; private set; }
 
-        public Configs Config { get; private set; }
-        
-        protected override DrawConfigDelegate DrawConfigTree => (ref bool hasChanged) => {
-            ImGui.Text(LocString("AllowAutoscrollLabel", "Always allow autoscrolling in:"));
+    private void DrawPanelConfig(string name, ref bool hasChanged, PanelConfig panelConfig) {
+        using var _ = ImRaii.PushId(name);
+
+        hasChanged |= ImGui.Checkbox($"Disable Autoscroll in {name}", ref panelConfig.Enabled);
+
+        if (panelConfig.Enabled) {
+            ImGui.SetNextItemWidth(90 * ImGuiHelpers.GlobalScale);
             ImGui.Indent();
-            hasChanged |= ImGui.Checkbox(LocString("TabLabel", "Tab {0}").Format(1) + "##allowAutoscroll", ref Config.DisablePanel0);
-            hasChanged |= ImGui.Checkbox(LocString("TabLabel", "Tab {0}").Format(2) + "##allowAutoscroll", ref Config.DisablePanel1);
-            hasChanged |= ImGui.Checkbox(LocString("TabLabel", "Tab {0}").Format(3) + "##allowAutoscroll", ref Config.DisablePanel2);
-            hasChanged |= ImGui.Checkbox(LocString("TabLabel", "Tab {0}").Format(4) + "##allowAutoscroll", ref Config.DisablePanel3);
+            hasChanged |= ImGui.InputInt("Minimum messages below scroll", ref panelConfig.Tolerance);
             ImGui.Unindent();
+        }
+
+        ImGui.Spacing();
+    }
+
+    protected void DrawConfig(ref bool hasChanged) {
+        DrawPanelConfig("Tab 1", ref hasChanged, Config.Panel0);
+        DrawPanelConfig("Tab 2", ref hasChanged, Config.Panel1);
+        DrawPanelConfig("Tab 3", ref hasChanged, Config.Panel2);
+        DrawPanelConfig("Tab 4", ref hasChanged, Config.Panel3);
+    }
+
+    private delegate void* ScrollToBottomDelegate(LogViewer* a1);
+
+    [TweakHook, Signature("E8 ?? ?? ?? ?? 48 8B 43 10 33 D2", DetourName = nameof(ScrollToBottomDetour))]
+    private HookWrapper<ScrollToBottomDelegate> scrollToBottomHook;
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x128)]
+    public struct LogViewer {
+        [FieldOffset(0x10)] public AddonChatLogPanel* ChatLogPanel;
+        [FieldOffset(0x18)] public AtkTextNode* ChatText;
+        [FieldOffset(0x48)] public byte FontSize;
+        [FieldOffset(0x4C)] public uint FirstLineVisible;
+        [FieldOffset(0x50)] public uint LastLineVisible;
+        [FieldOffset(0x58)] public uint Unknown2C0;
+        [FieldOffset(0x5C)] public uint TotalLineCount;
+        [FieldOffset(0x90)] public uint MessagesAboveCurrent;
+        [FieldOffset(0xD9)] public byte IsScrolledBottom;
+    }
+
+    private bool PreventScroll(LogViewer* logViewer) {
+        if (logViewer == null || logViewer->ChatLogPanel == null) return false; // Sanity
+        var panelName = logViewer->ChatLogPanel->NameString;
+        var config = panelName switch {
+            "ChatLogPanel_0" => Config.Panel0,
+            "ChatLogPanel_1" => Config.Panel1,
+            "ChatLogPanel_2" => Config.Panel2,
+            "ChatLogPanel_3" => Config.Panel3,
+            _ => null
         };
-        
-        private delegate void* ScrollToBottomDelegate(void* a1);
-
-        private HookWrapper<ScrollToBottomDelegate> scrollToBottomHook;
-
-        protected override void Enable() {
-            Config = LoadConfig<Configs>() ?? PluginConfig.ChatTweaks.DisableChatAutoscroll ?? new Configs();
-            scrollToBottomHook = Common.Hook<ScrollToBottomDelegate>("E8 ?? ?? ?? ?? 48 85 FF 75 0D", ScrollToBottomDetour);
-            scrollToBottomHook?.Enable();
-            base.Enable();
+        if (config == null) {
+            SimpleLog.Verbose($"Unknown panel {panelName} - Allow scroll");
+            return false;
         }
 
-        private void* ScrollToBottomDetour(void* a1) {
-            try {
-                var panel = (AddonChatLogPanel*) ((ulong) a1 - 0x268);
-                var name = Marshal.PtrToStringAnsi(new IntPtr(panel->AtkUnitBase.Name));
-                if (!string.IsNullOrEmpty(name)) {
-                    var isEnabled = name switch {
-                        "ChatLogPanel_0" => !Config.DisablePanel0,
-                        "ChatLogPanel_1" => !Config.DisablePanel1,
-                        "ChatLogPanel_2" => !Config.DisablePanel2,
-                        "ChatLogPanel_3" => !Config.DisablePanel3,
-                        _ => false
-                    };
+        if (!config.Enabled) return false;
+        if (logViewer->TotalLineCount <= logViewer->LastLineVisible || logViewer->TotalLineCount == uint.MaxValue) return false;
+        if (config.Tolerance <= 0) return true;
+        var messagesBelow = logViewer->TotalLineCount - logViewer->LastLineVisible;
+        return messagesBelow >= config.Tolerance;
+    }
 
-                    if (isEnabled && panel->IsScrolledBottom == 0) {
-                        SimpleLog.Verbose($"Prevented Autoscroll in: {name}");
-                        return null;
-                    }
-                }
-                return scrollToBottomHook.Original(a1);
-            } catch {
-                return scrollToBottomHook.Original(a1);
-            }
+    private void* ScrollToBottomDetour(LogViewer* a1) {
+        try {
+            if (PreventScroll(a1)) return null;
+            return scrollToBottomHook.Original(a1);
+        } catch {
+            return scrollToBottomHook.Original(a1);
         }
-
-        protected override void Disable() {
-            SaveConfig(Config);
-            PluginConfig.ChatTweaks.DisableChatAutoscroll = null;
-            scrollToBottomHook?.Disable();
-            base.Disable();
-        }
-
-        public override void Dispose() {
-            scrollToBottomHook?.Dispose();
-            base.Dispose();
-        }
-
     }
 }
