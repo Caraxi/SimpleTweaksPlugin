98e1560d6eaa248eca7b58acc4eb28e8fd3b6a69
Caraxi <git@caraxian.com>
2024-07-07 20:12:54 +0930

    Disable Chat Movement

--- Tweaks/Chat/DisableChatMovement.cs
+++ Tweaks/Chat/DisableChatMovement.cs
@@ -1,82 +1,30 @@
-﻿using System;
+﻿using Dalamud.Utility.Signatures;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using SimpleTweaksPlugin.TweakSystem;
 using SimpleTweaksPlugin.Utility;
 
-namespace SimpleTweaksPlugin.Tweaks.UiAdjustment; 
+namespace SimpleTweaksPlugin.Tweaks.Chat;
 
-public class DisableChatMovement : ChatTweaks.SubTweak {
+[TweakName("Disable Chat Movement")]
+[TweakDescription("Prevents movement of the chat window.")]
+public unsafe class DisableChatMovement : ChatTweaks.SubTweak {
+    private delegate void* SetUiPositionDelegate(AtkUnitManager* atkUnitManager, AtkUnitBase* unitBase, ulong a3);
 
-    public override string Name => "Disable Chat Movement";
-    public override string Description => "Prevents movement of the chat window.";
+    private delegate void ChatPanelDragControlDelegate(AtkAddonControl* addonControl, ulong controlCode, ulong a3, nint a4, short* a5);
 
-    private delegate nint SetUiPositionDelegate(nint _this, nint uiObject, ulong y);
-
-    private unsafe delegate void ChatPanelDragControlDelegate(nint _this, ulong controlCode, ulong a3, nint a4, short* a5);
-
-    private nint setUiPositionAddress = nint.Zero;
-    private nint chatPanelControlAddress = nint.Zero;
-
+    [TweakHook, Signature("40 53 48 83 EC 20 80 A2 ?? ?? ?? ?? ??", DetourName = nameof(SetUiPositionDetour))]
     private HookWrapper<SetUiPositionDelegate> setUiPositionHook;
+
+    [TweakHook, Signature("40 53 56 41 56 48 81 EC ?? ?? ?? ?? 48 8B F1", DetourName = nameof(ChatPanelControlDetour))]
     private HookWrapper<ChatPanelDragControlDelegate> chatPanelDragControlHook;
-        
-    public override void Setup() {
-        if (Ready) return;
 
-        try {
-            if (setUiPositionAddress == nint.Zero) {
-                setUiPositionAddress = Service.SigScanner.ScanText("40 53 48 83 EC 20 80 A2 ?? ?? ?? ?? ??");
-            }
-
-            if (chatPanelControlAddress == nint.Zero) {
-                chatPanelControlAddress = Service.SigScanner.ScanText("40 55 57 48 81 EC ?? ?? ?? ?? 48 8B F9 45 8B C8");
-            }
-                
-            if (setUiPositionAddress == nint.Zero || chatPanelControlAddress == nint.Zero) {
-                SimpleLog.Error($"Failed to setup {GetType().Name}: Failed to find required functions.");
-                return;
-            }
-
-            Ready = true;
-
-        } catch (Exception ex) {
-            SimpleLog.Error($"Failed to setup {this.GetType().Name}: {ex.Message}");
-        }
-    }
-
-    protected override unsafe void Enable() {
-        if (!Ready) return;
-        setUiPositionHook ??= Common.Hook(setUiPositionAddress, new SetUiPositionDelegate(SetUiPositionDetour));
-        chatPanelDragControlHook ??= Common.Hook(chatPanelControlAddress, new ChatPanelDragControlDelegate(ChatPanelControlDetour));
-
-        setUiPositionHook?.Enable();
-        chatPanelDragControlHook?.Enable();
-        Enabled = true;
-    }
-
-    private unsafe void ChatPanelControlDetour(nint a1, ulong controlCode, ulong a3, nint a4, short* a5) {
+    private void ChatPanelControlDetour(AtkAddonControl* addonControl, ulong controlCode, ulong a3, nint a4, short* a5) {
         if (controlCode == 0x17) return; // Suppress Start Drag
-        chatPanelDragControlHook.Original(a1, controlCode, a3, a4, a5);
+        chatPanelDragControlHook.Original(addonControl, controlCode, a3, a4, a5);
     }
 
-    private unsafe nint SetUiPositionDetour(nint _this, nint uiObject, ulong a3) {
-        var k = *(ulong*) (uiObject + 8);
-        if (k == 0x50676F4C74616843 || k == 0x676F4C74616843) {
-            // Suppress Movement of "ChatLog" and "ChatLogPanel_*"
-            return nint.Zero;
-        }
-
-        return setUiPositionHook.Original(_this, uiObject, a3);
-    }
-
-    protected override void Disable() {
-        setUiPositionHook?.Disable();
-        chatPanelDragControlHook?.Disable();
-        Enabled = false;
-    }
-
-    public override void Dispose() {
-        setUiPositionHook?.Dispose();
-        chatPanelDragControlHook?.Dispose();
-        Enabled = false;
-        Ready = false;
+    private void* SetUiPositionDetour(AtkUnitManager* atkUnitManager, AtkUnitBase* unitBase, ulong a3) {
+        if (unitBase->NameString.StartsWith("ChatLog")) return null;
+        return setUiPositionHook.Original(atkUnitManager, unitBase, a3);
     }
 }
