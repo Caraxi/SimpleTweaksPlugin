467d8e501fbc41c02f064fe0889f219292aa0179
AdminPC <AdminPC@ADMIN>
2024-07-09 01:15:08 +0200

    SmartStrafe Update for 7.0

--- Tweaks/SmartStrafe.cs
+++ Tweaks/SmartStrafe.cs
@@ -7,18 +7,12 @@ using SimpleTweaksPlugin.Utility;
 
 namespace SimpleTweaksPlugin.Tweaks
 {
-    internal class SmartStrafe : Tweak {
-        public override string Name => "Smart Strafe";
-        public override string Description => "Intelligently switches keyboard controls between strafing and turning.\n(Legacy type movement only)";
-        protected override string Author => "Iryoku";
-
-        public enum Mode {
-            Turning,
-            Strafing,
-            StrafingNoBackpedal,
-        }
-
-        public class Config : TweakConfig {
+    [TweakName("Smart Strafe")]
+    [TweakDescription("Intelligently switches keyboard controls between strafing and turning.\\n(Legacy type movement only)\"")]
+    public class SmartStrafe : Tweak
+    {
+        public class Config : TweakConfig
+        {
             public Mode InCombat = Mode.StrafingNoBackpedal;
             public Mode OutOfCombat = Mode.Turning;
             public bool ManualBackpedal = true;
@@ -26,45 +20,15 @@ namespace SimpleTweaksPlugin.Tweaks
 
         public Config TweakConfig { get; private set; }
 
-        protected override DrawConfigDelegate DrawConfigTree => (ref bool changed) => {
-            changed |= DrawModeSelector(LocString("InCombat", "In combat"), ref TweakConfig.InCombat);
-            changed |= DrawModeSelector(LocString("OutOfCombat", "Out of combat"), ref TweakConfig.OutOfCombat);
-
-            changed |= ImGui.Checkbox(
-                LocString("ManualBackpedal", "Enable manual quick turning and backpedaling"),
-                ref TweakConfig.ManualBackpedal);
-            if (ImGui.IsItemHovered()) {
-                ImGui.SetTooltip(LocString("ManualBackpedalTooltip", "Activates strafing when both left and right are held"));
-            }
-        };
-
-        private bool DrawModeSelector(string label, ref Mode selectedMode) {
-            var changed = false;
-            ImGui.SetNextItemWidth(200);
-            if (ImGui.BeginCombo(label, GetModeLabel(selectedMode))) {
-                foreach (var mode in Enum.GetValues<Mode>()) {
-                    if (ImGui.Selectable(GetModeLabel(mode), mode == selectedMode)) {
-                        selectedMode = mode;
-                        changed = true;
-                    }
-                }
-                ImGui.EndCombo();
-            }
-            return changed;
+        public enum Mode
+        {
+            Turning,
+            Strafing,
+            StrafingNoBackpedal,
         }
 
-        private string GetModeLabel(Mode mode) {
-            return LocString(mode.ToString(), mode switch {
-                Mode.StrafingNoBackpedal => "Strafing (no backpedaling)",
-                _ => mode.ToString(),
-            });
-        }
-
-        private static class Signatures {
-            internal const string CheckStrafeKeybind = "E8 ?? ?? ?? ?? 84 C0 74 04 41 C6 07 01 BA 44 01 00 00";
-        }
-
-        private enum Keybind : int {
+        private enum Keybind : int
+        {
             MoveForward = 321,
             MoveBack = 322,
             TurnLeft = 323,
@@ -78,16 +42,12 @@ namespace SimpleTweaksPlugin.Tweaks
 
         private HookWrapper<CheckStrafeKeybindDelegate> Hook;
 
-        protected override void Enable() {
+        protected override void Enable()
+        {
             TweakConfig = LoadConfig<Config>() ?? new Config();
 
-            // There are about twenty different copies of this function that all
-            // do almost exactly the same thing, but they are all called in
-            // different places for different purposes. This one is used (among
-            // other things) by the movement code to check if you are pressing the
-            // strafe keybinds.
-            Hook ??= Common.Hook(
-                Service.SigScanner.ScanText(Signatures.CheckStrafeKeybind), 
+            Hook ??= Common.Hook<CheckStrafeKeybindDelegate>(
+                Service.SigScanner.ScanText("E8 ?? ?? ?? ?? 84 C0 74 3C B8 0D 00 00 00 66 89 07 C6 47 0C 03 89 6F 08 4D 85 F6"),
                 new CheckStrafeKeybindDelegate(CheckStrafeKeybind));
             Hook.Enable();
 
@@ -97,18 +57,24 @@ namespace SimpleTweaksPlugin.Tweaks
         protected override void Disable()
         {
             SaveConfig(TweakConfig);
-
             Hook.Disable();
-
             base.Disable();
+        }
+
+        public override void Dispose()
+        {
+            Hook.Dispose();
+            base.Dispose();
         }
 
         private bool CheckStrafeKeybind(IntPtr ptr, Keybind keybind)
         {
-            if (keybind is Keybind.StrafeLeft or Keybind.StrafeRight) {
+            if (keybind is Keybind.StrafeLeft or Keybind.StrafeRight)
+            {
                 if (TweakConfig.ManualBackpedal &&
                     (Hook.Original(ptr, Keybind.TurnLeft) || Hook.Original(ptr, Keybind.StrafeLeft)) &&
-                    (Hook.Original(ptr, Keybind.TurnRight) || Hook.Original(ptr, Keybind.StrafeRight))) {
+                    (Hook.Original(ptr, Keybind.TurnRight) || Hook.Original(ptr, Keybind.StrafeRight)))
+                {
                     return true;
                 }
 
@@ -116,10 +82,12 @@ namespace SimpleTweaksPlugin.Tweaks
                     ? TweakConfig.InCombat
                     : TweakConfig.OutOfCombat;
 
-                switch (mode) {
+                switch (mode)
+                {
                     case Mode.Turning: return false;
                     case Mode.StrafingNoBackpedal:
-                        if (Hook.Original(ptr, Keybind.MoveBack)) {
+                        if (Hook.Original(ptr, Keybind.MoveBack))
+                        {
                             return false;
                         }
                         goto case Mode.Strafing;
@@ -129,6 +97,28 @@ namespace SimpleTweaksPlugin.Tweaks
             }
 
             return Hook.Original(ptr, keybind);
+        }
+
+        protected void DrawConfig(ref bool changed)
+        {
+            var inCombatMode = (int)TweakConfig.InCombat;
+            var outOfCombatMode = (int)TweakConfig.OutOfCombat;
+            var modeNames = Enum.GetNames(typeof(Mode));
+
+            changed |= ImGui.Combo("In Combat", ref inCombatMode, modeNames, modeNames.Length);
+            changed |= ImGui.Combo("Out of Combat", ref outOfCombatMode, modeNames, modeNames.Length);
+
+            if (changed)
+            {
+                TweakConfig.InCombat = (Mode)inCombatMode;
+                TweakConfig.OutOfCombat = (Mode)outOfCombatMode;
+            }
+
+            changed |= ImGui.Checkbox("Enable manual quick turning and backpedaling", ref TweakConfig.ManualBackpedal);
+            if (ImGui.IsItemHovered())
+            {
+                ImGui.SetTooltip("Activates strafing when both left and right are held");
+            }
         }
     }
 }
