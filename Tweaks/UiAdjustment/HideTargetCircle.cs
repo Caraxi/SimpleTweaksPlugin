36b54f00dd15d225c04d6cef27b94b50596ef2c2
Caraxi <git@caraxian.com>
2024-07-10 01:46:28 +0930

    Hide Target Circle

--- Tweaks/UiAdjustment/HideTargetCircle.cs
+++ Tweaks/UiAdjustment/HideTargetCircle.cs
@@ -2,132 +2,69 @@
 using System.Linq;
 using Dalamud.Game.ClientState.Conditions;
 using Dalamud.Game.ClientState.Objects.Enums;
+using SimpleTweaksPlugin.Events;
 using SimpleTweaksPlugin.TweakSystem;
-using SimpleTweaksPlugin.Utility;
 
-#if DEBUG
+namespace SimpleTweaksPlugin.Tweaks.UiAdjustment;
 
-using SimpleTweaksPlugin.Debugging;
+[TweakName("Hide Target Circle")]
+[TweakDescription("Allow hiding the target circle while not in combat or dungeons.")]
+[TweakAutoConfig]
+[TweakAuthor("darkarchon")]
+[TweakReleaseVersion("1.8.7.0")]
+public unsafe class HideTargetCircle : UiAdjustments.SubTweak {
+    private readonly ushort[] nonCombatTerritory = {
+        1055, // Island Sanctuary
+    };
 
-#endif
+    private readonly Stopwatch outOfCombatTimer = new();
+    public Configs Config { get; private set; }
 
-namespace SimpleTweaksPlugin.Tweaks.UiAdjustment
-{
-    public unsafe class HideTargetCircle : UiAdjustments.SubTweak
-    {
-        private readonly ushort[] nonCombatTerritory = {
-            1055, // Island Sanctuary
-        };
+    private bool InCombatDuty => Service.Condition[ConditionFlag.BoundByDuty] && !nonCombatTerritory.Contains(Service.ClientState.TerritoryType);
 
-        private readonly Stopwatch outOfCombatTimer = new Stopwatch();
-        public Configs Config { get; private set; }
-        public override string Description => "Allow hiding the target circle while not in combat or dungeons.";
-        public override string Name => "Hide Target Circle";
-        public override bool UseAutoConfig => true;
-        protected override string Author => "darkarchon";
+    protected override void Disable() => Update(true);
+    protected override void Enable() => outOfCombatTimer.Restart();
 
-        private bool InCombatDuty => Service.Condition[ConditionFlag.BoundByDuty] && !nonCombatTerritory.Contains(Service.ClientState.TerritoryType);
+    [FrameworkUpdate(NthTick = 30)] private void FrameworkUpdate() => Update();
 
-        protected override void Disable()
-        {
-            Common.FrameworkUpdate -= FrameworkUpdate;
-            try
-            {
-                Update(true);
-            }
-            catch
-            {
-                //
-            }
-            SaveConfig(Config);
-            base.Disable();
-        }
+    private void Update(bool reset = false) {
+        var targetCircleShown = Service.GameConfig.UiControl.GetBool("TargetCircleType");
+        bool requestToBeShown = false;
+        bool requestToHide = false;
 
-        protected override void Enable()
-        {
+        if (reset || Config.ShowInDuty && InCombatDuty) {
+            requestToBeShown = true;
+        } else if (Config.ShowInCombat && Service.Condition[ConditionFlag.InCombat]) {
+            requestToBeShown = true;
             outOfCombatTimer.Restart();
-            Config = LoadConfig<Configs>() ?? new Configs();
-            Common.FrameworkUpdate += FrameworkUpdate;
-            base.Enable();
+        } else if (Config.ShowInCombat && outOfCombatTimer.ElapsedMilliseconds < Config.CombatBuffer * 1000) {
+            requestToBeShown = true;
+        } else if (Config.ShowWhileWeaponDrawn && Service.ClientState.LocalPlayer != null && Service.ClientState.LocalPlayer.StatusFlags.HasFlag(StatusFlags.WeaponOut)) {
+            requestToBeShown = true;
+        } else {
+            requestToHide = true;
         }
 
-        private void FrameworkUpdate()
-        {
-            try
-            {
-                Update();
-            }
-            catch
-            {
-                //
-            }
+        if (!targetCircleShown && requestToBeShown) {
+            Service.GameConfig.UiControl.Set("TargetCircleType", 1);
+        } else if (targetCircleShown && requestToHide) {
+            Service.GameConfig.UiControl.Set("TargetCircleType", 0);
         }
+    }
 
-        public override void Setup() {
-            base.Setup();
-            AddChangelogNewTweak("1.8.7.0");
-        }
+    public class Configs : TweakConfig {
+        [TweakConfigOption("Out of Combat Time (Seconds)", 3, EditorSize = 100, IntMin = 0, IntMax = 300, ConditionalDisplay = true)]
+        public int CombatBuffer = 3;
 
-        private void Update(bool reset = false)
-        {
-#if DEBUG
-            PerformanceMonitor.Begin();
-#endif
+        [TweakConfigOption("Show In Combat", 2)]
+        public bool ShowInCombat = true;
 
-            var targetCircleShown = Service.GameConfig.UiControl.GetBool("TargetCircleType");
-            bool requestToBeShown = false;
-            bool requestToHide = false;
+        [TweakConfigOption("Show In Duty", 1)] 
+        public bool ShowInDuty = true;
 
-            if (reset || Config.ShowInDuty && InCombatDuty)
-            {
-                requestToBeShown = true;
-            }
-            else if (Config.ShowInCombat && Service.Condition[ConditionFlag.InCombat])
-            {
-                requestToBeShown = true;
-                outOfCombatTimer.Restart();
-            }
-            else if (Config.ShowInCombat && outOfCombatTimer.ElapsedMilliseconds < Config.CombatBuffer * 1000)
-            {
-                requestToBeShown = true;
-            }
-            else if (Config.ShowWhileWeaponDrawn && Service.ClientState.LocalPlayer != null && Service.ClientState.LocalPlayer.StatusFlags.HasFlag(StatusFlags.WeaponOut))
-            {
-                requestToBeShown = true;
-            }
-            else
-            {
-                requestToHide = true;
-            }
+        [TweakConfigOption("Show While Weapon Is Drawn", 4)]
+        public bool ShowWhileWeaponDrawn;
 
-            if (!targetCircleShown && requestToBeShown)
-            {
-                Service.GameConfig.UiControl.Set("TargetCircleType", 1);
-            }
-            else if (targetCircleShown && requestToHide)
-            {
-                Service.GameConfig.UiControl.Set("TargetCircleType", 0);
-            }
-#if DEBUG
-            PerformanceMonitor.End();
-#endif
-        }
-
-        public class Configs : TweakConfig
-        {
-            [TweakConfigOption("Out of Combat Time (Seconds)", 3, EditorSize = 100, IntMin = 0, IntMax = 300, ConditionalDisplay = true)]
-            public int CombatBuffer = 3;
-
-            [TweakConfigOption("Show In Combat", 2)]
-            public bool ShowInCombat = true;
-
-            [TweakConfigOption("Show In Duty", 1)]
-            public bool ShowInDuty = true;
-
-            [TweakConfigOption("Show While Weapon Is Drawn", 4)]
-            public bool ShowWhileWeaponDrawn;
-
-            public bool ShouldShowCombatBuffer() => ShowInCombat;
-        }
+        public bool ShouldShowCombatBuffer() => ShowInCombat;
     }
 }
