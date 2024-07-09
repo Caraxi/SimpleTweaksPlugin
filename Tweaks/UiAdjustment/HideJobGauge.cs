f878f5d1ab3397d04eec7a9cbd6565c669a4ff43
Caraxi <git@caraxian.com>
2024-07-09 15:05:33 +0930

    Hide Job Gauge

--- Tweaks/UiAdjustment/HideJobGauge.cs
+++ Tweaks/UiAdjustment/HideJobGauge.cs
@@ -1,84 +1,61 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices;
 using Dalamud.Game.ClientState.Conditions;
 using FFXIVClientStructs.FFXIV.Client.Game.Character;
 using FFXIVClientStructs.FFXIV.Component.GUI;
+using SimpleTweaksPlugin.Events;
 using SimpleTweaksPlugin.TweakSystem;
 using SimpleTweaksPlugin.Utility;
 
-#if DEBUG
-using SimpleTweaksPlugin.Debugging;
-#endif
+namespace SimpleTweaksPlugin.Tweaks.UiAdjustment;
 
-namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
-    public unsafe class HideJobGauge : UiAdjustments.SubTweak {
-        public override string Name => "Hide Job Gauge";
-        public override string Description => "Allow hiding the job gauge while not in combat or dungeons.";
+[TweakName("Hide Job Gauge")]
+[TweakDescription("Allow hiding the job gauge while not in combat or dungeons.")]
+[TweakAutoConfig]
+[Changelog("1.8.7.2", "Fixed 'Show while weapon is drawn' option not working.")]
+[Changelog("1.8.8.0", "Fixed 'Show In Duty' option not working in some duties.")]
+public unsafe class HideJobGauge : UiAdjustments.SubTweak {
+    public class Configs : TweakConfig {
+        [TweakConfigOption("Show In Duty", 1)] 
+        public bool ShowInDuty = true;
 
-        public class Configs : TweakConfig {
+        [TweakConfigOption("Show In Combat", 2)]
+        public bool ShowInCombat = true;
 
-            [TweakConfigOption("Show In Duty", 1)]
-            public bool ShowInDuty = true;
+        public bool ShouldShowCombatBuffer() => ShowInCombat;
 
-            [TweakConfigOption("Show In Combat", 2)]
-            public bool ShowInCombat = true;
+        [TweakConfigOption("Out of Combat Time (Seconds)", 3, EditorSize = 100, IntMin = 0, IntMax = 300, ConditionalDisplay = true)]
+        public int CombatBuffer;
 
-            public bool ShouldShowCombatBuffer() => ShowInCombat;
-            [TweakConfigOption("Out of Combat Time (Seconds)", 3, EditorSize = 100, IntMin = 0, IntMax = 300, ConditionalDisplay = true)]
-            public int CombatBuffer;
+        [TweakConfigOption("Show While Weapon Is Drawn", 4)]
+        public bool ShowWhileWeaponDrawn = true;
+    }
 
-            [TweakConfigOption("Show While Weapon Is Drawn", 4)]
-            public bool ShowWhileWeaponDrawn = true;
-        }
+    public Configs Config { get; private set; }
 
-        public Configs Config { get; private set; }
-        public override bool UseAutoConfig => true;
-        private readonly Stopwatch outOfCombatTimer = new Stopwatch();
-        
-        public override void Setup() {
-            base.Setup();
-            AddChangelog("1.8.7.2", "Fixed 'Show while weapon is drawn' option not working.");
-            AddChangelog("1.8.8.0", "Fixed 'Show In Duty' option not working in some duties.");
-        }
+    private readonly Stopwatch outOfCombatTimer = new();
 
-        protected override void Enable() {
-            outOfCombatTimer.Restart();
-            Config = LoadConfig<Configs>() ?? new Configs();
-            Common.FrameworkUpdate += FrameworkUpdate;
-            base.Enable();
-        }
-        
-        private void FrameworkUpdate() {
-            try {
-                Update();
-            } catch {
-                // 
-            }
-            
-        }
+    protected override void Enable() => outOfCombatTimer.Restart();
 
-        private readonly ushort[] nonCombatTerritory = {
-            1055, // Island Sanctuary
-        };
+    [FrameworkUpdate] private void FrameworkUpdate() => Update();
 
-        private bool InCombatDuty => Service.Condition.Duty() && !nonCombatTerritory.Contains(Service.ClientState.TerritoryType);
-        
-        private void Update(bool reset = false) {
+    private readonly ushort[] nonCombatTerritory = [1055];
+
+    private bool InCombatDuty => Service.Condition.Duty() && !nonCombatTerritory.Contains(Service.ClientState.TerritoryType);
+
+    private void Update(bool reset = false) {
+        try {
             if (Common.GetUnitBase("JobHudNotice") != null) reset = true;
-            var stage = AtkStage.GetSingleton();
+            var stage = AtkStage.Instance();
             var loadedUnitsList = &stage->RaptureAtkUnitManager->AtkUnitManager.AllLoadedUnitsList;
-#if DEBUG
-            PerformanceMonitor.Begin();
-            #endif
             var character = (Character*)(Service.ClientState.LocalPlayer?.Address ?? nint.Zero);
 
-            foreach (var j in Enumerable.Range(0, Math.Min(loadedUnitsList->Count, loadedUnitsList->EntriesSpan.Length))) {
-                var addon = loadedUnitsList->EntriesSpan[j].Value;
+            foreach (var j in Enumerable.Range(0, Math.Min(loadedUnitsList->Count, loadedUnitsList->Entries.Length))) {
+                var addon = loadedUnitsList->Entries[j].Value;
                 if (addon == null) continue;
-                var name = Marshal.PtrToStringAnsi(new IntPtr(addon->Name));
-                
+                var name = addon->NameString;
+
                 if (name != null && name.StartsWith("JobHud")) {
                     if (reset || Config.ShowInDuty && InCombatDuty) {
                         if (addon->UldManager.NodeListCount == 0) addon->UldManager.UpdateDrawNodeList();
@@ -93,22 +70,11 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment {
                         addon->UldManager.NodeListCount = 0;
                     }
                 }
-
             }
-            #if DEBUG
-            PerformanceMonitor.End();
-            #endif
-        }
-
-        protected override void Disable() {
-            Common.FrameworkUpdate -= FrameworkUpdate;
-            try {
-                Update(true);
-            } catch {
-                //
-            }
-            SaveConfig(Config);
-            base.Disable();
+        } catch (Exception ex) {
+            SimpleLog.Error(ex);
         }
     }
+
+    protected override void Disable() => Update(true);
 }
