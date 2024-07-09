26247b927d994c12d12b9e37da92807d09736188
a1hena <102157679+a1hena@users.noreply.github.com>
2024-07-09 14:19:54 +0900

    Enable PidCommand

--- Tweaks/PidCommand.cs
+++ Tweaks/PidCommand.cs
@@ -1,28 +1,47 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
 using Dalamud.Game.Text;
 using Dalamud.Game.Text.SeStringHandling;
 using Dalamud.Game.Text.SeStringHandling.Payloads;
-using SimpleTweaksPlugin.Tweaks.AbstractTweaks;
+using SimpleTweaksPlugin.TweakSystem;
+using SimpleTweaksPlugin.Utility;
 
 namespace SimpleTweaksPlugin.Tweaks; 
 
-public class PidCommand : CommandTweak {
-    public override string Name => "Show process id Command";
-    public override string Description => $"Adds the command '/{Command}' to show current process id.";
-    protected override string Command => "pid";
-    protected override string HelpMessage => "Print your process id to chat.";
+[TweakCategory(TweakCategory.Command)]
+[TweakName("Show process id Command")]
+[TweakDescription("Adds the command '/pid' to show current process id.")]
+public class PidCommand : Tweak {
 
-    protected override void OnCommand(string args) {
-        var process = Process.GetCurrentProcess();
-            
+    private string command = "/pid";
+    
+    private unsafe void OnChatMessage(XivChatType type, int timestamp, ref SeString sender, ref SeString message, ref bool isHandled) {
+        if (type != XivChatType.ErrorMessage) return;
+        if (Common.LastCommand == null || Common.LastCommand->StringPtr == null) return;
+        var lastCommandStr = Encoding.UTF8.GetString(Common.LastCommand->StringPtr, (int)Common.LastCommand->BufUsed);
+        if (!lastCommandStr.StartsWith(command)){
+            return;
+        }
+
+        isHandled = true;
         Service.Chat.Print(new XivChatEntry() {
             Message= new SeString(new List<Payload>() {
                 new TextPayload("current pid: "),
                 new UIForegroundPayload(22),
-                new TextPayload(process.Id.ToString()),
+                new TextPayload(Process.GetCurrentProcess().Id.ToString()),
                 new UIForegroundPayload(0)
             })
         });
     }
+
+    protected override void Enable()
+    {
+        Service.Chat.ChatMessage += OnChatMessage;
+    }
+    protected override void Disable()
+    {
+        Service.Chat.ChatMessage -= OnChatMessage;
+    }
+    
 }
