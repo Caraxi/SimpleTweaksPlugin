3a1faf3ad61fcaabb4d100995a8c292b67d81dff
Caraxi <git@caraxian.com>
2024-07-11 00:15:08 +0930

    Open Commendation Window

--- Tweaks/AutoOpenCommendWindow.cs
+++ Tweaks/AutoOpenCommendWindow.cs
@@ -1,67 +1,55 @@
 ﻿using System;
 using Dalamud.Game.ClientState.Conditions;
 using FFXIVClientStructs.FFXIV.Component.GUI;
+using SimpleTweaksPlugin.Events;
 using SimpleTweaksPlugin.TweakSystem;
 using SimpleTweaksPlugin.Utility;
 
-namespace SimpleTweaksPlugin.Tweaks; 
+namespace SimpleTweaksPlugin.Tweaks;
 
+[TweakName("Open Commendation Window Automatically")]
+[TweakDescription("Open the commendation window upon completion of a duty.")]
 public unsafe class AutoOpenCommendWindow : Tweak {
-    public override string Name => "Open commendation window automatically";
-
-    public override string Description => "Open the commendation window upon completion of a duty.";
-
-    protected override void Enable() {
-        Common.FrameworkUpdate += FrameworkOnUpdate;
-        base.Enable();
-    }
-
     private bool hasOpenedMvp;
     private byte throttle;
+
+    [FrameworkUpdate(NthTick = 10)]
     private void FrameworkOnUpdate() {
         throttle++;
 
-        if (Service.Condition[ConditionFlag.WatchingCutscene] ||
-            Service.Condition[ConditionFlag.WatchingCutscene78] ||
-            Service.Condition[ConditionFlag.OccupiedInCutSceneEvent]) {
+        if (Service.Condition[ConditionFlag.WatchingCutscene] || Service.Condition[ConditionFlag.WatchingCutscene78] || Service.Condition[ConditionFlag.OccupiedInCutSceneEvent]) {
             hasOpenedMvp = false;
             throttle = 0;
         }
 
-        if (throttle > 10) {
-            throttle = 0;
-            if (Common.GetUnitBase("_NotificationIcMvp") == null) {
-                if (hasOpenedMvp) {
-                    hasOpenedMvp = false;
-                }
-            } else {
-                if (hasOpenedMvp) return;
-                hasOpenedMvp = true;
-                TryOpenWindow();
-            }
+        if (throttle <= 2) return;
+
+        throttle = 0;
+
+        if (Common.GetUnitBase("_NotificationIcMvp") == null) {
+            hasOpenedMvp = false;
+        } else {
+            if (hasOpenedMvp) return;
+            hasOpenedMvp = true;
+            TryOpenWindow();
         }
     }
 
     private static void TryOpenWindow() {
         SimpleLog.Verbose("Try opening VoteMvp");
-        var needGreedWindow = Service.GameGui.GetAddonByName("VoteMvp", 1);
+        var needGreedWindow = Service.GameGui.GetAddonByName("VoteMvp");
         if (needGreedWindow != IntPtr.Zero) {
             SimpleLog.Verbose("VoteMvp already open.");
             return;
         }
 
         SimpleLog.Verbose("Opening VoteMvp window.");
-        var notification = (AtkUnitBase*)Service.GameGui.GetAddonByName("_Notification", 1);
-        if (notification== null) {
+        var notification = Common.GetUnitBase("_Notification");
+        if (notification == null) {
             SimpleLog.Verbose("_Notification not open.");
             return;
         }
 
         Common.GenerateCallback(notification, 0, 11);
-    }
-
-    protected override void Disable() {
-        Common.FrameworkUpdate -= FrameworkOnUpdate;
-        base.Disable();
     }
 }
