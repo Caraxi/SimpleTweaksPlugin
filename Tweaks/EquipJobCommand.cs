8414f24319471fb8df09798aeb5244e75e930d4c
Cerulan Lumina <CerulanLumina@users.noreply.github.com>
2024-07-11 23:18:58 -0400

    Update EquipJobCommand for DT (#767)

--- Tweaks/EquipJobCommand.cs
+++ Tweaks/EquipJobCommand.cs
@@ -14,45 +14,56 @@ namespace SimpleTweaksPlugin.Tweaks;
 [TweakDescription("Adds a command to switch to a class or job's gearset.")]
 [TweakAuthor("Lumina Sapphira")]
 [TweakReleaseVersion("1.9.6.0")]
-public unsafe class EquipJobCommand : CommandTweak {
+public unsafe class EquipJobCommand : CommandTweak
+{
     protected override string Command => "equipjob";
     protected override string HelpMessage => "Switches to the highest item-level gearset for a job.";
 
-    private class Config : TweakConfig {
+    private class Config : TweakConfig
+    {
         public bool AllowPriority;
     }
 
     private Config TweakConfig { get; set; } = null!;
 
-    protected override DrawConfigDelegate DrawConfigTree =>
-        (ref bool _) => {
-            if (ImGui.Checkbox(LocString("PriorityName", "Allow priority list of jobs? (Only allows using abbreviations)", "Allow Priority Config Option"), ref TweakConfig.AllowPriority)) {
-                SaveConfig(TweakConfig);
-            }
+    private void DrawConfig(ref bool _)
+    {
+        if (ImGui.Checkbox(
+                LocString("PriorityName", "Allow priority list of jobs? (Only allows using abbreviations)",
+                    "Allow Priority Config Option"), ref TweakConfig.AllowPriority))
+        {
+            SaveConfig(TweakConfig);
+        }
 
-            if (ImGui.IsItemHovered()) {
-                ImGui.SetNextWindowSize(new Vector2(280, -1));
-                ImGui.BeginTooltip();
-                ImGui.TextWrapped(LocString("PriorityHelp", "Useful when generalizing between classes / jobs (e.g. /equipjob pld gla)", "Allow Priority Tooltip"));
-                ImGui.EndTooltip();
-            }
+        if (ImGui.IsItemHovered())
+        {
+            ImGui.SetNextWindowSize(new Vector2(280, -1));
+            ImGui.BeginTooltip();
+            ImGui.TextWrapped(LocString("PriorityHelp",
+                "Useful when generalizing between classes / jobs (e.g. /equipjob pld gla)", "Allow Priority Tooltip"));
+            ImGui.EndTooltip();
+        }
 
-            ImGui.Separator();
-            ImGui.Text($"/{Command}");
-        };
+        ImGui.Separator();
+        ImGui.Text($"/{Command}");
+    }
 
-    protected override void Enable() {
+    protected override void Enable()
+    {
         TweakConfig = LoadConfig<Config>() ?? new Config();
         base.Enable();
     }
 
-    protected override void Disable() {
+    protected override void Disable()
+    {
         SaveConfig(TweakConfig);
         base.Disable();
     }
 
-    protected override void OnCommand(string arguments) {
-        if (string.IsNullOrWhiteSpace(arguments)) {
+    protected override void OnCommand(string arguments)
+    {
+        if (string.IsNullOrWhiteSpace(arguments))
+        {
             if (TweakConfig.AllowPriority)
                 Service.Chat.PrintError($"/{Command} (priority list of job abbreviations...)");
             else
@@ -60,10 +71,13 @@ public unsafe class EquipJobCommand : CommandTweak {
             return;
         }
 
-        if (TweakConfig.AllowPriority) {
+        if (TweakConfig.AllowPriority)
+        {
             var options = arguments.Split(" ");
-            foreach (var option in options) {
-                switch (TrySwitchClassJob(option, true)) {
+            foreach (var option in options)
+            {
+                switch (TrySwitchClassJob(option, true))
+                {
                     case SwitchClassJobResult.Success:
                         return;
                     case SwitchClassJobResult.FailedToFindGearset:
@@ -78,8 +92,11 @@ public unsafe class EquipJobCommand : CommandTweak {
                         throw new ArgumentOutOfRangeException();
                 }
             }
-        } else {
-            switch (TrySwitchClassJob(arguments, false)) {
+        }
+        else
+        {
+            switch (TrySwitchClassJob(arguments, false))
+            {
                 case SwitchClassJobResult.Success:
                     break;
                 case SwitchClassJobResult.FailedToFindGearset:
@@ -97,15 +114,18 @@ public unsafe class EquipJobCommand : CommandTweak {
         }
     }
 
-    private enum SwitchClassJobResult {
+    private enum SwitchClassJobResult
+    {
         Success,
         FailedToFindGearset,
         FailedToFindClassJob,
         InternalError
     }
 
-    private static SwitchClassJobResult TrySwitchClassJob(string arg, bool abbreviationsOnly) {
-        bool ComparisonDelegate(ClassJob row) {
+    private static SwitchClassJobResult TrySwitchClassJob(string arg, bool abbreviationsOnly)
+    {
+        bool ComparisonDelegate(ClassJob row)
+        {
             var abbr = row.Abbreviation.ToDalamudString().ToString().ToLower().Equals(arg.ToLower());
             if (abbreviationsOnly || abbr) return abbr;
             return row.Name.ToDalamudString().ToString().ToLower().Equals(arg.ToLower());
@@ -114,17 +134,19 @@ public unsafe class EquipJobCommand : CommandTweak {
         var sheet = Service.Data.GetExcelSheet<ClassJob>();
         var classJob = from row in sheet where ComparisonDelegate(row) select row.RowId;
         var id = classJob.FirstOrDefault((uint)0);
-        return id == 0 ? SwitchClassJobResult.FailedToFindClassJob : SwitchClassJobID(id);
+        return id == 0 ? SwitchClassJobResult.FailedToFindClassJob : SwitchClassJobId(id);
     }
 
-    private static SwitchClassJobResult SwitchClassJobID(uint classJobId) {
+    private static SwitchClassJobResult SwitchClassJobId(uint classJobId)
+    {
         var raptureGearsetModule = RaptureGearsetModule.Instance();
         if (raptureGearsetModule == null)
             return SwitchClassJobResult.InternalError;
 
-        var bestGearset = Enumerable.Range(0, 100).Where(raptureGearsetModule->IsValidGearset).Select(gearsetId => {
+        var bestGearset = Enumerable.Range(0, 100).Where(raptureGearsetModule->IsValidGearset).Select(gearsetId =>
+        {
             var gearset = raptureGearsetModule->GetGearset(gearsetId);
-            return (Id: gearset->ID, ClassJob: gearset->ClassJob, ILvl: gearset->ItemLevel);
+            return (Id: gearset->Id, ClassJob: gearset->ClassJob, ILvl: gearset->ItemLevel);
         }).Where(x => x.ClassJob == classJobId).OrderByDescending(x => x.ILvl).FirstOrDefault();
         if (bestGearset.ClassJob == 0) return SwitchClassJobResult.FailedToFindGearset;
         raptureGearsetModule->EquipGearset(bestGearset.Id);
