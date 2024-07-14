66bbdea993956bcf73b898afeaf07e0153b98d64
Caraxi <git@caraxian.com>
2024-07-14 21:19:48 +0930

    Auto Open Loot Window

--- Tweaks/AutoOpenLootWindow.cs
+++ Tweaks/AutoOpenLootWindow.cs
@@ -7,36 +7,28 @@ using FFXIVClientStructs.FFXIV.Component.GUI;
 using SimpleTweaksPlugin.TweakSystem;
 using SimpleTweaksPlugin.Utility;
 
-namespace SimpleTweaksPlugin.Tweaks; 
+namespace SimpleTweaksPlugin.Tweaks;
 
 [TweakName("Open loot window when items are added")]
 [TweakDescription("Open the loot rolling window when new items are added to be rolled on.")]
 public unsafe class AutoOpenLootWindow : Tweak {
     protected override void Enable() {
         Service.Chat.CheckMessageHandled += HandleChat;
-        base.Enable();
     }
 
     private void HandleChat(XivChatType type, int timestamp, ref SeString sender, ref SeString message, ref bool isHandled) {
         try {
             if ((ushort)type != 2105) return;
-            if (message.TextValue == Service.ClientState.ClientLanguage switch
-                {
+            if (message.TextValue == Service.ClientState.ClientLanguage switch {
                     ClientLanguage.German => "Bitte um das Beutegut würfeln.",
                     ClientLanguage.French => "Veuillez lancer les dés pour le butin.",
                     ClientLanguage.Japanese => "ロットを行ってください。",
                     _ => "Cast your lot."
-                })
-            {
-                if (Service.Condition[ConditionFlag.WatchingCutscene]
-                    || Service.Condition[ConditionFlag.WatchingCutscene78]
-                    || Service.Condition[ConditionFlag.OccupiedInCutSceneEvent])
-                {
+                }) {
+                if (Service.Condition.Cutscene()) {
                     Common.FrameworkUpdate -= TryOpenAfterCutsceneFrameworkUpdate;
                     Common.FrameworkUpdate += TryOpenAfterCutsceneFrameworkUpdate;
-                }
-                else
-                {
+                } else {
                     TryOpenWindow();
                 }
             }
@@ -46,17 +38,15 @@ public unsafe class AutoOpenLootWindow : Tweak {
     }
 
     private byte throttle;
-    private void TryOpenAfterCutsceneFrameworkUpdate()
-    {
+
+    private void TryOpenAfterCutsceneFrameworkUpdate() {
         throttle++;
         if (throttle <= 10) return;
         throttle = 0;
-        if (Service.Condition[ConditionFlag.WatchingCutscene]
-            || Service.Condition[ConditionFlag.WatchingCutscene78]
-            || Service.Condition[ConditionFlag.OccupiedInCutSceneEvent])
-        {
+        if (Service.Condition[ConditionFlag.WatchingCutscene] || Service.Condition[ConditionFlag.WatchingCutscene78] || Service.Condition[ConditionFlag.OccupiedInCutSceneEvent]) {
             return;
         }
+
         Common.FrameworkUpdate -= TryOpenAfterCutsceneFrameworkUpdate;
         TryOpenWindow();
     }
@@ -71,7 +61,7 @@ public unsafe class AutoOpenLootWindow : Tweak {
 
         SimpleLog.Verbose("Opening NeedGreed window.");
         var notification = (AtkUnitBase*)Service.GameGui.GetAddonByName("_Notification", 1);
-        if (notification== null) {
+        if (notification == null) {
             SimpleLog.Verbose("_Notification not open.");
             return;
         }
@@ -82,6 +72,5 @@ public unsafe class AutoOpenLootWindow : Tweak {
     protected override void Disable() {
         Service.Chat.CheckMessageHandled -= HandleChat;
         Common.FrameworkUpdate -= TryOpenAfterCutsceneFrameworkUpdate;
-        base.Disable();
     }
 }
