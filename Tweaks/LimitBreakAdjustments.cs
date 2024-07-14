c3aa65dae685045e4cf9392b8d584f57ca5d0dd1
Caraxi <git@caraxian.com>
2024-07-15 00:25:16 +0930

    Limit Break Adjustments

--- Tweaks/LimitBreakAdjustments.cs
+++ Tweaks/LimitBreakAdjustments.cs
@@ -2,6 +2,7 @@ using System;
 using System.Numerics;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using ImGuiNET;
+using SimpleTweaksPlugin.Events;
 using SimpleTweaksPlugin.TweakSystem;
 using SimpleTweaksPlugin.Utility;
 
@@ -10,6 +11,7 @@ namespace SimpleTweaksPlugin.Tweaks;
 [TweakName("Limit Break Adjustments")]
 [TweakDescription("Simple customization of the limit break bars.")]
 [TweakReleaseVersion("1.8.2.0")]
+[TweakAutoConfig]
 public unsafe class LimitBreakAdjustments : UiAdjustments.SubTweak {
     public class Configs : TweakConfig {
         public static readonly Configs Default = new();
@@ -21,26 +23,20 @@ public unsafe class LimitBreakAdjustments : UiAdjustments.SubTweak {
     }
 
     public Configs Config { get; private set; }
-    
-    protected void DrawConfig(ref bool _) {
+
+    protected void DrawConfig() {
         ImGui.DragFloat("Bar Rotation", ref Config.BarRotation, 0.5f, -360, 720, "%.1f");
         while (Config.BarRotation >= 360) Config.BarRotation -= 360;
         while (Config.BarRotation < 0) Config.BarRotation += 360;
-        
+
         ImGui.DragFloat2("Bar Seperation", ref Config.BarSpacing);
-        
+
         ImGui.Checkbox("Hide Text", ref Config.HideText);
         ImGui.DragFloat2("Text Position", ref Config.TextPosition);
     }
 
-    protected override void Enable() {
-        Config = LoadConfig<Configs>() ?? new Configs();
-        Common.FrameworkUpdate += OnFrameworkUpdate;
-    }
-
-    private void OnFrameworkUpdate() {
-        Update(Config);
-    }
+    [FrameworkUpdate]
+    private void OnFrameworkUpdate() => Update(Config);
 
     private void Update(Configs config) {
         var lbAddon = Common.GetUnitBase("_LimitBreak");
@@ -51,7 +47,7 @@ public unsafe class LimitBreakAdjustments : UiAdjustments.SubTweak {
 
         var textNode = lbAddon->GetTextNodeById(2);
         if (textNode == null) return;
-        
+
         textNode->AtkResNode.ToggleVisibility(!config.HideText);
         textNode->AtkResNode.SetPositionFloat(config.TextPosition.X, config.TextPosition.Y);
 
@@ -60,24 +56,18 @@ public unsafe class LimitBreakAdjustments : UiAdjustments.SubTweak {
             (AtkComponentNode*) lbAddon->GetNodeById(5),
             (AtkComponentNode*) lbAddon->GetNodeById(4),
         };
-        
+
         var p = new Vector2();
-        
+
         foreach (var b in bars) {
             if (b == null) continue;
-
             b->AtkResNode.Rotation = config.BarRotation * MathF.PI / 180f;
             b->AtkResNode.SetPositionFloat(p.X, p.Y);
             b->AtkResNode.SetScale(1, 1);
             b->AtkResNode.DrawFlags |= 1;
-            
             p += config.BarSpacing;
         }
     }
 
-    protected override void Disable() {
-        Common.FrameworkUpdate -= OnFrameworkUpdate;
-        Update(Configs.Default);
-        SaveConfig(Config);
-    }
+    protected override void Disable() => Update(Configs.Default);
 }
