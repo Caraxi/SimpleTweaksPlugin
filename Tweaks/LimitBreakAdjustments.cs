4dda27330b9640c0be0f73d8ed6920fbcef695ae
Enriath <ikada@protonmail.ch>
2024-07-14 15:25:56 +0100

    update LimitBreakAdjustments

--- Tweaks/LimitBreakAdjustments.cs
+++ Tweaks/LimitBreakAdjustments.cs
@@ -7,10 +7,10 @@ using SimpleTweaksPlugin.Utility;
 
 namespace SimpleTweaksPlugin.Tweaks;
 
+[TweakName("Limit Break Adjustments")]
+[TweakDescription("Simple customization of the limit break bars.")]
+[TweakReleaseVersion("1.8.2.0")]
 public unsafe class LimitBreakAdjustments : UiAdjustments.SubTweak {
-    public override string Name => "Limit Break Adjustments";
-    public override string Description => "Simple customization of the limit break bars";
-
     public class Configs : TweakConfig {
         public static readonly Configs Default = new();
 
@@ -22,7 +22,7 @@ public unsafe class LimitBreakAdjustments : UiAdjustments.SubTweak {
 
     public Configs Config { get; private set; }
     
-    protected override DrawConfigDelegate DrawConfigTree => (ref bool hasChanged) => {
+    protected void DrawConfig(ref bool _) {
         ImGui.DragFloat("Bar Rotation", ref Config.BarRotation, 0.5f, -360, 720, "%.1f");
         while (Config.BarRotation >= 360) Config.BarRotation -= 360;
         while (Config.BarRotation < 0) Config.BarRotation += 360;
@@ -31,17 +31,11 @@ public unsafe class LimitBreakAdjustments : UiAdjustments.SubTweak {
         
         ImGui.Checkbox("Hide Text", ref Config.HideText);
         ImGui.DragFloat2("Text Position", ref Config.TextPosition);
-    };
-
-    public override void Setup() {
-        AddChangelogNewTweak("1.8.2.0");
-        base.Setup();
     }
 
     protected override void Enable() {
         Config = LoadConfig<Configs>() ?? new Configs();
         Common.FrameworkUpdate += OnFrameworkUpdate;
-        base.Enable();
     }
 
     private void OnFrameworkUpdate() {
@@ -85,6 +79,5 @@ public unsafe class LimitBreakAdjustments : UiAdjustments.SubTweak {
         Common.FrameworkUpdate -= OnFrameworkUpdate;
         Update(Configs.Default);
         SaveConfig(Config);
-        base.Disable();
     }
 }
