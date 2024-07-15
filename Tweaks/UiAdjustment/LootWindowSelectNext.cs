7ed9bda822c8ceb4a000951fcf61440a2d6cf965
MidoriKami <mi_kami@outlook.com>
2024-07-14 17:22:51 -0700

    Update

--- Tweaks/UiAdjustment/LootWindowSelectNext.cs
+++ Tweaks/UiAdjustment/LootWindowSelectNext.cs
@@ -1,11 +1,11 @@
 #nullable enable
-using System;
 using System.Linq;
+using System.Runtime.InteropServices;
+using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using SimpleTweaksPlugin.Events;
 using SimpleTweaksPlugin.TweakSystem;
-using SimpleTweaksPlugin.Utility;
 
 namespace SimpleTweaksPlugin.Tweaks.UiAdjustment;
 
@@ -14,63 +14,53 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment;
 [TweakAuthor("MidoriKami")]
 [TweakReleaseVersion("1.8.9.2")]
 public unsafe class LootWindowSelectNext : UiAdjustments.SubTweak {
-    private delegate void NeedGreedReceiveEventDelegate(AddonNeedGreed* addon, AtkEventType type, ButtonType buttonType, AtkEvent* eventInfo, nint data);
-
-    [TweakHook(AutoEnable = false)]
-    private HookWrapper<NeedGreedReceiveEventDelegate>? needGreedReceiveEventHook;
-
     [AddonPostSetup("NeedGreed")]
     private void AddonSetup(AtkUnitBase* atkUnitBase) {
-        needGreedReceiveEventHook ??= Common.Hook<NeedGreedReceiveEventDelegate>((nint) atkUnitBase->AtkEventListener.vfunc[2], OnNeedGreedReceiveEvent);
-        needGreedReceiveEventHook?.Enable();
-
         // Find first item that hasn't been rolled on, and select it.
         var addonNeedGreed = (AddonNeedGreed*) atkUnitBase;
         foreach (var index in Enumerable.Range(0, addonNeedGreed->NumItems)) {
-            if (addonNeedGreed->ItemsSpan[index] is { Roll: 0, ItemId: not 0 }) {
+            if (addonNeedGreed->Items[index] is { Roll: 0, ItemId: not 0 }) {
                 SelectItem(addonNeedGreed, index);
                 break;
             }
         }
     }
 
+    [AddonPostReceiveEvent("NeedGreed")]
+    private void OnNeedGreedReceiveEvent(AddonReceiveEventArgs args) {
+        var type = (AtkEventType) args.AtkEventType;
+        var buttonType = (ButtonType) args.EventParam;
+        var addon = (AddonNeedGreed*) args.Addon;
+        
+        if (type is not AtkEventType.ButtonClick) return;
+        
+        switch (buttonType) {
+            // Fall through unconditionally
+            case ButtonType.Need:
+            case ButtonType.Greed:
+            
+            // Don't select next item if we are passing on an item that we already rolled on
+            case ButtonType.Pass when addon->Items[addon->SelectedItemIndex] is { Roll: 0, ItemId: not 0 }: 
+                var currentItemCount = addon->NumItems;
+                var nextIndex = addon->SelectedItemIndex + 1;
+
+                if (nextIndex < currentItemCount) {
+                    SelectItem(addon, nextIndex);
+                }
+                break;
+        }
+    }
+
+    private static void SelectItem(AddonNeedGreed* addon, int index) {
+        var eventData = new AtkEventData();
+        eventData.ListItemData.SelectedIndex = index;
+        addon->ReceiveEvent(AtkEventType.ListItemToggle, 0, null, &eventData);
+    }
+    
     // There are other button types such as "Greed Only" and "Loot Recipient"
     private enum ButtonType : uint {
         Need = 0,
         Greed = 1,
         Pass = 2,
     }
-
-    private void OnNeedGreedReceiveEvent(AddonNeedGreed* addon, AtkEventType type, ButtonType buttonType, AtkEvent* eventInfo, nint data) {
-        needGreedReceiveEventHook!.Original(addon, type, buttonType, eventInfo, data);
-        
-        try {
-            if (type is not AtkEventType.ButtonClick) return;
-
-            switch (buttonType) {
-                case ButtonType.Need:
-                case ButtonType.Greed:
-                case ButtonType.Pass when IsSelectedItemUnrolled(addon): // Don't select next item if we are passing on an item that we already rolled on
-                    var currentItemCount = addon->NumItems;
-                    var nextIndex = addon->SelectedItemIndex + 1;
-                    
-                    if (nextIndex < currentItemCount) SelectItem(addon, nextIndex);
-                    break;
-            }
-        } catch (Exception exception) {
-            SimpleLog.Error(exception, "Something went wrong in 'Loot Window Select Next Item', let MidoriKami know.");
-        }
-    }
-
-    private void SelectItem(AddonNeedGreed* addon, int index) {
-        var values = stackalloc int[6];
-        values[4] = index;
-        OnNeedGreedReceiveEvent(addon, AtkEventType.ListItemToggle, 0, null, (nint)values); 
-    }
-
-    private LootItemInfo GetSelectedItem(AddonNeedGreed* addon) 
-        => addon->ItemsSpan[addon->SelectedItemIndex];
-
-    private bool IsSelectedItemUnrolled(AddonNeedGreed* addon)
-        => GetSelectedItem(addon) is { Roll: 0, ItemId: not 0 };
 }
