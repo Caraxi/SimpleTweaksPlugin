2fde9baca74e955bcc11ddb6968c09316322aaa1
Logan Mitchell <logan.s.mitchell@gmail.com>
2024-08-10 15:46:34 -0700

    Mostly fix battle talk adjustments

--- Tweaks/UiAdjustment/BattleTalkAdjustments.cs
+++ Tweaks/UiAdjustment/BattleTalkAdjustments.cs
@@ -1,21 +1,26 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Numerics;
 using Dalamud.Interface.Utility;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using ImGuiNET;
+using SimpleTweaksPlugin.Events;
 using SimpleTweaksPlugin.TweakSystem;
 using SimpleTweaksPlugin.Utility;
 
 namespace SimpleTweaksPlugin.Tweaks.UiAdjustment; 
 
+[TweakName("Battle Talk Adjustments")]
+[TweakDescription("Allows moving of the dialogue box that appears in the middle of battles.")]
+[TweakAuthor("Chivalrik")]
+[TweakAutoConfig]
 public unsafe class BattleTalkAdjustments : UiAdjustments.SubTweak {
-    public override string Name => "Battle Talk Adjustments";
-    public override string Description => "Allows moving of the dialogue box that appears in the middle of battles.";
-    protected override string Author => "Chivalrik";
+    
+    public override IEnumerable<string> Tags => new[] { "BattleTalk" };
 
     public class Configuration : TweakConfig {
-        public int OffsetX = 0;
-        public int OffsetY = 0;
+        public static readonly Configuration Default = new();
+        
+        public Vector2 TextPosition = new(0, 0);
         public float Scale = 1;
     }
 
@@ -23,15 +28,15 @@ public unsafe class BattleTalkAdjustments : UiAdjustments.SubTweak {
 
     private float originalPositionX = 0f;
     private float originalPositionY = 0f;
-        
-    protected override DrawConfigDelegate DrawConfigTree => (ref bool changed) => {
-        ImGui.SetNextItemWidth(100 * ImGui.GetIO().FontGlobalScale);
-        changed |= ImGui.InputInt(LocString("X Offset") + "##battletalkadjustments_offsetPosition", ref Config.OffsetX, 1);
-        ImGui.SetNextItemWidth(100 * ImGui.GetIO().FontGlobalScale);
-        changed |= ImGui.InputInt(LocString("Y Offset") + "##battletalkadjustments_offsetPosition", ref Config.OffsetY, 1);
-        ImGui.SetNextItemWidth(200 * ImGui.GetIO().FontGlobalScale);
-        changed |= ImGui.SliderFloat("##battletalkadjustments_Scale", ref Config.Scale, 0.01f, 3f, LocString("Scale") + ": %.2fx");
-            
+
+    protected void DrawConfig() {
+        ImGui.DragFloat2("Text Position", ref Config.TextPosition);
+    }
+    
+    [FrameworkUpdate]
+    private void OnFrameworkUpdate() => Update(Config);
+
+    private void Update(Configuration config) {
         const ImGuiWindowFlags flags = ImGuiWindowFlags.NoCollapse
                                        | ImGuiWindowFlags.NoDecoration
                                        | ImGuiWindowFlags.NoNav
@@ -53,71 +58,12 @@ public unsafe class BattleTalkAdjustments : UiAdjustments.SubTweak {
         ImGui.Dummy(new Vector2(25,0) * ImGui.GetIO().FontGlobalScale);
         ImGui.Text(LocString("PositionPreviewText", "NEW BATTLE TALK DIALOGUE BOX POSITION"));
         ImGui.End();
-        if (!changed) return;
-        Common.FrameworkUpdate -= FrameworkOnUpdate;
-        Common.FrameworkUpdate += FrameworkOnUpdate;
-    };
-
-    protected override void Enable() {
-        Config = LoadConfig<Configuration>() ?? new Configuration();
-        Service.ClientState.Login += OnLogin;
-        Service.ClientState.Logout += OnLogout;
-        if(Service.ClientState.LocalPlayer is not null) OnLogin();
-        base.Enable();
+        
+        UiHelper.SetPosition(battleTalkResNode, originalPositionX + config.TextPosition.X, originalPositionY + config.TextPosition.Y);
+        battleTalkResNode->SetScale(config.Scale, config.Scale);
     }
 
-    protected override void Disable() {
-        Service.ClientState.Login -= OnLogin;
-        Service.ClientState.Logout -= OnLogout;
-        OnLogout();
-        SaveConfig(Config);
-        base.Disable();
-    }
-
-    private void OnLogin()
-    {
-        Common.FrameworkUpdate += FrameworkOnUpdateSetup;
-    }
-        
-    private void OnLogout()
-    {
-        Common.FrameworkUpdate -= FrameworkOnUpdate;
-        Common.FrameworkUpdate -= FrameworkOnUpdateSetup;
-        ResetBattleTalk();
-    }
-        
-    private void FrameworkOnUpdateSetup()
-    {
-        try {
-            var battleTalkNode = GetBattleTalkNode();
-            if (battleTalkNode == null) return;
-            originalPositionX = battleTalkNode->X;
-            originalPositionY = battleTalkNode->Y;
-            Common.FrameworkUpdate -= FrameworkOnUpdateSetup;
-            Common.FrameworkUpdate += FrameworkOnUpdate;
-        } catch (Exception ex) {
-            SimpleLog.Error(ex);
-        }
-    }
-    private void FrameworkOnUpdate() {
-        try {
-            var battleTalkNode = GetBattleTalkNode();
-            if (battleTalkNode == null) return;
-            UiHelper.SetPosition(battleTalkNode, originalPositionX + Config.OffsetX, originalPositionY + Config.OffsetY);
-            battleTalkNode->SetScale(Config.Scale, Config.Scale);
-            Common.FrameworkUpdate -= FrameworkOnUpdate;
-        } catch (Exception ex) {
-            SimpleLog.Error(ex);
-        }
-    }
-        
-    private void ResetBattleTalk()
-    {
-        var battleTalkNode = GetBattleTalkNode();
-        if (battleTalkNode == null) return;
-        UiHelper.SetPosition(battleTalkNode, originalPositionX, originalPositionY);
-        battleTalkNode->SetScale(1, 1);
-    }
+    protected override void Disable() => Update(Configuration.Default);
         
     private static AtkResNode* GetBattleTalkNode() {
         var battleTalkUnitBase = Common.GetUnitBase("_BattleTalk");
