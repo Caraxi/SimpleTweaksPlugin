d8803bdf996a57f20298057c9d9abdc4b281e702
Caraxi <git@caraxian.com>
2024-09-08 16:32:24 +0930

    Equip from Hotbar

--- Tweaks/EquipFromHotbar.cs
+++ Tweaks/EquipFromHotbar.cs
@@ -20,57 +20,56 @@ namespace SimpleTweaksPlugin.Tweaks;
 [TweakCategory(TweakCategory.UI)]
 [TweakReleaseVersion("1.9.7.0")]
 public unsafe class EquipFromHotbar : Tweak {
-    private void DrawConfig() {
+    protected void DrawConfig() {
         using (ImRaii.PushIndent()) {
             ImGui.Bullet();
             ImGui.SameLine();
             ImGui.TextWrapped("Equipment will be searched for in the armoury chest and main inventory.");
-        
+
             ImGui.Bullet();
             ImGui.SameLine();
             ImGui.TextWrapped("If multiple matching items are in your inventory, the first in inventory will be used.");
-        
+
             ImGui.Bullet();
             ImGui.SameLine();
             ImGui.TextWrapped("When equipping a ring, hold the SHIFT key to equip to left finger.");
-        
+
             ImGui.Bullet();
             ImGui.SameLine();
             ImGui.TextWrapped("Any equipment with a special action, such as the teleport with Eternity Ring, will not equip from hotbars.");
         }
-        
     }
-    
-    private delegate void* UseItem(AgentInventoryContext* inventoryContext, uint a2, uint a3, uint a4, ushort a5);
 
-    [TweakHook, Signature("E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 89 7C 24", DetourName = nameof(UseItemDetour))]
-    private readonly HookWrapper<UseItem> useItemHook = null!;
+    [TweakHook(typeof(AgentInventoryContext), nameof(AgentInventoryContext.UseItem), nameof(UseItemDetour))]
+    private readonly HookWrapper<AgentInventoryContext.Delegates.UseItem> useItemHook = null!;
 
     private delegate void MoveItem(RaptureAtkModule* a1, void* outValue, AtkValue* atkValues);
-    [Signature("48 89 5C 24 ?? 48 89 74 24 ?? 55 57 41 56 48 8B EC 48 83 EC 40 4C 8B F1")]
+
+    [Signature("48 89 5C 24 ?? 48 89 74 24 ?? 48 89 7C 24 ?? 55 41 56 41 57 48 8B EC 48 83 EC 40 4C 8B F1")]
     private readonly MoveItem moveItem = null!;
-    
-    private uint GetContainerId(InventoryType inventoryType) => inventoryType switch {
-        InventoryType.Inventory1 => 48,
-        InventoryType.Inventory2 => 49,
-        InventoryType.Inventory3 => 50,
-        InventoryType.Inventory4 => 51,
-        InventoryType.ArmoryMainHand => 57,
-        InventoryType.ArmoryHead => 58,
-        InventoryType.ArmoryBody => 59,
-        InventoryType.ArmoryHands => 60,
-        InventoryType.ArmoryLegs => 61,
-        InventoryType.ArmoryFeets => 62,
-        InventoryType.ArmoryOffHand => 63,
-        InventoryType.ArmoryEar => 64,
-        InventoryType.ArmoryNeck => 65,
-        InventoryType.ArmoryWrist => 66,
-        InventoryType.ArmoryRings => 67,
-        InventoryType.ArmorySoulCrystal => 68,
-        InventoryType.EquippedItems => 4,
-        _ => 0
-    };
-    
+
+    private uint GetContainerId(InventoryType inventoryType) =>
+        inventoryType switch {
+            InventoryType.Inventory1 => 48,
+            InventoryType.Inventory2 => 49,
+            InventoryType.Inventory3 => 50,
+            InventoryType.Inventory4 => 51,
+            InventoryType.ArmoryMainHand => 57,
+            InventoryType.ArmoryHead => 58,
+            InventoryType.ArmoryBody => 59,
+            InventoryType.ArmoryHands => 60,
+            InventoryType.ArmoryLegs => 61,
+            InventoryType.ArmoryFeets => 62,
+            InventoryType.ArmoryOffHand => 63,
+            InventoryType.ArmoryEar => 64,
+            InventoryType.ArmoryNeck => 65,
+            InventoryType.ArmoryWrist => 66,
+            InventoryType.ArmoryRings => 67,
+            InventoryType.ArmorySoulCrystal => 68,
+            InventoryType.EquippedItems => 4,
+            _ => 0
+        };
+
     private void Equip(InventoryType sourceInventory, uint sourceSlot, uint equipSlot) {
         var sourceContainerId = GetContainerId(sourceInventory);
         var destinationContainerId = GetContainerId(InventoryType.EquippedItems);
@@ -86,8 +85,8 @@ public unsafe class EquipFromHotbar : Tweak {
             moveItem(atkModule, dropOut, eis);
         }
     }
-    
-    private void* UseItemDetour(AgentInventoryContext* inventoryContext, uint itemId, uint a3, uint a4, ushort a5) {
+
+    private long UseItemDetour(AgentInventoryContext* inventoryContext, uint itemId, uint a3, uint a4, short a5) {
         var retVal = useItemHook.Original(inventoryContext, itemId, a3, a4, a5);
         try {
             if (!(a3 == 9999 && a4 == 0 && a5 == 0 && itemId < 1500000)) return retVal;
@@ -96,8 +95,8 @@ public unsafe class EquipFromHotbar : Tweak {
             var item = Service.Data.GetExcelSheet<Item>()?.GetRow(realId);
             if (item == null) return retVal;
             if (item.EquipSlotCategory.Row == 0 || item.EquipSlotCategory.Value == null) return retVal;
-            if (item.ItemAction.Row != 0)return retVal;
-            
+            if (item.ItemAction.Row != 0) return retVal;
+
             var itemOrderModule = ItemOrderModule.Instance();
             var esc = item.EquipSlotCategory.Value;
             if (esc.MainHand == 1 && FindAndEquip(realId, isHq, InventoryType.ArmoryMainHand, 0, itemOrderModule->ArmouryMainHandSorter)) return retVal;
@@ -116,23 +115,23 @@ public unsafe class EquipFromHotbar : Tweak {
             SimpleLog.Error(ex, "Error in UseItemDetour");
             return retVal;
         }
-        
+
         return retVal;
     }
-    
+
     private bool FindAndEquip(uint itemId, bool isHq, InventoryType inventoryType, uint equipSlot, ItemOrderModuleSorter* sorter) {
         var inventoryManager = InventoryManager.Instance();
-        for (var i = 0U; i < sorter->Items.Size(); i++) {
-            var entry = sorter->Items.Get(i).Value;
+        for (var i = 0U; i < sorter->Items.LongCount; i++) {
+            var entry = sorter->Items[i].Value;
             var item = inventoryManager->GetInventorySlot(inventoryType + entry->Page, entry->Slot);
-            if (item->ItemID == itemId && item->Flags.HasFlag(InventoryItem.ItemFlags.HQ) == isHq) {
-                var page = (uint) (i / sorter->ItemsPerPage);
-                var slot = (uint) (i % sorter->ItemsPerPage);
+            if (item->ItemId == itemId && item->Flags.HasFlag(InventoryItem.ItemFlags.HighQuality) == isHq) {
+                var page = (uint)(i / sorter->ItemsPerPage);
+                var slot = (uint)(i % sorter->ItemsPerPage);
                 Equip(inventoryType + page, slot, equipSlot);
                 return true;
             }
         }
-        
+
         if (inventoryType != InventoryType.Inventory1) return FindAndEquip(itemId, isHq, InventoryType.Inventory1, equipSlot, ItemOrderModule.Instance()->InventorySorter);
         return false;
     }
