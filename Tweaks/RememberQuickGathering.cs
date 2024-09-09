72f1a0c251b24d34963d4e87d9de0b7d271d5a8c
Caraxi <git@caraxian.com>
2024-09-09 12:43:15 +0930

    Remember Quick Gathering

--- Tweaks/RememberQuickGathering.cs
+++ Tweaks/RememberQuickGathering.cs
@@ -1,105 +1,21 @@
-﻿using FFXIVClientStructs.FFXIV.Client.UI;
-using FFXIVClientStructs.FFXIV.Component.GUI;
-using SimpleTweaksPlugin.TweakSystem;
+﻿using SimpleTweaksPlugin.TweakSystem;
 using SimpleTweaksPlugin.Utility;
-using System;
-using System.Runtime.InteropServices;
+using Dalamud.Utility.Signatures;
+using FFXIVClientStructs.FFXIV.Client.Game.Event;
+using FFXIVClientStructs.FFXIV.Client.UI;
 
 namespace SimpleTweaksPlugin.Tweaks;
+
+[TweakName("Remember Quick Gathering")]
+[TweakDescription("Remembers quick gathering status even after gathering at unspoiled nodes.")]
 public unsafe class RememberQuickGathering : Tweak {
-    public override string Name => "Remember Quick Gathering";
-    public override string Description => "Remembers quick gathering status even after gathering at unspoiled nodes.";
+    private delegate void SetupGatheringPointDelegate(GatheringPointEventHandler* handler);
 
-    private delegate void ToggleQuickGathering(AddonGathering* _this);
+    [TweakHook, Signature("E8 ?? ?? ?? ?? EB 0A 45 33 C0 33 D2 E8 ?? ?? ?? ?? 45 8B CE", DetourName = nameof(SetupGatheringPoint))]
+    private HookWrapper<SetupGatheringPointDelegate> setupGatheringPointHook;
 
-    private ToggleQuickGathering toggleQuickGathering;
-    private bool? targetQuickGatheringStatus;
-    private bool wasAddonOpen;
-    private bool shouldUpdateCheckmark;
-
-    public override void Setup() {
-        if (Ready) return;
-
-        try {
-            if (toggleQuickGathering is null) {
-                var toggleQuickGatheringPtr = Service.SigScanner.ScanText("e8 ?? ?? ?? ?? eb 3f 4c 8b 4c 24 50");
-                toggleQuickGathering = Marshal.GetDelegateForFunctionPointer<ToggleQuickGathering>(toggleQuickGatheringPtr);
-            }
-
-            base.Setup();
-        } catch (Exception ex) {
-            SimpleLog.Error($"Failed to setup {this.GetType().Name}: {ex.Message}");
-        }
-    }
-
-    protected override void Enable() {
-        Common.FrameworkUpdate += FrameworkUpdate;
-        base.Enable();
-    }
-
-    private void FrameworkUpdate() {
-        var addon = Common.GetUnitBase<AddonGathering>("Gathering");
-        if (addon is not null && IsGatheringPointLoaded(addon)) {
-            if (CanQuickGather(addon)) {
-                if (targetQuickGatheringStatus.HasValue) {
-                    if (!wasAddonOpen) {
-                        TrySetQuickGathering(addon);
-                    }
-                    if (shouldUpdateCheckmark) {
-                        TryUpdateCheckmark(addon);
-                    }
-                }
-
-                targetQuickGatheringStatus = addon->QuickGatheringComponentCheckBox->IsChecked;
-            }
-            wasAddonOpen = true;
-        } else {
-            wasAddonOpen = false;
-            shouldUpdateCheckmark = false;
-        }
-    }
-
-    private bool CanQuickGather(AddonGathering* addon) {
-        // Use visibility of quick gathering checkbox as indicator
-        return addon->QuickGatheringComponentCheckBox->AtkComponentButton.AtkComponentBase.OwnerNode->AtkResNode.IsVisible;
-    }
-    private void TrySetQuickGathering(AddonGathering* addon) {
-        // Only toggle if not already in target state
-        if (addon->QuickGatheringComponentCheckBox->IsChecked != targetQuickGatheringStatus) {
-            addon->QuickGatheringComponentCheckBox->AtkComponentButton.Flags ^= 0x40000;
-            toggleQuickGathering(addon);
-            shouldUpdateCheckmark = true;
-        }
-    }
-    private void TryUpdateCheckmark(AddonGathering* addon) {
-        // It takes a few attempts for these changes to stick, haven't looked into why so we just keep trying.
-
-        // I don't know if there's a better way to get the checkmark image node
-        AtkResNode* checkmarkNode = addon->QuickGatheringComponentCheckBox->AtkComponentButton.ButtonBGNode->PrevSiblingNode;
-        if (checkmarkNode->IsVisible == targetQuickGatheringStatus) {
-            // Finally, success
-            shouldUpdateCheckmark = false;
-        } else {
-            checkmarkNode->Color.A = (byte)(targetQuickGatheringStatus.Value ? 0xFF : 0x7F);
-            checkmarkNode->NodeFlags ^= NodeFlags.Visible;
-        }
-    }
-
-    private static bool IsGatheringPointLoaded(AddonGathering* addon) {
-        // At least one item slot needs to have an item ID
-        return addon->GatheredItemId1 != 0
-                || addon->GatheredItemId2 != 0
-                || addon->GatheredItemId3 != 0
-                || addon->GatheredItemId4 != 0
-                || addon->GatheredItemId5 != 0
-                || addon->GatheredItemId6 != 0
-                || addon->GatheredItemId7 != 0
-                || addon->GatheredItemId8 != 0;
-    }
-
-    protected override void Disable() {
-        targetQuickGatheringStatus = null;
-        Common.FrameworkUpdate -= FrameworkUpdate;
-        base.Disable();
+    private void SetupGatheringPoint(GatheringPointEventHandler* gatheringPointEventHandler) {
+        setupGatheringPointHook.Original(gatheringPointEventHandler);
+        gatheringPointEventHandler->QuickGatheringEnabled = RaptureAtkModule.Instance()->QuickGatheringEnabled;
     }
 }
