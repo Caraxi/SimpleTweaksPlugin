3944d2ad3c64adf93b0003f3f8af0f577820fd28
Caraxi <git@caraxian.com>
2024-09-11 20:32:41 +0930

    Submarine Destination Letters

--- Tweaks/UiAdjustment/SubmarineDestinationLetters.cs
+++ Tweaks/UiAdjustment/SubmarineDestinationLetters.cs
@@ -4,8 +4,10 @@ using System.Runtime.InteropServices;
 using System.Text;
 using Dalamud.Game.Text;
 using Dalamud.Utility;
+using Dalamud.Utility.Signatures;
 using Lumina.Data;
 using Lumina.Excel.GeneratedSheets;
+using SimpleTweaksPlugin.TweakSystem;
 using SimpleTweaksPlugin.Utility;
 
 namespace SimpleTweaksPlugin.Tweaks.UiAdjustment;
@@ -14,116 +16,107 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment;
  * https://git.anna.lgbt/ascclemens/Namingway/src/branch/main/Namingway/Renamer.cs
  */
 
+[TweakName("Label Submarine Destinations with Letters")]
+[TweakDescription("Uses the standard A-Z lettering to identify submarine destinations for easier use with other tools.")]
 public unsafe class SubmarineDestinationLetters : UiAdjustments.SubTweak {
-    public override string Name => "Label Submarine Destinations with Letters";
-    public override string Description => "Uses the standard A-Z lettering to identify submarine destinations for easier use with other tools.";
-    public override bool Experimental => true;
+    private delegate nint GetSubmarineExplorationRowDelegate(uint explorationId);
 
-    private delegate IntPtr GetSubmarineExplorationRowDelegate(uint explorationId);
+    [TweakHook, Signature("E8 ?? ?? ?? ?? 80 78 16 ?? 75 04", DetourName = nameof(GetExplorationRowDetour))]
     private HookWrapper<GetSubmarineExplorationRowDelegate> getSubmarineExplorationRowHook;
-    private readonly Dictionary<uint, IntPtr> allocations = new();
+
+    private readonly Dictionary<uint, nint> allocations = new();
     private readonly Dictionary<uint, string> destinationNames = new();
 
     protected override void Enable() {
         destinationNames.Clear();
-        var jpSheet = Service.Data.Excel.GetSheet<SubmarineExploration>(Language.French);
-        if (jpSheet == null) return;
+        var frSheet = Service.Data.Excel.GetSheet<SubmarineExploration>(Language.French);
+        if (frSheet == null) return;
         var sheet = Service.Data.Excel.GetSheet<SubmarineExploration>();
         if (sheet == null) return;
         foreach (var e in sheet) {
-            var jpRow = jpSheet.GetRow(e.RowId);
-            if (jpRow == null) continue;
-            destinationNames.Add(e.RowId, $"{ToBoxedLetters(jpRow.Location.RawString)} {e.Destination.ToDalamudString().TextValue}");
+            var frRow = frSheet.GetRow(e.RowId);
+            if (frRow == null) continue;
+            destinationNames.Add(e.RowId, $"{ToBoxedLetters(frRow.Location.RawString)} {e.Destination.ToDalamudString().TextValue}");
         }
-            
-        this.getSubmarineExplorationRowHook ??= Common.Hook<GetSubmarineExplorationRowDelegate>("E8 ?? ?? ?? ?? 80 78 16 ?? 75 04", this.GetExplorationRowDetour);
-        this.getSubmarineExplorationRowHook?.Enable();
-        base.Enable();
     }
 
-    private IntPtr GetExplorationRowDetour(uint statusId) {
-        var data = this.getSubmarineExplorationRowHook.Original(statusId);
+    private nint GetExplorationRowDetour(uint statusId) {
+        var data = getSubmarineExplorationRowHook.Original(statusId);
 
         try {
-            return this.GetStatusSheetDetourInner(statusId, data);
+            return GetStatusSheetDetourInner(statusId, data);
         } catch (Exception ex) {
             SimpleLog.Error(ex, "Exception in GetStatusSheetDetour");
         }
 
         return data;
     }
-    
-    private IntPtr GetStatusSheetDetourInner(uint statusId, IntPtr data) {
-            const int destinationOffset = 0;
-            const int locationOffset = 4;
-            const int expReward = 8;
 
-            if (destinationNames.TryGetValue(statusId, out var name)) {
-                if (this.allocations.TryGetValue(statusId, out var cached)) {
-                    return cached + destinationOffset;
-                }
+    private nint GetStatusSheetDetourInner(uint statusId, nint data) {
+        const int destinationOffset = 0;
+        const int locationOffset = 4;
+        const int expReward = 8;
 
-                if (data == nint.Zero) return data;
-                var raw = new byte[1024];
-                Marshal.Copy(data - destinationOffset, raw, 0, raw.Length);
-                var nameBytes = Encoding.UTF8.GetBytes(name);
-                var oldName = ReadRawBytes(data + raw[destinationOffset]);
-                var descBytes = ReadRawBytes(data + raw[locationOffset] + 4);
-                var oldPost = raw[destinationOffset] + oldName.Length + 1 + descBytes.Length + 1;
-                var newPost = raw[destinationOffset] + nameBytes.Length + 1 + descBytes.Length + 1;
-
-                var newData = new byte[1536];
-
-                // copy over header
-                for (var i = 0; i < destinationOffset + raw[destinationOffset]; i++) {
-                    newData[i] = raw[i];
-                }
-
-                newData[destinationOffset] = raw[destinationOffset];
-                newData[locationOffset] = (byte) (destinationOffset + newData[destinationOffset] + nameBytes.Length + 1 - 4);
-
-                // copy icon
-                for (var i = 0; i < 4; i++) {
-                    newData[expReward + i] = raw[expReward + i];
-                }
-
-                // copy name
-                for (var i = 0; i < nameBytes.Length; i++) {
-                    newData[destinationOffset + newData[destinationOffset] + i] = nameBytes[i];
-                }
-
-                // copy description
-                for (var i = 0; i < descBytes.Length; i++) {
-                    newData[locationOffset + newData[locationOffset] + i] = descBytes[i];
-                }
-
-                // copy post-description info
-                for (var i = 0; i < raw.Length - oldPost; i++) {
-                    newData[newPost + i] = raw[oldPost + i];
-                }
-
-                var newSheet = Marshal.AllocHGlobal(newData.Length);
-                Marshal.Copy(newData, 0, newSheet, newData.Length);
-
-                this.allocations[statusId] = newSheet;
-                return newSheet + destinationOffset;
+        if (destinationNames.TryGetValue(statusId, out var name)) {
+            if (allocations.TryGetValue(statusId, out var cached)) {
+                return cached + destinationOffset;
             }
 
-            return data;
+            if (data == nint.Zero) return data;
+            var raw = new byte[1024];
+            Marshal.Copy(data - destinationOffset, raw, 0, raw.Length);
+            var nameBytes = Encoding.UTF8.GetBytes(name);
+            var oldName = ReadRawBytes(data + raw[destinationOffset]);
+            var descBytes = ReadRawBytes(data + raw[locationOffset] + 4);
+            var oldPost = raw[destinationOffset] + oldName.Length + 1 + descBytes.Length + 1;
+            var newPost = raw[destinationOffset] + nameBytes.Length + 1 + descBytes.Length + 1;
+
+            var newData = new byte[1536];
+
+            // copy over header
+            for (var i = 0; i < destinationOffset + raw[destinationOffset]; i++) {
+                newData[i] = raw[i];
+            }
+
+            newData[destinationOffset] = raw[destinationOffset];
+            newData[locationOffset] = (byte)(destinationOffset + newData[destinationOffset] + nameBytes.Length + 1 - 4);
+
+            // copy icon
+            for (var i = 0; i < 4; i++) {
+                newData[expReward + i] = raw[expReward + i];
+            }
+
+            // copy name
+            for (var i = 0; i < nameBytes.Length; i++) {
+                newData[destinationOffset + newData[destinationOffset] + i] = nameBytes[i];
+            }
+
+            // copy description
+            for (var i = 0; i < descBytes.Length; i++) {
+                newData[locationOffset + newData[locationOffset] + i] = descBytes[i];
+            }
+
+            // copy post-description info
+            for (var i = 0; i < raw.Length - oldPost; i++) {
+                newData[newPost + i] = raw[oldPost + i];
+            }
+
+            var newSheet = Marshal.AllocHGlobal(newData.Length);
+            Marshal.Copy(newData, 0, newSheet, newData.Length);
+
+            allocations[statusId] = newSheet;
+            return newSheet + destinationOffset;
         }
 
-    protected override void Disable() {
-        foreach (var ptr in this.allocations.Values) {
-            Marshal.FreeHGlobal(ptr);
-        }
-        allocations.Clear();
-        getSubmarineExplorationRowHook?.Disable();
-        base.Disable();
+        return data;
     }
 
-    public override void Dispose() {
-        getSubmarineExplorationRowHook?.Dispose();
-        base.Dispose();
+    protected override void Disable() {
+        foreach (var ptr in allocations.Values) {
+            Marshal.FreeHGlobal(ptr);
+        }
+
+        allocations.Clear();
     }
 
     private static string ToBoxedLetters(string inString) {
@@ -141,11 +134,11 @@ public unsafe class SubmarineDestinationLetters : UiAdjustments.SubTweak {
 
         return outString;
     }
-    
-    internal static byte[] ReadRawBytes(IntPtr ptr) {
+
+    private static byte[] ReadRawBytes(nint ptr) {
         var bytes = new List<byte>();
 
-        var bytePtr = (byte*) ptr;
+        var bytePtr = (byte*)ptr;
         while (*bytePtr != 0) {
             bytes.Add(*bytePtr);
             bytePtr += 1;
