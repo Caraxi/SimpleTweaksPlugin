af98bfeb0f228ac734b5d0e2ab6bde7826f58982
Caraxi <git@caraxian.com>
2024-09-19 17:23:25 +0930

    `Add Number Separators`

--- Tweaks/UiAdjustment/AddNumberSeparators.cs
+++ Tweaks/UiAdjustment/AddNumberSeparators.cs
@@ -15,7 +15,8 @@ namespace SimpleTweaksPlugin.Tweaks.UiAdjustment;
 [TweakName("Add Number Separators")]
 [TweakDescription("Add separators for various numbers shown in the UI")]
 [TweakAuthor("Anna")]
-[TweakVersion(1)]
+[TweakAutoConfig]
+[TweakReleaseVersion(UnreleasedVersion)]
 public unsafe class AddNumberSeparators : UiAdjustments.SubTweak {
     public class Configs : TweakConfig {
         public bool FlyText = true;
@@ -25,7 +26,8 @@ public unsafe class AddNumberSeparators : UiAdjustments.SubTweak {
         public char? CustomSeparator;
     }
 
-    public Configs Config { get; private set; }
+    [TweakConfig]
+    public Configs Config { get; protected set; }
 
     protected void DrawConfig(ref bool hasChanged) {
         hasChanged |= ImGui.Checkbox("Add separators to damage/healing numbers", ref Config.FlyText);
@@ -40,20 +42,15 @@ public unsafe class AddNumberSeparators : UiAdjustments.SubTweak {
             SetSeparator(Config.CustomSeparator);
         }
 
-        if (hasChanged) {
-            SaveConfig(Config);
-            ConfigureInstructions();
-        }
+        if (hasChanged) ConfigureInstructions();
     }
 
     private static class Signatures {
         internal const string ShowFlyText = "E8 ?? ?? ?? ?? FF C7 41 D1 C7";
-        internal const string SprintfNumber = "48 83 EC 28 44 8B C9";
-
-        internal const string FlyTextStringify = "45 33 C0 C6 44 24 ?? ?? 8B D3 E8 ?? ?? ?? ?? 41 8B CF E8";
-        internal const string HotbarManaStringify = "45 33 C0 44 88 64 24 ?? 44 88 64 24 ?? 8B 14 08 48 8B CE E8";
-        internal const string PartyListStringify = "45 33 C0 C6 44 24 ?? ?? 8B D5 E8 ?? ?? ?? ?? EB";
-
+        internal const string SprintfNumber = "E8 ?? ?? ?? ?? EB 68 48 8B 03";
+        internal const string FlyTextStringify = " 45 33 C0 C6 44 24 ?? ?? 8B D7 E8 ?? ?? ?? ?? 41 8B CF";
+        internal const string HotbarManaStringify = "45 33 C0 48 8B CE 44 88 64 24 ?? 42 8B 54 B8 ?? E8 ?? ?? ?? ?? EB 21";
+        internal const string PartyListStringify = "45 33 C0 C6 44 24 20 00 41 8B D6 E8 ?? ?? ?? ?? 49 8B";
         internal const string Separator = "44 0F B6 05 ?? ?? ?? ?? 45 84 C0 74 36 F6 87";
     }
 
@@ -64,41 +61,32 @@ public unsafe class AddNumberSeparators : UiAdjustments.SubTweak {
     private delegate void ShowFlyTextDelegate(nint addon, uint actorIndex, uint messageMax, nint numbers, int offsetNum, int offsetNumMax, nint strings, int offsetStr, int offsetStrMax, int a10);
 
     [TweakHook, Signature(Signatures.ShowFlyText, DetourName = nameof(ShowFlyTextDetour))]
-    private HookWrapper<ShowFlyTextDelegate>? _showFlyTextHook;
+    private HookWrapper<ShowFlyTextDelegate>? showFlyTextHook;
 
     private delegate nint SprintfNumberDelegate(uint number);
 
     [TweakHook, Signature(Signatures.SprintfNumber, DetourName = nameof(SprintfNumberDetour))]
-    private HookWrapper<SprintfNumberDelegate>? _sprintfNumberHook;
+    private HookWrapper<SprintfNumberDelegate>? sprintfNumberHook;
 
     private static readonly byte[] ThirdArgOne = [
         0x41, 0xB0, 0x01,
     ];
 
-    public override void Setup() {
+    protected override void Setup() {
         if (Service.SigScanner.TryGetStaticAddressFromSig(Signatures.Separator, out var separatorPtr)) {
             SeparatorPtr = separatorPtr;
-            OriginalSeparator = *(byte*) separatorPtr;
+            OriginalSeparator = *(byte*)separatorPtr;
         }
-
-        base.Setup();
     }
 
     protected override void Enable() {
-        Config = LoadConfig<Configs>() ?? new Configs();
         ConfigureInstructions();
         SetSeparator(Config.CustomSeparator);
-        base.Enable();
     }
 
     protected override void Disable() {
         RestoreAllBytes();
-
-        if (SeparatorPtr != 0 && OriginalSeparator != 0) {
-            *(byte*) SeparatorPtr = OriginalSeparator;
-        }
-
-        base.Disable();
+        SetSeparator(null);
     }
 
     private void SetSeparator(char? sep) {
@@ -106,12 +94,12 @@ public unsafe class AddNumberSeparators : UiAdjustments.SubTweak {
             return;
         }
 
-        var separator = (byte?) sep ?? OriginalSeparator;
+        var separator = (byte?)sep ?? OriginalSeparator;
         if (separator == 0) {
-            separator = (byte) ',';
+            separator = (byte)',';
         }
 
-        *(byte*) SeparatorPtr = separator;
+        *(byte*)SeparatorPtr = separator;
     }
 
     internal void ConfigureInstructions() {
@@ -133,20 +121,14 @@ public unsafe class AddNumberSeparators : UiAdjustments.SubTweak {
     }
 
     private void ReplaceBytes(nint ptr) {
-        if (OldBytes.ContainsKey(ptr)) {
-            return;
-        }
-
+        if (OldBytes.ContainsKey(ptr)) return;
         SafeMemory.ReadBytes(ptr, ThirdArgOne.Length, out var oldBytes);
         SafeMemory.WriteBytes(ptr, ThirdArgOne);
         OldBytes[ptr] = oldBytes;
     }
 
     private void RestoreBytes(nint ptr) {
-        if (!OldBytes.TryGetValue(ptr, out var oldBytes)) {
-            return;
-        }
-
+        if (!OldBytes.TryGetValue(ptr, out var oldBytes)) return;
         SafeMemory.WriteBytes(ptr, oldBytes);
         OldBytes.Remove(ptr);
     }
@@ -158,7 +140,7 @@ public unsafe class AddNumberSeparators : UiAdjustments.SubTweak {
     }
 
     private void ShowFlyTextDetour(nint addon, uint actorIndex, uint messageMax, nint numbers, int offsetNum, int offsetNumMax, nint strings, int offsetStr, int offsetStrMax, int a10) {
-        _showFlyTextHook!.Original(addon, actorIndex, messageMax, numbers, offsetNum, offsetNumMax, strings, offsetStr, offsetStrMax, a10);
+        showFlyTextHook!.Original(addon, actorIndex, messageMax, numbers, offsetNum, offsetNumMax, strings, offsetStr, offsetStrMax, a10);
 
         if (!Config.FlyText) {
             return;
@@ -166,12 +148,12 @@ public unsafe class AddNumberSeparators : UiAdjustments.SubTweak {
 
         static void Action(nint ptr) {
             // only check text nodes
-            var node = (AtkResNode*) ptr;
+            var node = (AtkResNode*)ptr;
             if (node->Type != NodeType.Text) {
                 return;
             }
 
-            var text = (AtkTextNode*) node;
+            var text = (AtkTextNode*)node;
             var font = (text->AlignmentFontType & 0xF0) >> 4;
             // only touch text nodes with a font above four and less than eight
             if (font is not (> 4 and < 8)) {
@@ -180,15 +162,15 @@ public unsafe class AddNumberSeparators : UiAdjustments.SubTweak {
 
             // only touch text nodes with a string starting with a digit
             var stringPtr = text->NodeText.StringPtr;
-            if (stringPtr == null || !char.IsDigit((char) *stringPtr)) {
+            if (stringPtr == null || !char.IsDigit((char)*stringPtr)) {
                 return;
             }
 
             // set the font type of the node to 4 for non-number support
-            text->AlignmentFontType = (byte) ((text->AlignmentFontType & 0xF) | (4 << 4));
+            text->AlignmentFontType = (byte)((text->AlignmentFontType & 0xF) | (4 << 4));
         }
 
-        var unit = (AtkUnitBase*) addon;
+        var unit = (AtkUnitBase*)addon;
         if (unit->RootNode != null) {
             TraverseNodes(unit->RootNode, Action);
         }
@@ -204,12 +186,12 @@ public unsafe class AddNumberSeparators : UiAdjustments.SubTweak {
             return;
         }
 
-        action((nint) node);
+        action((nint)node);
 
-        if ((int) node->Type < 1000) {
+        if ((int)node->Type < 1000) {
             TraverseNodes(node->ChildNode, action);
         } else {
-            var comp = (AtkComponentNode*) node;
+            var comp = (AtkComponentNode*)node;
 
             for (var i = 0; i < comp->Component->UldManager.NodeListCount; i++) {
                 TraverseNodes(comp->Component->UldManager.NodeList[i], action);
@@ -232,12 +214,12 @@ public unsafe class AddNumberSeparators : UiAdjustments.SubTweak {
     }
 
     private nint SprintfNumberDetour(uint number) {
-        var ret = (byte*) _sprintfNumberHook!.Original(number);
+        var ret = (byte*)sprintfNumberHook!.Original(number);
         if (!Config.AbilityTooltip) {
             goto Return;
         }
 
-        var nfi = (NumberFormatInfo) NumberFormatInfo.CurrentInfo.Clone();
+        var nfi = (NumberFormatInfo)NumberFormatInfo.CurrentInfo.Clone();
         if (Config.CustomSeparator != null) {
             nfi.NumberGroupSeparator = Config.CustomSeparator.ToString();
         }
@@ -251,6 +233,6 @@ public unsafe class AddNumberSeparators : UiAdjustments.SubTweak {
         *(ret + strBytes.Length) = 0;
 
         Return:
-        return (nint) ret;
+        return (nint)ret;
     }
 }
