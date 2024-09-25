f98740c28eb7c096d71e347007ed8ab9700d5b4f
Caraxi <git@caraxian.com>
2024-09-25 18:47:38 +0930

    [`Echo Story Selection`] Add option to hide some choices

--- Tweaks/Chat/EchoStorySelection.cs
+++ Tweaks/Chat/EchoStorySelection.cs
@@ -1,12 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using Dalamud.Game.ClientState.Conditions;
 using Dalamud.Game.Text;
 using Dalamud.Game.Text.SeStringHandling;
+using Dalamud.Interface;
+using Dalamud.Interface.Components;
+using Dalamud.Interface.Utility.Raii;
 using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Component.GUI;
+using ImGuiNET;
 using SimpleTweaksPlugin.Events;
 using SimpleTweaksPlugin.TweakSystem;
 using SimpleTweaksPlugin.Utility;
@@ -18,57 +23,62 @@ namespace SimpleTweaksPlugin.Tweaks.Chat;
 [TweakAuthor("MidoriKami")]
 [TweakReleaseVersion("1.8.3.2")]
 [TweakAutoConfig]
+[Changelog(UnreleasedVersion, "Added option to hide some choices.")]
 public unsafe class EchoStorySelection : ChatTweaks.SubTweak {
-    private readonly List<string> options = [];
-    
     public class Config : TweakConfig {
         [TweakConfigOption("Display Character Name")]
-        public bool DisplayCharacterName = false;
+        public bool DisplayCharacterName;
+
+        public List<string> IgnoredMessages = [];
     }
 
-    public Config TweakConfig { get; private set; } = null!;
-    
-    [AddonPostSetup("CutSceneSelectString", "SelectString")]
-    private void OnAddonSetup(AddonSetupArgs obj) {
-        switch (obj) {
-            case { AddonName: "CutSceneSelectString", Addon: not 0 }:
-                GetAddonStrings(((AddonCutSceneSelectString*) obj.Addon)->OptionList);
-                break;
-            
-            case { AddonName: "SelectString", Addon: not 0 } when Service.Condition[ConditionFlag.OccupiedInQuestEvent]:
-                GetAddonStrings(((AddonSelectString*) obj.Addon)->PopupMenu.PopupMenu.List);
-                break;
+    [TweakConfig] public Config TweakConfig { get; private set; } = null!;
+
+    private string newIgnoredMessageString = string.Empty;
+
+    protected void DrawConfig() {
+        if (!ImGui.CollapsingHeader($"Hidden Choices ({TweakConfig.IgnoredMessages.Count})###HiddenMessages_{nameof(EchoStorySelection)}")) return;
+        using (ImRaii.PushIndent()) {
+            for (var i = 0; i < TweakConfig.IgnoredMessages.Count; i++) {
+                using (ImRaii.PushId($"ignoredMessage_{i}")) {
+                    if (ImGuiComponents.IconButton(FontAwesomeIcon.Trash)) {
+                        TweakConfig.IgnoredMessages.RemoveAt(i);
+                        i--;
+                        continue;
+                    }
+
+                    var ignoredMessage = TweakConfig.IgnoredMessages[i];
+
+                    ImGui.SameLine();
+
+                    if (ImGui.InputText($"##hiddenMessage_{i}", ref ignoredMessage, 512)) {
+                        TweakConfig.IgnoredMessages[i] = ignoredMessage;
+                    }
+                }
+            }
+
+            using (ImRaii.Disabled(string.IsNullOrWhiteSpace(newIgnoredMessageString) || TweakConfig.IgnoredMessages.Contains(newIgnoredMessageString.Trim()))) {
+                if (ImGuiComponents.IconButton(FontAwesomeIcon.Plus) && !string.IsNullOrWhiteSpace(newIgnoredMessageString)) {
+                    TweakConfig.IgnoredMessages.Add(newIgnoredMessageString.Trim());
+                    newIgnoredMessageString = string.Empty;
+                }
+            }
+
+            ImGui.SameLine();
+            ImGui.InputText($"##newIgnoredMessage", ref newIgnoredMessageString, 512);
         }
     }
-    
+
     [AddonFinalize("CutSceneSelectString", "SelectString")]
     private void OnAddonFinalize(AddonFinalizeArgs obj) {
         switch (obj) {
             case { AddonName: "CutSceneSelectString", Addon: not 0 }:
-                PrintSelectedString(((AddonCutSceneSelectString*) obj.Addon)->OptionList);
+                PrintSelectedString(((AddonCutSceneSelectString*)obj.Addon)->OptionList);
                 break;
-            
+
             case { AddonName: "SelectString", Addon: not 0 } when Service.Condition[ConditionFlag.OccupiedInQuestEvent]:
-                PrintSelectedString(((AddonSelectString*) obj.Addon)->PopupMenu.PopupMenu.List);
+                PrintSelectedString(((AddonSelectString*)obj.Addon)->PopupMenu.PopupMenu.List);
                 break;
-        }
-    }
-
-    private void GetAddonStrings(AtkComponentList* list) {
-        if(list is null) return;
-        
-        options.Clear();
-        
-        foreach (var index in Enumerable.Range(0, list->ListLength)) {
-            var listItemRenderer = list->ItemRendererList[index].AtkComponentListItemRenderer;
-            if (listItemRenderer is null) continue;
-
-            var buttonTextNode = listItemRenderer->AtkComponentButton.ButtonTextNode;
-            if (buttonTextNode is null) continue;
-
-            var buttonText = Common.ReadSeString(buttonTextNode->NodeText.StringPtr);
-
-            options.Add(buttonText.TextValue);
         }
     }
 
@@ -76,16 +86,26 @@ public unsafe class EchoStorySelection : ChatTweaks.SubTweak {
         if (list is null) return;
 
         var selectedItem = list->SelectedItemIndex;
-        if (selectedItem < 0 || selectedItem >= options.Count) return;
+        if (selectedItem < 0 || selectedItem >= list->ListLength) return;
 
-        var message = new SeStringBuilder()
-            .AddText(options[selectedItem])
+        var listItemRenderer = list->ItemRendererList[selectedItem].AtkComponentListItemRenderer;
+        if (listItemRenderer is null) return;
+
+        var buttonTextNode = listItemRenderer->AtkComponentButton.ButtonTextNode;
+        if (buttonTextNode is null) return;
+
+        var buttonText = Common.ReadSeString(buttonTextNode->NodeText);
+
+        if (TweakConfig.IgnoredMessages.Any(i => i.Trim()
+                .Equals(buttonText.TextValue.Trim(), StringComparison.InvariantCultureIgnoreCase)))
+            return;
+
+        var message = new SeStringBuilder().AddText(buttonText.TextValue)
             .Build();
 
         var playerName = PlayerState.Instance()->CharacterNameString;
-            
-        var name = new SeStringBuilder()
-            .AddUiForeground(TweakConfig.DisplayCharacterName ? playerName : "Story Selection", 62)
+
+        var name = new SeStringBuilder().AddUiForeground(TweakConfig.DisplayCharacterName ? playerName : "Story Selection", 62)
             .Build();
 
         Service.Chat.Print(new XivChatEntry {
