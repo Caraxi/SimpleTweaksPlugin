f66ea2cef2ec25ee5b84aae812974e7ffaac0c51
Caraxi <git@caraxian.com>
2025-01-22 16:04:55 +1030

    debugging stuff

---
+++ Debugging/ResetWindowPositions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+using ImGuiNET;
+
+namespace SimpleTweaksPlugin.Debugging;
+
+public class ResetWindowPositions : DebugHelper {
+    private List<string> windowList;
+    private string searchInput = string.Empty;
+
+    private string? resetWindowName;
+    
+    public override void Draw() {
+        if (windowList == null) {
+            var uiConfigFile = new FileInfo(Path.Join(Service.PluginInterface.ConfigFile.Directory?.Parent?.FullName ?? string.Empty, "dalamudUi.ini"));
+            windowList = [];
+
+            if (uiConfigFile.Exists) {
+                var lines = File.ReadAllLines(uiConfigFile.FullName);
+                foreach (var t in lines) {
+                    if (!t.StartsWith("[Window][")) continue;
+                    var windowName = t[9..^1];
+                    windowList.Add(windowName);
+                }
+
+                windowList.Sort();
+            }
+        }
+
+        ImGui.SetNextItemWidth(ImGui.GetContentRegionAvail().X);
+        ImGui.InputTextWithHint("###search", "Search...", ref searchInput, 512);
+
+        if (ImGui.BeginChild("buttonScroll", ImGui.GetContentRegionAvail())) {
+            var buttonIndex = 0;
+            var dl = ImGui.GetWindowDrawList();
+            foreach (var l in windowList) {
+                if (!string.IsNullOrWhiteSpace(searchInput) && !l.Contains(searchInput, StringComparison.InvariantCultureIgnoreCase)) continue;
+                if (ImGui.Button($"##Button#{buttonIndex++}", new Vector2(ImGui.GetContentRegionAvail().X, ImGui.GetTextLineHeight() + ImGui.GetStyle().FramePadding.Y * 2))) {
+                    resetWindowName = l;
+                    Service.PluginInterface.UiBuilder.Draw += DoReset;
+                }
+
+                dl.AddText(ImGui.GetItemRectMin() + ImGui.GetStyle().FramePadding, ImGui.GetColorU32(ImGuiCol.Text), l);
+            }
+        }
+
+        ImGui.EndChild();
+    }
+
+    private void DoReset() {
+        if (string.IsNullOrWhiteSpace(resetWindowName)) {
+            Service.PluginInterface.UiBuilder.Draw -= DoReset;
+            return;
+        }
+
+        ImGui.SetNextWindowPos(new Vector2(50, 50), ImGuiCond.Always);
+        ImGui.SetNextWindowCollapsed(false);
+
+        if (ImGui.Begin(resetWindowName, ImGuiWindowFlags.NoSavedSettings | ImGuiWindowFlags.NoDecoration | ImGuiWindowFlags.NoBackground | ImGuiWindowFlags.NoInputs)) {
+            Service.PluginInterface.UiBuilder.Draw -= DoReset;
+            resetWindowName = null;
+        }
+
+        ImGui.End();
+    }
+
+    public override void Dispose() {
+        Service.PluginInterface.UiBuilder.Draw -= DoReset;
+    }
+
+    public override string Name => "Reset Window Positions";
+}
--- Debugging/TargetDebugging.cs
+++ Debugging/TargetDebugging.cs
@@ -1,29 +1,40 @@
 ﻿using System.Collections.Generic;
+using FFXIVClientStructs.FFXIV.Client.Game.Character;
 using FFXIVClientStructs.FFXIV.Client.Game.Control;
+using FFXIVClientStructs.FFXIV.Client.Game.Object;
 using ImGuiNET;
 
 namespace SimpleTweaksPlugin.Debugging;
 
 public class TargetDebugging : DebugHelper {
+
+    private unsafe void PrintTarget(GameObject* gameObject) {
+        if (gameObject->IsCharacter()) {
+            DebugManager.PrintOutObject((Character*)gameObject);
+            return;
+        }
+        DebugManager.PrintOutObject(gameObject);
+    }
+    
     public override unsafe void Draw() {
         var targets = Service.Targets;
         var targetSystem = TargetSystem.Instance();
 
         ImGui.Text("Current Target:");
         if (targets.Target != null) DebugManager.PrintOutObject(targets.Target, (ulong)targets.Target.Address.ToInt64(), new List<string>() { "Target", "Current" });
-        if (targetSystem->Target != null) DebugManager.PrintOutObject(targetSystem->Target);
+        if (targetSystem->Target != null) PrintTarget(targetSystem->Target);
 
         ImGui.Text("Focus Target:");
         if (targets.FocusTarget != null) DebugManager.PrintOutObject(targets.FocusTarget, (ulong)targets.FocusTarget.Address.ToInt64(), new List<string>() { "Target", "Focus" });
-        if (targetSystem->FocusTarget != null) DebugManager.PrintOutObject(targetSystem->FocusTarget);
+        if (targetSystem->FocusTarget != null) PrintTarget(targetSystem->FocusTarget);
 
         ImGui.Text("Previous Target:");
         if (targets.PreviousTarget != null) DebugManager.PrintOutObject(targets.PreviousTarget, (ulong)targets.PreviousTarget.Address.ToInt64(), new List<string>() { "Target", "Prev" });
-        if (targetSystem->PreviousTarget != null) DebugManager.PrintOutObject(targetSystem->PreviousTarget);
+        if (targetSystem->PreviousTarget != null) PrintTarget(targetSystem->PreviousTarget);
 
         ImGui.Text("Mouse Over Target:");
         if (targets.MouseOverTarget != null) DebugManager.PrintOutObject(targets.MouseOverTarget, (ulong)targets.MouseOverTarget.Address.ToInt64(), new List<string>() { "Target", "MouseOver" });
-        if (targetSystem->MouseOverTarget != null) DebugManager.PrintOutObject(targetSystem->MouseOverTarget);
+        if (targetSystem->MouseOverTarget != null) PrintTarget(targetSystem->MouseOverTarget);
     }
 
     public override string Name => "Targets";
