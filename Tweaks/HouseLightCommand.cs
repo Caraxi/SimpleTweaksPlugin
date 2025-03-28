5ce31b8bb448268f8b89d3cf00abad34a616afe2
Caraxi <git@caraxian.com>
2025-03-28 18:06:45 +1030

    [`House Lights Command`] Add SSAO option

--- Tweaks/HouseLightCommand.cs
+++ Tweaks/HouseLightCommand.cs
@@ -14,55 +14,58 @@ namespace SimpleTweaksPlugin.Tweaks;
 [TweakDescription("Adds a command to control lighting in your own housing areas.")]
 [TweakReleaseVersion("1.8.2.0")]
 [Changelog("1.10.5.0", "Fixed tweak not working.")]
+[Changelog(UnreleasedVersion, "Added ability to toggle SSAO with 'ssao-on' and 'ssao-off' parameters")]
 public unsafe class HouseLightCommand : CommandTweak {
     public override bool Experimental => true;
     protected override string Command => "lights";
-    protected override string HelpMessage => $"Adjust the lighting of the house or apartment you are currently in. /{CustomOrDefaultCommand} (0-5) [save]";
+    protected override string HelpMessage => $"Adjust the lighting of the house or apartment you are currently in. /{CustomOrDefaultCommand} (0-5) [ssao-on | ssao-off] [save]";
     protected override bool ShowInHelp => true;
 
     private readonly string[] permanentMarkers = ["save"];
 
     protected override void OnCommand(string args) {
-        
-        
         var housingManager = HousingManager.Instance();
 
         if (!housingManager->IsInside()) {
             Service.Chat.PrintError("You must be inside a house or apartment to use that command.");
             return;
         }
-
-        /*
+        
         if (!housingManager->HasHousePermissions()) {
             Service.Chat.PrintError("You don't have permission to adjust the lights in this house/apartment.");
             return;
         }
-        */
+        
         var s = args.Split(' ');
         if (s.Length < 1) {
-            Service.Chat.PrintError($"/{CustomOrDefaultCommand} (0-5) [save]");
+            Service.Chat.PrintError($"/{CustomOrDefaultCommand} (0-5) [ssao-on | ssao-off] [save]");
             return;
         }
 
         var brightness = -1;
         var permanent = false;
-
+        var ssaoEnable = housingManager->IndoorTerritory->SSAOEnable;
+        
         foreach (var a in s) {
             if (byte.TryParse(a, out var o)) {
                 if (o <= 5) brightness = o;
             }
 
             if (permanentMarkers.Contains(a, StringComparer.InvariantCultureIgnoreCase)) permanent = true;
+
+            if (a.Equals("ssao-on", StringComparison.InvariantCultureIgnoreCase)) ssaoEnable = true;
+            if (a.Equals("ssao-off", StringComparison.InvariantCultureIgnoreCase)) ssaoEnable = false;
         }
 
         if (brightness < 0) {
-            Service.Chat.PrintError($"/{CustomOrDefaultCommand} (0-5) [save]");
+            Service.Chat.PrintError($"/{CustomOrDefaultCommand} (0-5) [ssao-on | ssao-off] [save]");
             return;
         }
-
+        
         var agent = AgentModule.Instance()->GetAgentByInternalId(AgentId.Housing);
         var isOpen = agent->IsAgentActive();
-        Common.SendEvent(agent, 33, permanent ? 0 : 3, brightness);
+        
+        Common.SendEvent(agent, 33, permanent ? 0 : 3, brightness, ssaoEnable);
         if (permanent) {
             // This just stops the housing menu from toggling
             if (!isOpen)
@@ -74,7 +77,7 @@ public unsafe class HouseLightCommand : CommandTweak {
                 new SeString(
                     new TextPayload("Lighting has been changed on your client. Use "),
                     new UIForegroundPayload(500),
-                    new TextPayload($"/{Command} {brightness} save"),
+                    new TextPayload($"/{Command} {brightness}  {(ssaoEnable ? "ssao-on" : "ssao-off")} save"),
                     new UIForegroundPayload(0),
                     new TextPayload(" to make it permanent.")
                     )
