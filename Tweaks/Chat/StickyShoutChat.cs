a0e29a82a4dc988234c1db1fc6f37cea03e80e6a
Caraxi <git@caraxian.com>
2025-08-12 17:34:56 +0930

    Fix `Sticky Shout Chat` - Error when failing to find signature

--- Tweaks/Chat/StickyShoutChat.cs
+++ Tweaks/Chat/StickyShoutChat.cs
@@ -2,11 +2,9 @@ using System.Linq;
 using Dalamud;
 using Dalamud.Game.Text;
 using Dalamud.Game.Text.SeStringHandling;
-using Dalamud.Game.Text.SeStringHandling.Payloads;
+using Dalamud.Utility.Signatures;
 using FFXIVClientStructs.FFXIV.Client.System.String;
-using FFXIVClientStructs.FFXIV.Client.UI.Misc;
 using FFXIVClientStructs.FFXIV.Client.UI.Shell;
-using FFXIVClientStructs.FFXIV.Component.Log;
 using SimpleTweaksPlugin.TweakSystem;
 
 namespace SimpleTweaksPlugin.Tweaks.Chat;
@@ -15,16 +13,14 @@ namespace SimpleTweaksPlugin.Tweaks.Chat;
 [TweakDescription("Prevents the game from automatically switching out of shout chat.")]
 [TweakReleaseVersion("1.9.6.0")]
 public class StickyShoutChat : ChatTweaks.SubTweak {
+    [Signature("05 75 0C 8B D7 E8 ?? ?? ?? ?? E9", ScanType = ScanType.Text)]
     private nint editAddress;
 
     protected override void Enable() {
         Service.Chat.CheckMessageHandled -= ChatOnCheckMessageHandled;
         Service.Chat.CheckMessageHandled += ChatOnCheckMessageHandled;
-        
-        if (!Service.SigScanner.TryScanText("05 75 0C 8B D3 E8 ?? ?? ?? ?? E9", out editAddress)) return;
         SafeMemory.Write(editAddress, (sbyte)-2);
     }
-
 
     private readonly string[] errorMessages = [
         "“/shout” requires a valid string.",
