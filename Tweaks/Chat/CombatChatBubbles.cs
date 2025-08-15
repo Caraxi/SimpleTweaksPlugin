8680b40013e210e49e08b1c1dc71b4c0bb01ed77
Caraxi <git@caraxian.com>
2025-08-15 16:36:35 +0930

    New Tweak: `Combat Chat Bubbles`

---
+++ Tweaks/Chat/CombatChatBubbles.cs
@@ -0,0 +1,15 @@
+﻿using Dalamud;
+using Dalamud.Utility.Signatures;
+using SimpleTweaksPlugin.TweakSystem;
+
+namespace SimpleTweaksPlugin.Tweaks.Chat;
+
+[TweakName("Enable chat bubbles in combat")]
+[TweakDescription("Allow chat bubbles to be displayed while in combat.")]
+[TweakReleaseVersion(UnreleasedVersion)]
+public class CombatChatBubbles : Tweak {
+    [Signature("?? 0F 84 ?? ?? ?? ?? F6 80 ?? ?? ?? ?? ?? 0F 84 ?? ?? ?? ?? 49 8B 06")]
+    private nint addr;
+    protected override void Enable() => SafeMemory.WriteBytes(addr + 1, [0x90, 0xE9]);
+    protected override void Disable() => SafeMemory.WriteBytes(addr + 1, [0x0F, 0x84]);
+}
